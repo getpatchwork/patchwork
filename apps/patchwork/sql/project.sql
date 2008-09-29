a15bd7baf3bbb5756ce6e5502912528db7e7224b
Jeremy Kerr <jk@ozlabs.org>
2008-09-29 23:14:05 +1000

    [sql] Default data should be in fixtures, not custom SQL
    Signed-off-by: Jeremy Kerr <jk@ozlabs.org>

--- apps/patchwork/sql/project.sql
+++
@@ -1,6 +0,0 @@
-insert into patchwork_project (linkname, name, listid, listemail)
-    values ('cbe-oss-dev', 'Cell Broadband Engine development',
-            'cbe-oss-dev.ozlabs.org', 'cbe-oss-dev@ozlabs.org');
-insert into patchwork_project (linkname, name, listid, listemail)
-    values ('linuxppc-dev', 'Linux PPC development',
-            'linuxppc-dev.ozlabs.org', 'linuxppc-dev@ozlabs.org');
