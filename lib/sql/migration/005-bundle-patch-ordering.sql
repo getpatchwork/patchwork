5f20247c07c17d266321d49042c525deafe65152
Jeremy Kerr <jk@ozlabs.org>
2009-02-07 15:48:37 +1100

    Normalise order numbers in bundle-ordering migration SQL
    Signed-off-by: Jeremy Kerr <jk@ozlabs.org>

--- lib/sql/migration/005-bundle-patch-ordering.sql
+++ lib/sql/migration/005-bundle-patch-ordering.sql
@@ -8,4 +8,7 @@ ALTER TABLE patchwork_bundlepatch
 ALTER TABLE patchwork_bundlepatch ALTER COLUMN "order" DROP DEFAULT;
 DROP SEQUENCE bundlepatch_tmp_seq;
 ALTER TABLE patchwork_bundlepatch ADD UNIQUE("bundle_id", "order");
+UPDATE patchwork_bundlepatch SET "order" = 1 + "order" -
+	(SELECT min("order") FROM patchwork_bundlepatch AS p2
+		WHERE p2.bundle_id = patchwork_bundlepatch.bundle_id);
 COMMIT;
