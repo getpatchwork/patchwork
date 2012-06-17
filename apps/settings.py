2324d4467a60664beb730b1db4d067b782089561
Jeremy Kerr <jk@ozlabs.org>
2012-06-17 15:36:41 +0800

    settings: Use class-based template loading API
    The TEMPLATE_LOADERS setting changed in django-1.2. Template loaders are
    now based on calsses rather than callables.
    
    Signed-off-by: Jeremy Kerr <jk@ozlabs.org>

--- apps/settings.py
+++ apps/settings.py
@@ -49,8 +49,8 @@ SECRET_KEY = '00000000000000000000000000000000000000000000000000'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
 #     'django.template.loaders.eggs.load_template_source',
 )
 
