c641660e041fdf932bad89681c167328ee85249a
Stephen Finucane <stephen.finucane@intel.com>
2015-04-07 22:20:52 +0100

    settings: Restructure settings file
    Restructure to add some order to the settings file. This includes
    removing some variables:
    
    - TEMPLATE_CONTEXT_PROCESSORS
    - TEMPLATE_LOADERS
    
    These variables have the exact same value as the Django defaults and
    thus do not need to be specified.
    
    Signed-off-by: Stephen Finucane <stephenfinucane@hotmail.com>
    Signed-off-by: Jeremy Kerr <jk@ozlabs.org>

--- apps/settings.py
+++ apps/settings.py
@@ -1,65 +1,34 @@
 # Django settings for patchwork project.
+
 import os
+
 import django
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+ROOT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)
 
-ADMINS = (
-     ('Jeremy Kerr', 'jk@ozlabs.org'),
+#
+# Core settings
+# https://docs.djangoproject.com/en/1.6/ref/settings/#core-settings
+#
+
+# Models
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.admin',
+    'patchwork',
 )
 
-MANAGERS = ADMINS
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'patchwork',
-    },
-}
-
-# Local time zone for this installation. Choices can be found here:
-# http://www.postgresql.org/docs/8.1/static/datetime-keywords.html#DATETIME-TIMEZONE-SET-TABLE
-# although not all variations may be possible on all operating systems.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'Australia/Canberra'
-
-# Language code for this installation. All choices can be found here:
-# http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
-# http://blogs.law.harvard.edu/tech/stories/storyReader$15
-LANGUAGE_CODE = 'en-au'
-
-SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
-
-# URL that handles the media served from MEDIA_ROOT.
-# Example: "http://media.lawrence.com"
-MEDIA_URL = ''
-
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = '00000000000000000000000000000000000000000000000000'
-
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-)
+# HTTP
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.doc.XViewMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
 ]
 
@@ -70,37 +39,83 @@ else:
         'django.contrib.admindocs.middleware.XViewMiddleware')
     TEST_RUNNER = 'django.test.runner.DiscoverRunner'
 
+# Debugging
+
+DEBUG = True
+
+if django.VERSION >= (1, 7):
+    TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+
+# Email
+
+ADMINS = (
+     ('Jeremy Kerr', 'jk@ozlabs.org'),
+)
+
+MANAGERS = ADMINS
+
+# Databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'patchwork',
+    },
+}
+
+# File Uploads
+
+MEDIA_ROOT = os.path.join(
+    ROOT_DIR, 'lib', 'python', 'django', 'contrib', 'admin', 'media')
+
+# Globalization
+
+TIME_ZONE = 'Australia/Canberra'
+
+LANGUAGE_CODE = 'en-au'
+
+USE_I18N = True
+
+# URLs
+
 ROOT_URLCONF = 'urls'
+
+# Security
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '00000000000000000000000000000000000000000000000000'
+
+# Templates
+
+TEMPLATE_DEBUG = True
+
+TEMPLATE_DIRS = (
+    os.path.join(ROOT_DIR, 'templates'),
+)
+
+
+#
+# Auth settings
+# https://docs.djangoproject.com/en/1.6/ref/settings/#auth
+#
+
+ADMIN_MEDIA_PREFIX = '/media/'
 
 LOGIN_URL = '/user/login/'
 LOGIN_REDIRECT_URL = '/user/'
 
-ROOT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.join(ROOT_DIR, 'templates'),
-)
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = os.path.join(
-    ROOT_DIR, 'lib', 'python', 'django', 'contrib', 'admin', 'media')
 
-TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.contrib.auth.context_processors.auth",
-    "django.core.context_processors.debug",
-    "django.core.context_processors.i18n",
-    "django.core.context_processors.media")
+#
+# Sites settings
+# https://docs.djangoproject.com/en/1.6/ref/settings/#sites
+#
 
-INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.admin',
-    'patchwork',
-)
+SITE_ID = 1
+
+
+#
+# Patchwork settings
+#
 
 DEFAULT_PATCHES_PER_PAGE = 100
 DEFAULT_FROM_EMAIL = 'Patchwork <patchwork@patchwork.example.com>'
