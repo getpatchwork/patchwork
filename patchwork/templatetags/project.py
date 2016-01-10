f8cc68d3a0c77906b9d1914f4b78a80b7b3f09db
Stephen Finucane <stephen.finucane@intel.com>
2016-01-10 15:26:37 +0000

    templates: Cleanup patch-list
    Cleanup this file before doing any work on it. Items addressed:
    
    * Move complex logic into template tags
    * Move related content closer together
    * Random whitespace annoyances
    
    Signed-off-by: Stephen Finucane <stephen.finucane@intel.com>

---
+++ patchwork/templatetags/project.py
@@ -0,0 +1,33 @@
+# Patchwork - automated patch tracking system
+# Copyright (C) 2016 Intel Corporation
+#
+# This file is part of the Patchwork package.
+#
+# Patchwork is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Patchwork is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Patchwork; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+from __future__ import absolute_import
+
+from django import template
+from django.utils.safestring import mark_safe
+
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def project_tags(context):
+    return mark_safe('<span title="%s">%s</span>' % (
+        ' / '.join([tag.name for tag in context['project'].tags]),
+        '/'.join([tag.abbrev for tag in context['project'].tags])))
