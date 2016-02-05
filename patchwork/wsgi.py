8fe68d96f18eab84479697cbb0439ba616fcc162
Stephen Finucane <stephen.finucane@intel.com>
2016-02-05 17:31:57 +0000

    wsgi: Move wsgi file to expected location
    Django places a wsgi.py file in the root of each application's
    directory. Do this, adding a symlink to preserve existing
    operation for users.
    
    Signed-off-by: Stephen Finucane <stephen.finucane@intel.com>

---
+++ patchwork/wsgi.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Patchwork - automated patch tracking system
+# Copyright (C) 2010 Martin F. Krafft <madduck@madduck.net>
+#
+# This file is part of the Patchwork package.
+#
+# Patchwork is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Patchwork is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Patchwork; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# Released under the GNU General Public License v2 or later.
+
+import os
+import sys
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'patchwork.settings.production'
+
+application = get_wsgi_application()
