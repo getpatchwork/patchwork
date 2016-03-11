908f2022f2f2de70a1b09e25b62992ebada4a2c9
Stephen Finucane <stephen.finucane@intel.com>
2016-03-11 19:09:58 +0000

    tests: Add sanity test for HashField
    Signed-off-by: Stephen Finucane <stephen.finucane@intel.com>
    Reviewed-by: Andy Doan <andy.doan@linaro.org>

---
+++ patchwork/tests/test_fields.py
@@ -0,0 +1,33 @@
+# Patchwork - automated patch tracking system
+# Copyright (C) 2016 Intel Corporation
+#
+# This file is part of the Patchwork package.
+#
+# Patchwork is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Patchwork is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Patchwork; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+from django.test import SimpleTestCase
+
+from patchwork import fields
+
+
+class TestHashField(SimpleTestCase):
+
+    def test_n_bytes(self):
+        """Sanity check the hashing algorithm.
+
+        Changing this can change our database schema.
+        """
+        field = fields.HashField()
+        self.assertEqual(field.n_bytes, 40)
