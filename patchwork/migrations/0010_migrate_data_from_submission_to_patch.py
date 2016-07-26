09be3a455bfffd6269c505c91cfb74f29200d05a
David Lamparter <equinox@diac24.net>
2016-07-26 16:50:52 +0200

    migrations: fixup 0010 for django 1.7
    apparently, Django 1.7 (or something else in my setup...) doesn't
    support list arguments to RunSQL.
    
    Signed-off-by: David Lamparter <equinox@opensourcerouting.org>

--- patchwork/migrations/0010_migrate_data_from_submission_to_patch.py
+++ patchwork/migrations/0010_migrate_data_from_submission_to_patch.py
@@ -12,19 +12,19 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RunSQL(
-            ['''INSERT INTO patchwork_patch
+            '''INSERT INTO patchwork_patch
                   (submission_ptr_id, diff2, commit_ref2, pull_url2,
                    delegate2_id, state2_id, archived2, hash2)
                 SELECT id, diff, commit_ref, pull_url, delegate_id, state_id,
                        archived, hash
                 FROM patchwork_submission
-                '''],
-            ['''UPDATE patchwork_submission SET
+                '''),
+        migrations.RunSQL(
+            '''UPDATE patchwork_submission SET
                   diff=diff2, commit_ref=commit_ref2, pull_url=pull_url2,
                   delegate_id=delegate2_id, state_id=state2_id,
                   archived=archived2, hash=hash2
                 FROM patchwork_patch WHERE
                   patchwork_submission.id = patchwork_patch.submission_ptr_id
-                ''']
-        ),
+                '''),
     ]
