getpatchwork/patchwork

commit 2d142b2c0a2710a0641966c86edbc9546498b7e2

bin: Run scripts through shellcheck
This mostly results in the addition of quotes to prevent globbing.

Signed-off-by: Stephen Finucane <stephen@that.guru>
Reviewed-by: Daniel Axtens <dja@axtens.net>

patchwork/bin/parsemail-batch.sh
@@ -19,10 +19,9 @@
 # along with Patchwork; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-PATCHWORK_BINDIR=`dirname $0`
+PATCHWORK_BINDIR=$(dirname "$0")
 
-if [ $# -lt 1 ]
-then
+if [ $# -lt 1 ]; then
     echo "usage: $0 <dir> [options]" >&2
     exit 1
 fi
@@ -31,17 +30,15 @@ mail_dir="$1"
 
 echo "dir: $mail_dir"
 
-if [ ! -d "$mail_dir" ]
-then
+if [ ! -d "$mail_dir" ]; then
     echo "$mail_dir should be a directory"? >&2
     exit 1
 fi
 
 shift
 
-ls -1rt "$mail_dir" |
-while read line;
-do
-    echo $line
-    $PATCHWORK_BINDIR/parsemail.sh $@ < "$mail_dir/$line"
+find "$mail_dir" -maxdepth 1 |
+while read -r line; do
+    echo "$line"
+    "$PATCHWORK_BINDIR/parsemail.sh" "$@" < "$mail_dir/$line"
 done

patchwork/bin/parsemail.sh
@@ -19,20 +19,20 @@
 # along with Patchwork; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-BIN_DIR=`dirname $0`
-PATCHWORK_BASE=`readlink -e $BIN_DIR/../..`
+BIN_DIR=$(dirname "$0")
+PATCHWORK_BASE=$(readlink -e "$BIN_DIR/../..")
 
-if [ -z $PW_PYTHON ]; then
+if [ -z "$PW_PYTHON" ]; then
     PW_PYTHON=python2
 fi
 
-if [ -z $DJANGO_SETTINGS_MODULE ]; then
+if [ -z "$DJANGO_SETTINGS_MODULE" ]; then
     DJANGO_SETTINGS_MODULE=patchwork.settings.production
 fi
 
-PYTHONPATH="$PATCHWORK_BASE":"$PATCHWORK_BASE/lib/python:$PYTHONPATH" \
+PYTHONPATH="${PATCHWORK_BASE}:${PATCHWORK_BASE}/lib/python:$PYTHONPATH" \
     DJANGO_SETTINGS_MODULE="$DJANGO_SETTINGS_MODULE" \
-    $PW_PYTHON "$PATCHWORK_BASE/manage.py" parsemail $@
+    "$PW_PYTHON" "$PATCHWORK_BASE/manage.py" parsemail "$@"
 
 # NOTE(stephenfin): We must return 0 here. When parsemail is used as a
 # delivery command from a mail server like postfix (as it is intended

commit 3afa6abcc76948d652da3b353eec6d7ff54bf6eb

Fix double use of mail directory in parsemail-batch.sh
2d142b2c0a27 ("bin: Run scripts through shellcheck") changes an
ls to a find. find output includes the directory name, unlike ls.
This breaks the script, because it prepends the mail directory
later on.

Remove the prepending in the script - leave it exclusively to find.

Signed-off-by: Daniel Axtens <dja@axtens.net>
Reviewed-by: stephen@that.guru
Fixes: 2d142b2c0a27 ("bin: Run scripts through shellcheck")

patchwork/bin/parsemail-batch.sh
@@ -40,5 +40,5 @@ shift
 find "$mail_dir" -maxdepth 1 |
 while read -r line; do
     echo "$line"
-    "$BIN_DIR/parsemail.sh" "$@" < "$mail_dir/$line"
+    "$BIN_DIR/parsemail.sh" "$@" < "$line"
 done