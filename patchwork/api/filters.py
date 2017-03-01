getpatchwork/patchwork

commit 0fc323373df84d0e75776eaa16a1490fe5f028f4

REST: Integrate django-filter support
This mostly works out of the box, thanks to Django REST Framework.
Mostly unique fields, like name or email, are excluded as these will be
handled separately.

Signed-off-by: Stephen Finucane <stephen@that.guru>

patchwork/api/filters.py
@@ -1,0 +1,62 @@
+# Patchwork - automated patch tracking system
+# Copyright (C) 2017 Stephen Finucane <stephen@that.guru>
+#
+# This file is part of the Patchwork package.
+#
+# Patchwork is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Patchwork is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Patchwork; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+from django_filters import FilterSet
+from django_filters import IsoDateTimeFilter
+
+from patchwork.models import Check
+from patchwork.models import CoverLetter
+from patchwork.models import Patch
+from patchwork.models import Series
+
+
+class TimestampMixin(object):
+
+    # TODO(stephenfin): These should filter on a 'updated_at' field instead
+    before = IsoDateTimeFilter(name='date', lookup_expr='lt')
+    since = IsoDateTimeFilter(name='date', lookup_expr='gte')
+
+
+class SeriesFilter(TimestampMixin, FilterSet):
+
+    class Meta:
+        model = Series
+        fields = ('submitter',)
+
+
+class CoverLetterFilter(TimestampMixin, FilterSet):
+
+    class Meta:
+        model = CoverLetter
+        fields = ('project', 'series', 'submitter')
+
+
+class PatchFilter(FilterSet):
+
+    class Meta:
+        model = Patch
+        fields = ('project', 'series', 'submitter', 'delegate', 'state',
+                  'archived')
+
+
+class CheckFilter(TimestampMixin, FilterSet):
+
+    class Meta:
+        model = Check
+        fields = ('user', 'state', 'context')

patchwork/api/check.py
@@ -27,6 +27,7 @@ from rest_framework.serializers import HyperlinkedModelSerializer
 from rest_framework.serializers import HyperlinkedIdentityField
 
 from patchwork.api.base import MultipleFieldLookupMixin
+from patchwork.api.filters import CheckFilter
 from patchwork.models import Check
 from patchwork.models import Patch
 
@@ -89,6 +90,7 @@ class CheckMixin(object):
 
     queryset = Check.objects.prefetch_related('patch', 'user')
     serializer_class = CheckSerializer
+    filter_class = CheckFilter
 
 
 class CheckListCreate(CheckMixin, ListCreateAPIView):

patchwork/api/cover.py
@@ -26,6 +26,7 @@ from rest_framework.serializers import HyperlinkedModelSerializer
 from rest_framework.serializers import HyperlinkedRelatedField
 from rest_framework.serializers import SerializerMethodField
 
+from patchwork.api.filters import CoverLetterFilter
 from patchwork.models import CoverLetter
 
 
@@ -66,6 +67,7 @@ class CoverLetterList(ListAPIView):
     """List cover letters."""
 
     serializer_class = CoverLetterListSerializer
+    filter_class = CoverLetterFilter
 
     def get_queryset(self):
         qs = CoverLetter.objects.all().prefetch_related('series')\

patchwork/api/patch.py
@@ -29,6 +29,7 @@ from rest_framework.serializers import SerializerMethodField
 
 from patchwork.api.base import PatchworkPermission
 from patchwork.api.base import STATE_CHOICES
+from patchwork.api.filters import PatchFilter
 from patchwork.models import Patch
 from patchwork.models import State
 
@@ -117,6 +118,7 @@ class PatchList(ListAPIView):
 
     permission_classes = (PatchworkPermission,)
     serializer_class = PatchListSerializer
+    filter_class = PatchFilter
 
     def get_queryset(self):
         return Patch.objects.all().with_tag_counts()\

patchwork/api/series.py
@@ -22,6 +22,7 @@ from rest_framework.generics import RetrieveAPIView
 from rest_framework.serializers import HyperlinkedModelSerializer
 
 from patchwork.api.base import PatchworkPermission
+from patchwork.api.filters import SeriesFilter
 from patchwork.models import Series
 
 
@@ -56,7 +57,7 @@ class SeriesMixin(object):
 class SeriesList(SeriesMixin, ListAPIView):
     """List series."""
 
-    pass
+    filter_class = SeriesFilter
 
 
 class SeriesDetail(SeriesMixin, RetrieveAPIView):

patchwork/settings/base.py
@@ -133,7 +133,8 @@ try:
     import rest_framework  # NOQA
 
     INSTALLED_APPS += [
-        'rest_framework'
+        'rest_framework',
+        'django_filters',
     ]
 except ImportError:
     pass
@@ -143,6 +144,9 @@ REST_FRAMEWORK = {
     'DEFAULT_VERSIONING_CLASS':
         'rest_framework.versioning.NamespaceVersioning',
     'DEFAULT_PAGINATION_CLASS': 'patchwork.api.base.LinkHeaderPagination',
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
 }
 
 #

commit f380cc58aa3daa0d6e63ccb9f27dbb064f45aa1d

REST: Resolve issues with since, until filters
It would appear that object-based mixins don't work with django-filter's
FilterSet. This should probably be fixed upstream, but for now let's
live with the extra duplication.

Signed-off-by: Stephen Finucane <stephen@that.guru>
Fixes: 0fc32337 ("REST: Integrate django-filter support")
Reviewed-by: Andy Doan <andy.doan@linaro.org>

patchwork/api/filters.py
@@ -26,7 +26,7 @@ from patchwork.models import Patch
 from patchwork.models import Series
 
 
-class TimestampMixin(object):
+class TimestampMixin(FilterSet):
 
     # TODO(stephenfin): These should filter on a 'updated_at' field instead
     before = IsoDateTimeFilter(name='date', lookup_expr='lt')