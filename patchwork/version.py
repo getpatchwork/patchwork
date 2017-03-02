getpatchwork/patchwork

commit d3e9aee4605411ad3ab672e7dd6b7bdd15836fe3

Don't catch non-Python 2.7 exceptions
The 'FileNotFoundError' exception is only available in Python 3.3+.

Signed-off-by: Stephen Finucane <stephen@that.guru>
Fixes: 7b07b71e ("templates: Add Patchwork version")

patchwork/version.py
@@ -57,7 +57,7 @@ def get_raw_git_version():
     try:
         git_version = subprocess.check_output(['git', 'describe'],
                                               cwd=ROOT_DIR)
-    except (OSError, FileNotFoundError):
+    except OSError:
         return ''
 
     return git_version.strip().decode('utf-8')

commit 4e456618a22c5cd1080c14537806765fcfbc6a6f

Catch Subprocess exceptions
Apparently different environments raise different return codes. Handle
the invalid return codes thrown by subprocess, presumably related to how
Travis checks out things from git.

Signed-off-by: Stephen Finucane <stephen@that.guru>
Fixes: d3e9aee ("Don't catch non-Python 2.7 exceptions")

patchwork/version.py
@@ -57,7 +57,7 @@ def get_raw_git_version():
     try:
         git_version = subprocess.check_output(['git', 'describe'],
                                               cwd=ROOT_DIR)
-    except OSError:
+    except (OSError, subprocess.CalledProcessError):
         return ''
 
     return git_version.strip().decode('utf-8')