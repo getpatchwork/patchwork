getpatchwork/patchwork

commit e56391f66cb8624695e4adbec7bf1467655c0fe9

trivial: Standardize variable naming in scripts
Just so we can interact with them as expected.

Signed-off-by: Stephen Finucane <stephen@that.guru>
Reviewed-by: Daniel Axtens <dja@axtens.net>

patchwork/bin/parsemail.sh
@@ -22,6 +22,11 @@
 BIN_DIR=$(dirname "$0")
 PATCHWORK_BASE=$(readlink -e "$BIN_DIR/../..")
 
+if [ $# -lt 1 ]; then
+    echo "usage: $0 <file> [options]" >&2
+    exit 1
+fi
+
 if [ -z "$PW_PYTHON" ]; then
     PW_PYTHON=python2
 fi

patchwork/bin/parsemail-batch.sh
@@ -19,7 +19,7 @@
 # along with Patchwork; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-PATCHWORK_BINDIR=$(dirname "$0")
+BIN_DIR=$(dirname "$0")
 
 if [ $# -lt 1 ]; then
     echo "usage: $0 <dir> [options]" >&2
@@ -40,5 +40,5 @@ shift
 find "$mail_dir" -maxdepth 1 |
 while read -r line; do
     echo "$line"
-    "$PATCHWORK_BINDIR/parsemail.sh" "$@" < "$mail_dir/$line"
+    "$BIN_DIR/parsemail.sh" "$@" < "$mail_dir/$line"
 done

tools/patchwork-update-commits
@@ -19,8 +19,8 @@
 # along with Patchwork; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-toolsdir="$(dirname "$0")"
-pwpath="${toolsdir}/../patchwork"
+TOOLS_DIR="$(dirname "$0")"
+PW_DIR="${TOOLS_DIR}/../patchwork"
 
 if [ "$#" -lt 1 ]; then
     echo "usage: $0 <revspec>" >&2
@@ -29,6 +29,6 @@ fi
 
 git rev-list --reverse "$@" |
 while read -r commit; do
-    hash=$(git diff "$commit~..$commit" | python "$pwpath/hasher.py")
-    "$pwpath/bin/pwclient" update -s Accepted -c "$commit" -h "$hash"
+    hash=$(git diff "$commit~..$commit" | python "$PW_DIR/hasher.py")
+    "$PW_DIR/bin/pwclient" update -s Accepted -c "$commit" -h "$hash"
 done

tools/post-receive.hook
@@ -21,37 +21,36 @@
 # Git post-receive hook to update Patchwork patches after Git pushes
 set -eu
 
+PW_DIR=/opt/patchwork/patchwork
+
 #TODO: the state map should really live in the repo's git-config
 STATE_MAP="refs/heads/master:Accepted"
-#
+
 # ignore all commits already present in these refs
 # e.g.,
 #   EXCLUDE="refs/heads/upstream refs/heads/other-project"
-#
 EXCLUDE=""
-
-PWDIR=/opt/patchwork/patchwork
 
 do_exit=0
 trap "do_exit=1" INT
 
 get_patchwork_hash() {
     local hash
-    hash=$(git diff "$1~..$1" | python $PWDIR/hasher.py)
+    hash=$(git diff "$1~..$1" | python $PW_DIR/hasher.py)
     echo "$hash"
     test -n "$hash"
 }
 
 get_patch_id() {
     local id
-    id=$($PWDIR/bin/pwclient info -h "$1" 2>/dev/null \
+    id=$($PW_DIR/bin/pwclient info -h "$1" 2>/dev/null \
          | sed -rne 's,- id[[:space:]]*: ,,p')
     echo "$id"
     test -n "$id"
 }
 
 set_patch_state() {
-    $PWDIR/bin/pwclient update -s "$2" -c "$3" "$1" 2>&1
+    $PW_DIR/bin/pwclient update -s "$2" -c "$3" "$1" 2>&1
 }
 
 update_patches() {

commit 03ff2f51d9a59fa6e118362dc0e07ecd17a4cabb

parsemail.sh: Fix usage via stdin.
Commit e56391f66cb8 ("trivial: Standardize variable naming in scripts")
sneaked in a checked for missing command line arguments in parsemail.sh
which broke typical argument-less usage for mail delivery.  Revert that
again.

Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
Reviewed-by: Stephen Finucane <stephen@that.guru>
Fixes: e56391f6 ("trivial: Standardize variable naming in scripts")

patchwork/bin/parsemail.sh
@@ -22,11 +22,6 @@
 BIN_DIR=$(dirname "$0")
 PATCHWORK_BASE=$(readlink -e "$BIN_DIR/../..")
 
-if [ $# -lt 1 ]; then
-    echo "usage: $0 <file> [options]" >&2
-    exit 1
-fi
-
 if [ -z "$PW_PYTHON" ]; then
     PW_PYTHON=python2
 fi