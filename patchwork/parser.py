getpatchwork/patchwork

commit 2a915efdf95a691577738418ef31ed3f2b6383ca

parser: fix wrong parsing of diff comments
If the subject of a submission is prefixed by 'Re:', then it can't be a
patch or cover letter.

Signed-off-by: WEN Pingbo <wengpingbo@gmail.com>
Reviewed-by: Stephen Finucane <stephenfinucane@hotmail.com>

patchwork/parser.py
@@ -353,6 +353,13 @@ def clean_subject(subject, drop_prefixes=None):
     return (subject, prefixes)
 
 
+def subject_check(subject):
+    """Determine if a mail is a reply."""
+    comment_re = re.compile(r'^(re)[:\s]\s*', re.I)
+
+    return comment_re.match(subject)
+
+
 def clean_content(content):
     """Remove cruft from the email message.
 
@@ -619,7 +626,9 @@ def parse_mail(mail, list_id=None):
 
     msgid = mail.get('Message-Id').strip()
     author = find_author(mail)
-    name, prefixes = clean_subject(mail.get('Subject'), [project.linkname])
+    subject = mail.get('Subject')
+    name, prefixes = clean_subject(subject, [project.linkname])
+    is_comment = subject_check(subject)
     x, n = parse_series_marker(prefixes)
     refs = find_references(mail)
     date = find_date(mail)
@@ -628,7 +637,7 @@ def parse_mail(mail, list_id=None):
 
     # build objects
 
-    if diff or pull_url:  # patches or pull requests
+    if not is_comment and (diff or pull_url):  # patches or pull requests
         # we delay the saving until we know we have a patch.
         author.save()
 
@@ -658,17 +667,18 @@ def parse_mail(mail, list_id=None):
         # however, we need to see if a match already exists and, if
         # not, assume that it is indeed a new cover letter
         is_cover_letter = False
-        if not refs == []:
-            try:
-                CoverLetter.objects.all().get(name=name)
-            except CoverLetter.DoesNotExist:
-                # if no match, this is a new cover letter
+        if not is_comment:
+            if not refs == []:
+                try:
+                    CoverLetter.objects.all().get(name=name)
+                except CoverLetter.DoesNotExist:
+                    # if no match, this is a new cover letter
+                    is_cover_letter = True
+                except CoverLetter.MultipleObjectsReturned:
+                    # if multiple cover letters are found, just ignore
+                    pass
+            else:
                 is_cover_letter = True
-            except CoverLetter.MultipleObjectsReturned:
-                # if multiple cover letters are found, just ignore
-                pass
-        else:
-            is_cover_letter = True
 
         if is_cover_letter:
             author.save()
@@ -692,6 +702,9 @@ def parse_mail(mail, list_id=None):
     submission = find_submission_for_comment(project, refs)
     if not submission:
         return
+
+    if is_comment and diff:
+        message += diff
 
     author.save()
 

patchwork/tests/test_parser.py
@@ -38,6 +38,7 @@ from patchwork.parser import parse_mail as _parse_mail
 from patchwork.parser import parse_pull_request
 from patchwork.parser import parse_series_marker
 from patchwork.parser import split_prefixes
+from patchwork.parser import subject_check
 from patchwork.tests.utils import create_project
 from patchwork.tests.utils import create_state
 from patchwork.tests.utils import create_user
@@ -706,3 +707,11 @@ class SubjectTest(TestCase):
                          ('[bar] meep', ['bar']))
         self.assertEqual(clean_subject('[FOO] [bar] meep', ['foo']),
                          ('[bar] meep', ['bar']))
+
+    def test_subject_check(self):
+        self.assertIsNotNone(subject_check('RE: meep'))
+        self.assertIsNotNone(subject_check('Re: meep'))
+        self.assertIsNotNone(subject_check('re: meep'))
+        self.assertIsNotNone(subject_check('RE meep'))
+        self.assertIsNotNone(subject_check('Re meep'))
+        self.assertIsNotNone(subject_check('re meep'))

commit 69e7e366593eeb19cf9ec65ba1ca9649e9c7ccb0

parser: Don't extract diffs from replies
In '2a915efd', a check was added to ensure mails prefixed with 'RE:' or
similar would not be parsed as patches. By time this check actually
happens, any patches had already been extracted from the mail thus these
patches were re-added to the mail content before saving the comment.
Unfortunately, this didn't take into account cases where a patch or diff
was not the last part of a mail but rather located somewhere in the
middle of the content:

    Introduction content
    Diff or patch content ***
    Additional content

This would result in mangling of the mail as the patch would _always_ be
appended to the end:

    Introduction content
    Additional content
    Diff or patch content ***

Handle this by only breaking a mail into a comment and a diff if there
is any possibility that we might want to use that diff.

Signed-off-by: Stephen Finucane <stephen@that.guru>
Fixes: 2a915efd ("parser: fix wrong parsing of diff comments")
Closes-bug: #95

patchwork/parser.py
@@ -342,10 +342,15 @@ def parse_version(subject, subject_prefixes):
     return 1
 
 
-def find_content(mail):
-    """Extract a comment and potential diff from a mail."""
-    patchbuf = None
-    commentbuf = ''
+def _find_content(mail):
+    """Extract the payload(s) from a mail.
+
+    Handles various payload types.
+
+    :returns: A list of tuples, corresponding the payload and subtype
+        of payload.
+    """
+    results = []
 
     for part in mail.walk():
         if part.get_content_maintype() != 'text':
@@ -381,8 +386,19 @@ def find_content(mail):
 
             # Could not find a valid decoded payload.  Fail.
             if payload is None:
-                return None, None
+                continue
 
+        results.append((payload, subtype))
+
+    return results
+
+
+def find_patch_content(mail):
+    """Extract a comment and potential diff from a mail."""
+    patchbuf = None
+    commentbuf = ''
+
+    for payload, subtype in _find_content(mail):
         if subtype in ['x-patch', 'x-diff']:
             patchbuf = payload
         elif subtype == 'plain':
@@ -397,6 +413,22 @@ def find_content(mail):
     commentbuf = clean_content(commentbuf)
 
     return patchbuf, commentbuf
+
+
+def find_comment_content(mail):
+    """Extract content from a mail."""
+    commentbuf = ''
+
+    for payload, _ in _find_content(mail):
+        if not payload:
+            continue
+
+        commentbuf += payload.strip() + '\n'
+
+    commentbuf = clean_content(commentbuf)
+
+    # keep the method signature the same as find_patch_content
+    return None, commentbuf
 
 
 def find_submission_for_comment(project, refs):
@@ -759,12 +791,7 @@ def parse_mail(mail, list_id=None):
         logger.error('Failed to find a project for email')
         return
 
-    # parse content
-
-    diff, message = find_content(mail)
-
-    if not (diff or message):
-        return  # nothing to work with
+    # parse metadata
 
     msgid = mail.get('Message-Id').strip()
     author = find_author(mail)
@@ -776,6 +803,17 @@ def parse_mail(mail, list_id=None):
     refs = find_references(mail)
     date = find_date(mail)
     headers = find_headers(mail)
+
+    # parse content
+
+    if not is_comment:
+        diff, message = find_patch_content(mail)
+    else:
+        diff, message = find_comment_content(mail)
+
+    if not (diff or message):
+        return  # nothing to work with
+
     pull_url = parse_pull_request(message)
 
     # build objects
@@ -913,9 +951,6 @@ def parse_mail(mail, list_id=None):
     submission = find_submission_for_comment(project, refs)
     if not submission:
         return
-
-    if is_comment and diff:
-        message += diff
 
     author.save()
 

patchwork/tests/test_parser.py
@@ -33,7 +33,7 @@ from patchwork.models import Person
 from patchwork.models import State
 from patchwork.parser import clean_subject
 from patchwork.parser import find_author
-from patchwork.parser import find_content
+from patchwork.parser import find_patch_content as find_content
 from patchwork.parser import find_project_by_header
 from patchwork.parser import find_series
 from patchwork.parser import parse_mail as _parse_mail