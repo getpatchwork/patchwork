getpatchwork/patchwork

commit 6deedfb7a7125eade8d6a84c8dfec89c08f54338

views: Expose Series mbox
It is possible to download a patch mbox with all dependencies. Now make
it possible to download the entire series. This takes the form of the
following URL when using the default routes:

    /series/{seriesID}/mbox/

Like the equivalent patch and bundle links, this will return a 404 if
'{seriesID}' does not match an existing series' ID. However, a 404 will
also be returned in the series is not complete, as indicated by
Series.total > Series.received_total. You can override this behavior by
providing the 'force' parameter:

    /series/{seriesID}/mbox/?force=1

Note that there are no current plans to provide a series-specific view,
a.k.a.

    /series/{seriesID}/

As a result, this particular URL will continue to return a 404.

Signed-off-by: Stephen Finucane <stephen@that.guru>
Reviewed-by: Daniel Axtens <dja@axtens.net>

patchwork/models.py
@@ -587,6 +587,12 @@ class Series(models.Model):
     def received_all(self):
         return self.total <= self.received_total
 
+    @property
+    def filename(self):
+        fname_re = re.compile(r'[^-_A-Za-z0-9\.]+')
+        fname = fname_re.sub('-', str(self))
+        return fname.strip('-') + '.patch'
+
     def add_cover_letter(self, cover):
         """Add a cover letter to the series.
 

patchwork/views/series.py
@@ -1,0 +1,35 @@
+# Patchwork - automated patch tracking system
+# Copyright (C) 2017 Stephen Finucane <stephen@that.guru>
+#
+# This file is part of the Patchwork package.
+#
+# Patchwork is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Patchwork is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Patchwork; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+
+from patchwork.models import Series
+from patchwork.views.utils import series_to_mbox
+
+
+def series_mbox(request, series_id):
+    series = get_object_or_404(Series, id=series_id)
+
+    response = HttpResponse(content_type='text/plain')
+    response.write(series_to_mbox(series))
+    response['Content-Disposition'] = 'attachment; filename=' + \
+        series.filename.replace(';', '').replace('\n', '')
+
+    return response

patchwork/urls.py
@@ -32,6 +32,7 @@ from patchwork.views import notification as notification_views
 from patchwork.views import patch as patch_views
 from patchwork.views import project as project_views
 from patchwork.views import pwclient as pwclient_views
+from patchwork.views import series as series_views
 from patchwork.views import user as user_views
 from patchwork.views import xmlrpc as xmlrpc_views
 
@@ -61,9 +62,13 @@ urlpatterns = [
     url(r'^cover/(?P<cover_id>\d+)/$', cover_views.cover_detail,
         name='cover-detail'),
 
-    # comment urls
+    # comment views
     url(r'^comment/(?P<comment_id>\d+)/$', comment_views.comment,
         name='comment-redirect'),
+
+    # series views
+    url(r'^series/(?P<series_id>\d+)/mbox/$', series_views.series_mbox,
+        name='series-mbox'),
 
     # logged-in user stuff
     url(r'^user/$', user_views.profile, name='user-profile'),

patchwork/views/utils.py
@@ -151,3 +151,20 @@ def series_patch_to_mbox(patch, series_num):
     mbox.append(patch_to_mbox(patch))
 
     return '\n'.join(mbox)
+
+
+def series_to_mbox(series):
+    """Get an mbox representation of an entire series.
+
+    Arguments:
+        series: The Series object to convert.
+
+    Returns:
+        A string for the mbox file.
+    """
+    mbox = []
+
+    for dep in series.seriespatch_set.all():
+        mbox.append(patch_to_mbox(dep.patch))
+
+    return '\n'.join(mbox)

commit e5309a3898c7a3f17546c4a1cff474cb8658b7a9

REST: Embed 'mbox' parameter in /series
This wasn't added in '6deedfb7' but it should have been.

Signed-off-by: Stephen Finucane <stephen@that.guru>
Fixes: 6deedfb7 ("views: Expose Series mbox")

patchwork/api/series.py
@@ -20,6 +20,7 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.serializers import HyperlinkedModelSerializer
+from rest_framework.serializers import SerializerMethodField
 
 from patchwork.api.base import PatchworkPermission
 from patchwork.api.filters import SeriesFilter
@@ -28,13 +29,19 @@ from patchwork.models import Series
 
 class SeriesSerializer(HyperlinkedModelSerializer):
 
+    mbox = SerializerMethodField()
+
+    def get_mbox(self, instance):
+        request = self.context.get('request')
+        return request.build_absolute_uri(instance.get_mbox_url())
+
     class Meta:
         model = Series
         fields = ('id', 'url', 'project', 'name', 'date', 'submitter',
                   'version', 'total', 'received_total', 'received_all',
-                  'cover_letter', 'patches')
+                  'mbox', 'cover_letter', 'patches')
         read_only_fields = ('date', 'submitter', 'total', 'received_total',
-                            'received_all', 'cover_letter', 'patches')
+                            'received_all', 'mbox', 'cover_letter', 'patches')
         extra_kwargs = {
             'url': {'view_name': 'api-series-detail'},
             'project': {'view_name': 'api-project-detail'},

patchwork/models.py
@@ -668,6 +668,10 @@ class Series(models.Model):
                                           patch=patch,
                                           number=number)
 
+    @models.permalink
+    def get_mbox_url(self):
+        return ('series-mbox', (), {'series_id': self.id})
+
     def __str__(self):
         return self.name if self.name else 'Untitled series #%d' % self.id
 

patchwork/tests/test_rest_api.py
@@ -511,6 +511,7 @@ class TestSeriesAPI(APITestCase):
     def assertSerialized(self, series_obj, series_json):
         self.assertEqual(series_obj.id, series_json['id'])
         self.assertEqual(series_obj.name, series_json['name'])
+        self.assertIn(series_obj.get_mbox_url(), series_json['mbox'])
         self.assertIn(TestProjectAPI.api_url(series_obj.project.id),
                       series_json['project'])
         self.assertIn(TestPersonAPI.api_url(series_obj.submitter.id),