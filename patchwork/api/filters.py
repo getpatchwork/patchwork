getpatchwork/patchwork

commit e27b68a1cc113310535f0fadd7a56a5a54cc2b25

REST: Filter on Project.linkname - not Project.pk
This is what we expect based on the web UI.

Signed-off-by: Stephen Finucane <stephen@that.guru>

patchwork/api/filters.py
@@ -20,6 +20,7 @@
 from django_filters import FilterSet
 from django_filters import IsoDateTimeFilter
 from django_filters import CharFilter
+from django_filters import ModelChoiceFilter
 
 from patchwork.compat import LOOKUP_FIELD
 from patchwork.models import Bundle
@@ -27,6 +28,7 @@ from patchwork.models import Check
 from patchwork.models import CoverLetter
 from patchwork.models import Event
 from patchwork.models import Patch
+from patchwork.models import Project
 from patchwork.models import Series
 
 
@@ -37,21 +39,27 @@ class TimestampMixin(FilterSet):
     since = IsoDateTimeFilter(name='date', **{LOOKUP_FIELD: 'gte'})
 
 
-class SeriesFilter(TimestampMixin, FilterSet):
+class ProjectMixin(FilterSet):
+
+    project = ModelChoiceFilter(to_field_name='linkname',
+                                queryset=Project.objects.all())
+
+
+class SeriesFilter(ProjectMixin, TimestampMixin, FilterSet):
 
     class Meta:
         model = Series
         fields = ('submitter', 'project')
 
 
-class CoverLetterFilter(TimestampMixin, FilterSet):
+class CoverLetterFilter(ProjectMixin, TimestampMixin, FilterSet):
 
     class Meta:
         model = CoverLetter
         fields = ('project', 'series', 'submitter')
 
 
-class PatchFilter(FilterSet):
+class PatchFilter(ProjectMixin, FilterSet):
 
     # TODO(stephenfin): We should probably be using a ChoiceFilter here?
     state = CharFilter(name='state__name')
@@ -69,14 +77,14 @@ class CheckFilter(TimestampMixin, FilterSet):
         fields = ('user', 'state', 'context')
 
 
-class EventFilter(FilterSet):
+class EventFilter(ProjectMixin, FilterSet):
 
     class Meta:
         model = Event
         fields = ('project', 'category', 'series', 'patch', 'cover')
 
 
-class BundleFilter(FilterSet):
+class BundleFilter(ProjectMixin, FilterSet):
 
     class Meta:
         model = Bundle

commit 08b211519826febcdba7714ffd5d6fa80282a0c2

REST: Allow filtering by both project ID and linkname
In hindsight, it's a bit odd that we would filter project by linkname by
everything else by ID. Simply support both.

Signed-off-by: Stephen Finucane <stephen@that.guru>
Fixes: e27b68a ("REST: Filter on Project.linkname - not Project.pk")

patchwork/api/filters.py
@@ -41,10 +41,34 @@ class TimestampMixin(FilterSet):
     since = IsoDateTimeFilter(name='date', **{LOOKUP_FIELD: 'gte'})
 
 
+class ProjectChoiceField(ModelChoiceField):
+
+    def to_python(self, value):
+        if value in self.empty_values:
+            return None
+
+        try:
+            filters = {'pk': int(value)}
+        except ValueError:
+            filters = {'name__iexact': ' '.join(value.split('-'))}
+
+        try:
+            value = self.queryset.get(**filters)
+        except (ValueError, TypeError, self.queryset.model.DoesNotExist):
+            raise ValidationError(self.error_messages['invalid_choice'],
+                                  code='invalid_choice')
+        return value
+
+
+class ProjectFilter(ModelChoiceFilter):
+
+    field_class = ProjectChoiceField
+
+
 class ProjectMixin(FilterSet):
 
-    project = ModelChoiceFilter(to_field_name='linkname',
-                                queryset=Project.objects.all())
+    project = ProjectFilter(to_field_name='linkname',
+                            queryset=Project.objects.all())
 
 
 class SeriesFilter(ProjectMixin, TimestampMixin, FilterSet):