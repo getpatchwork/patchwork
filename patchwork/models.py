getpatchwork/patchwork

commit 8585ea5afec383c9c5622843bcff0e6c448614c7

models: Use 'base_manager_name'
This raises warnings in Django 1.10 and will be required in Django 1.11.

We also set 'silence_use_for_related_fields_deprecation' to allow us to
retain the current behavior on versions of Django < 1.10.

Signed-off-by: Stephen Finucane <stephen@that.guru>

patchwork/models.py
@@ -270,6 +270,9 @@ class PatchQuerySet(models.query.QuerySet):
 
 class PatchManager(models.Manager):
     use_for_related_fields = True
+    # NOTE(stephenfin): This is necessary to silence a warning with Django >=
+    # 1.10. Remove when 1.10 is the minimum supported version.
+    silence_use_for_related_fields_deprecation = True
 
     def get_queryset(self):
         return PatchQuerySet(self.model, using=self.db)
@@ -537,6 +540,7 @@ class Patch(SeriesMixin, Submission):
 
     class Meta:
         verbose_name_plural = 'Patches'
+        base_manager_name = 'objects'
 
 
 class Comment(EmailMixin, models.Model):

commit 5708fb48adecfd9929366b114bdb6d4cd09df74e

models: Only set 'base_manager_name' for Django >= 1.10
Signed-off-by: Stephen Finucane <stephen@that.guru>
Fixes: 8585ea5 ("models: Use 'base_manager_name'")

patchwork/models.py
@@ -545,7 +545,8 @@ class Patch(SeriesMixin, Submission):
 
     class Meta:
         verbose_name_plural = 'Patches'
-        base_manager_name = 'objects'
+        if django.VERSION >= (1, 10):
+            base_manager_name = 'objects'
 
 
 class Comment(EmailMixin, models.Model):