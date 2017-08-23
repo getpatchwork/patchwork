getpatchwork/patchwork

commit 866d14b4c1ce64e20ec4d2e7b3dd7667188ab630

models: Fix invocation of refresh_tag_counts() for Comments
In Comment.save() and Comment.delete(), we always call
Submission.refresh_tag_counts(), which is an empty stub, rather than
calling Patch.refresh_tag_counts() if the Submission is a Patch.

As such, tag counts are never updated on incoming comments.

Delete Submission.refresh_tag_counts(), as it's useless, and in
Comment.save()/delete(), invoke Patch.refresh_tag_counts() directly when
the submission is a Patch.

Reported-by: David Demelier <markand@malikania.fr>
Fixes: 86172ccc161b ("models: Split Patch into two models")
Closes-bug: #111 ("A/R/T not updated on comments")
Signed-off-by: Andrew Donnellan <andrew.donnellan@au1.ibm.com>
Reviewed-by: Stephen Finucane <stephen@that.guru>

patchwork/models.py
@@ -350,10 +350,6 @@ class Submission(FilenameMixin, EmailMixin, models.Model):
 
     # patchwork metadata
 
-    def refresh_tag_counts(self):
-        # This is subclassed on 'Patch' to do something useful
-        pass
-
     def is_editable(self, user):
         return False
 
@@ -578,11 +574,13 @@ class Comment(EmailMixin, models.Model):
 
     def save(self, *args, **kwargs):
         super(Comment, self).save(*args, **kwargs)
-        self.submission.refresh_tag_counts()
+        if hasattr(self.submission, 'patch'):
+            self.submission.patch.refresh_tag_counts()
 
     def delete(self, *args, **kwargs):
         super(Comment, self).delete(*args, **kwargs)
-        self.submission.refresh_tag_counts()
+        if hasattr(self.submission, 'patch'):
+            self.submission.patch.refresh_tag_counts()
 
     class Meta:
         ordering = ['date']

commit fb7321bb36616d194313ab9d3151e508efb9b2a4

Fix issue with Python 3.4, Django 1.6
Signed-off-by: Stephen Finucane <stephen@that.guru>
Fixes: 866d14b4 ("models: Fix invocation of refresh_tag_counts() for Comments")

patchwork/models.py
@@ -574,8 +574,12 @@ class Comment(EmailMixin, models.Model):
 
     def save(self, *args, **kwargs):
         super(Comment, self).save(*args, **kwargs)
-        if hasattr(self.submission, 'patch'):
-            self.submission.patch.refresh_tag_counts()
+        # NOTE(stephenfin): Mitigate an issue with Python 3.4 + Django 1.6
+        try:
+            if hasattr(self.submission, 'patch'):
+                self.submission.patch.refresh_tag_counts()
+        except Patch.DoesNotExist:
+            pass
 
     def delete(self, *args, **kwargs):
         super(Comment, self).delete(*args, **kwargs)