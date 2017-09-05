getpatchwork/patchwork

commit 08b211519826febcdba7714ffd5d6fa80282a0c2

REST: Allow filtering by both project ID and linkname
In hindsight, it's a bit odd that we would filter project by linkname by
everything else by ID. Simply support both.

Signed-off-by: Stephen Finucane <stephen@that.guru>
Fixes: e27b68a ("REST: Filter on Project.linkname - not Project.pk")

patchwork/api/filters.py
@@ -41,10 +41,34 @@ class TimestampMixin(FilterSet):
     since = IsoDateTimeFilter(name='date', **{LOOKUP_FIELD: 'gte'})
 
 
+class ProjectChoiceField(ModelChoiceField):
+
+    def to_python(self, value):
+        if value in self.empty_values:
+            return None
+
+        try:
+            filters = {'pk': int(value)}
+        except ValueError:
+            filters = {'name__iexact': ' '.join(value.split('-'))}
+
+        try:
+            value = self.queryset.get(**filters)
+        except (ValueError, TypeError, self.queryset.model.DoesNotExist):
+            raise ValidationError(self.error_messages['invalid_choice'],
+                                  code='invalid_choice')
+        return value
+
+
+class ProjectFilter(ModelChoiceFilter):
+
+    field_class = ProjectChoiceField
+
+
 class ProjectMixin(FilterSet):
 
-    project = ModelChoiceFilter(to_field_name='linkname',
-                                queryset=Project.objects.all())
+    project = ProjectFilter(to_field_name='linkname',
+                            queryset=Project.objects.all())
 
 
 class SeriesFilter(ProjectMixin, TimestampMixin, FilterSet):

commit 1cb350d891a7625911789ab273332c03d6a8285d

REST: Filter projects by 'linkname', not 'name'
Based on a report on the OVS mailing list, it appears that projects are
being filtered on the 'Project.name' field instead of the
'Project.linkname' field. Correct this and add regression tests to
prevent it happening again.

Signed-off-by: Stephen Finucane <stephen@that.guru>
Fixes: 08b2115 ("REST: Allow filtering by both project ID and linkname")
Closes-bug: #117 ("Projects are filtered on the wrong field")
Cc: Daniel Axtens <dja@axtens.net>
Signed-off-by: Daniel Axtens <dja@axtens.net>

patchwork/api/filters.py
@@ -50,7 +50,7 @@ class ProjectChoiceField(ModelChoiceField):
         try:
             filters = {'pk': int(value)}
         except ValueError:
-            filters = {'name__iexact': ' '.join(value.split('-'))}
+            filters = {'linkname__iexact': ' '.join(value.split('-'))}
 
         try:
             value = self.queryset.get(**filters)

patchwork/tests/test_rest_api.py
@@ -97,7 +97,23 @@ class TestProjectAPI(APITestCase):
         self.assertEqual(status.HTTP_200_OK, resp.status_code)
         self.assertSerialized(project, resp.data)
 
-    def test_get_numeric_linkname(self):
+    def test_get_by_id(self):
+        """Validate that it's possible to filter by pk."""
+        project = create_project()
+
+        resp = self.client.get(self.api_url(project.pk))
+        self.assertEqual(status.HTTP_200_OK, resp.status_code)
+        self.assertSerialized(project, resp.data)
+
+    def test_get_by_linkname(self):
+        """Validate that it's possible to filter by linkname."""
+        project = create_project(linkname='project', name='Sample project')
+
+        resp = self.client.get(self.api_url('project'))
+        self.assertEqual(status.HTTP_200_OK, resp.status_code)
+        self.assertSerialized(project, resp.data)
+
+    def test_get_by_numeric_linkname(self):
         """Validate we try to do the right thing for numeric linkname"""
         project = create_project(linkname='12345')
 
@@ -326,7 +342,8 @@ class TestPatchAPI(APITestCase):
         self.assertEqual(0, len(resp.data))
 
         state_obj = create_state(name='Under Review')
-        patch_obj = create_patch(state=state_obj)
+        project_obj = create_project(linkname='myproject')
+        patch_obj = create_patch(state=state_obj, project=project_obj)
 
         # anonymous user
         resp = self.client.get(self.api_url())
@@ -338,12 +355,6 @@ class TestPatchAPI(APITestCase):
         self.assertNotIn('content', patch_rsp)
         self.assertNotIn('diff', patch_rsp)
 
-        # test filtering by state
-        resp = self.client.get(self.api_url(), {'state': 'under-review'})
-        self.assertEqual([patch_obj.id], [x['id'] for x in resp.data])
-        resp = self.client.get(self.api_url(), {'state': 'missing-state'})
-        self.assertEqual(0, len(resp.data))
-
         # authenticated user
         user = create_user()
         self.client.force_authenticate(user=user)
@@ -352,6 +363,18 @@ class TestPatchAPI(APITestCase):
         self.assertEqual(1, len(resp.data))
         patch_rsp = resp.data[0]
         self.assertSerialized(patch_obj, patch_rsp)
+
+        # test filtering by state
+        resp = self.client.get(self.api_url(), {'state': 'under-review'})
+        self.assertEqual([patch_obj.id], [x['id'] for x in resp.data])
+        resp = self.client.get(self.api_url(), {'state': 'missing-state'})
+        self.assertEqual(0, len(resp.data))
+
+        # test filtering by project
+        resp = self.client.get(self.api_url(), {'project': 'myproject'})
+        self.assertEqual([patch_obj.id], [x['id'] for x in resp.data])
+        resp = self.client.get(self.api_url(), {'project': 'invalidproject'})
+        self.assertEqual(0, len(resp.data))
 
     def test_detail(self):
         """Validate we can get a specific patch."""