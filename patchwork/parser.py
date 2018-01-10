getpatchwork/patchwork

commit b891f8df77127253ebc81874bf3f30dd84938cdb

parser: Log when invalid project list-id passed
I thought there was a bug. Turns out I was just using the wrong list-id.
Make this clearer.

Signed-off-by: Stephen Finucane <stephen@that.guru>

patchwork/parser.py
@@ -156,7 +156,7 @@ def find_project_by_id(list_id):
     try:
         project = Project.objects.get(listid=list_id)
     except Project.DoesNotExist:
-        pass
+        logger.debug("'%s' if not a valid project list-id", list_id)
     return project
 
 
@@ -184,6 +184,9 @@ def find_project_by_header(mail):
             project = find_project_by_id(listid)
             if project:
                 break
+
+    if not project:
+        logger.debug("Could not find a list-id in mail headers", list_id)
 
     return project
 

commit 572e15e2382c51137529c7c12080b976211f67ef

Correct type (v2)
This is why you shouldn't do stuff at 1am.

Signed-off-by: Stephen Finucane <stephen@that.guru>
Fixes: b891f8d ("parser: Log when invalid project list-id passed")

patchwork/parser.py
@@ -186,7 +186,7 @@ def find_project_by_header(mail):
                 break
 
     if not project:
-        logger.debug("Could not find a list-id in mail headers", listid)
+        logger.debug("Could not find a list-id in mail headers")
 
     return project
 