getpatchwork/patchwork

commit 01b9cbb9ce9f44387be22b5d036dde0fff4bb14b

Include all email headers in mboxes
Solves issue #165 (Exported mboxes should include In-Reply-To,
References, etc headers). Instead of including only a few chosen ones,
all received headers are added to mboxes.

Signed-off-by: Veronika Kabatova <vkabatov@redhat.com>
Reviewed-by: Stephen Finucane <stephen@that.guru>

patchwork/views/utils.py
@@ -89,21 +89,17 @@ def _submission_to_mbox(submission):
     utc_timestamp = delta.seconds + delta.days * 24 * 3600
 
     mail = PatchMbox(body)
-    mail['Subject'] = submission.name
     mail['X-Patchwork-Submitter'] = email.utils.formataddr((
         str(Header(submission.submitter.name, mail.patch_charset)),
         submission.submitter.email))
     mail['X-Patchwork-Id'] = str(submission.id)
     if is_patch and submission.delegate:
         mail['X-Patchwork-Delegate'] = str(submission.delegate.email)
-    mail['Message-Id'] = submission.msgid
     mail.set_unixfrom('From patchwork ' + submission.date.ctime())
 
-    copied_headers = ['To', 'Cc', 'Date', 'From', 'List-Id']
     orig_headers = HeaderParser().parsestr(str(submission.headers))
-    for header in copied_headers:
-        if header in orig_headers:
-            mail[header] = orig_headers[header]
+    for key, val in orig_headers.items():
+        mail[key] = val
 
     if 'Date' not in mail:
         mail['Date'] = email.utils.formatdate(utc_timestamp)

commit b2a2534289df1877246b5c077eb426ba6f4e32e1

Use parsed subject for mboxes
With a recent change, we started using the original subject header
instead of the one we had already cleaned up at the parsing stage.
Revert this aspect of that change.

Signed-off-by: Stephen Finucane <stephen@that.guru>
Fixes: 01b9cbb9 ("Include all email headers in mboxes")

patchwork/views/utils.py
@@ -101,6 +101,9 @@ def _submission_to_mbox(submission):
     for key, val in orig_headers.items():
         mail[key] = val
 
+    # specifically overwrite the subject with our own nicely formatted name
+    mail['Subject'] = submission.name
+
     if 'Date' not in mail:
         mail['Date'] = email.utils.formatdate(utc_timestamp)
 