getpatchwork/patchwork

commit 26f22a87cdd80018f1cbd2de3bfb822f9018ccba

models: Add a backreference for a user's bundles
This is more intuitive.

Signed-off-by: Stephen Finucane <stephen@that.guru>

patchwork/views/user.py
@@ -111,9 +111,8 @@ def profile(request):
     else:
         form = UserProfileForm(instance=request.user.profile)
 
-    # TODO(stephenfin): Add a related_name for User->Bundle
     context = {
-        'bundles': Bundle.objects.filter(owner=request.user),
+        'bundles': request.user.bundles.all(),
         'profileform': form,
     }
 

patchwork/migrations/0026_add_user_bundles_backref.py
@@ -1,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Generated by Django 1.11.10 on 2018-04-12 11:59
+from __future__ import unicode_literals
+
+from django.conf import settings
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('patchwork', '0025_add_regex_validators'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='bundle',
+            name='owner',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='bundles', related_query_name='bundle', to=settings.AUTH_USER_MODEL),
+        ),
+    ]

patchwork/models.py
@@ -778,7 +778,9 @@ class SeriesReference(models.Model):
 
 
 class Bundle(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE,
+                              related_name='bundles',
+                              related_query_name='bundle')
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     name = models.CharField(max_length=50, null=False, blank=False)
     patches = models.ManyToManyField(Patch, through='BundlePatch')

patchwork/views/bundle.py
@@ -71,10 +71,10 @@ def bundle_list(request, project_id=None):
                 bundle.delete()
 
     if project_id is None:
-        bundles = Bundle.objects.filter(owner=request.user)
+        bundles = request.user.bundles.all()
     else:
         project = get_object_or_404(Project, linkname=project_id)
-        bundles = Bundle.objects.filter(owner=request.user, project=project)
+        bundles = request.user.bundles.filter(project=project)
 
     for bundle in bundles:
         bundle.delete_form = DeleteBundleForm(auto_id=False,

patchwork/views/patch.py
@@ -44,7 +44,7 @@ def patch_list(request, project_id):
                            view_args={'project_id': project.linkname})
 
     if is_authenticated(request.user):
-        context['bundles'] = Bundle.objects.filter(owner=request.user)
+        context['bundles'] = request.user.bundles.all()
 
     return render(request, 'patchwork/list.html', context)
 
@@ -112,7 +112,7 @@ def patch_detail(request, patch_id):
                 messages.success(request, 'Patch updated')
 
     if is_authenticated(request.user):
-        context['bundles'] = Bundle.objects.filter(owner=request.user)
+        context['bundles'] = request.user.bundles.all()
 
     context['submission'] = patch
     context['patchform'] = form

commit 6c8dbc7214cab869c00e95250deb059273c3f963

Resolve pep8 issues
Signed-off-by: Stephen Finucane <stephen@that.guru>
Fixes: 26f22a87 ("models: Add a backreference for a user's bundles")

patchwork/views/user.py
@@ -34,7 +34,6 @@ from patchwork.filters import DelegateFilter
 from patchwork.forms import EmailForm
 from patchwork.forms import RegistrationForm
 from patchwork.forms import UserProfileForm
-from patchwork.models import Bundle
 from patchwork.models import EmailConfirmation
 from patchwork.models import EmailOptout
 from patchwork.models import Person