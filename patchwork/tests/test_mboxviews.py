getpatchwork/patchwork

commit e2dfd490d1250ae7cba58377903b2bcba082e75d

views: Add 'series' parameter to '/mbox' endpoint
This allows a user to download dependencies for a patch without having
to do it manually. This is primarily aimed at users testing patches.

Signed-off-by: Stephen Finucane <stephen@that.guru>
Reviewed-by: Daniel Axtens <dja@axtens.net>

patchwork/views/patch.py
@@ -34,6 +34,7 @@ from patchwork.models import Project
 from patchwork.models import Submission
 from patchwork.views import generic_list
 from patchwork.views.utils import patch_to_mbox
+from patchwork.views.utils import series_patch_to_mbox
 
 
 def patch_list(request, project_id):
@@ -120,6 +121,7 @@ def patch_detail(request, patch_id):
 
 def patch_raw(request, patch_id):
     patch = get_object_or_404(Patch, id=patch_id)
+
     response = HttpResponse(content_type="text/x-patch")
     response.write(patch.diff)
     response['Content-Disposition'] = 'attachment; filename=' + \
@@ -130,8 +132,13 @@ def patch_raw(request, patch_id):
 
 def patch_mbox(request, patch_id):
     patch = get_object_or_404(Patch, id=patch_id)
-    response = HttpResponse(content_type="text/plain")
-    response.write(patch_to_mbox(patch))
+    series_num = request.GET.get('series')
+
+    response = HttpResponse(content_type='text/plain')
+    if series_num:
+        response.write(series_patch_to_mbox(patch, series_num))
+    else:
+        response.write(patch_to_mbox(patch))
     response['Content-Disposition'] = 'attachment; filename=' + \
         patch.filename.replace(';', '').replace('\n', '')
 

patchwork/views/utils.py
@@ -26,9 +26,11 @@ from email.parser import HeaderParser
 import email.utils
 import re
 
+from django.http import Http404
 from django.utils import six
 
 from patchwork.models import Comment
+from patchwork.models import Series
 
 
 class PatchMbox(MIMENonMultipart):
@@ -116,3 +118,36 @@ def bundle_to_mbox(bundle):
         A string for the mbox file.
     """
     return '\n'.join([patch_to_mbox(p) for p in bundle.ordered_patches()])
+
+
+def series_patch_to_mbox(patch, series_num):
+    """Get an mbox representation of a patch with dependencies.
+
+    Arguments:
+        patch: The Patch object to convert.
+        series_num: The series number to retrieve dependencies from.
+
+    Returns:
+        A string for the mbox file.
+    """
+    try:
+        series_num = int(series_num)
+    except ValueError:
+        raise Http404('Expected integer series value. Received: %r' %
+                      series_num)
+
+    try:
+        series = patch.series.get(id=series_num)
+    except Series.DoesNotExist:
+        raise Http404('Patch does not belong to series %d' % series_num)
+
+    mbox = []
+
+    # get the series-ified patch
+    number = series.seriespatch_set.get(patch=patch).number
+    for dep in series.seriespatch_set.filter(number__lt=number):
+        mbox.append(patch_to_mbox(dep.patch))
+
+    mbox.append(patch_to_mbox(patch))
+
+    return '\n'.join(mbox)

commit c975e20d3b5ba503df5fa1ea9f9a7490f3f223ae

views: Raise 404 if downloading non-existent dependencies
If a patch was processed by Patchwork before series support was added,
it will not have a series associated with it. As a result, it is not
possible to extract the dependencies for that patch from the series and
a 404 should be raised. This was not previously handled correctly.

Signed-off-by: Stephen Finucane <stephen@that.guru>
Reviewed-by: Daniel Axtens <dja@axtens.net>
Reported-by: John McNamara <john.mcnamara@intel.com>
Fixes: e2dfd490 ("views: Add 'series' parameter to '/mbox' endpoint")
Closes: #189

patchwork/views/patch.py
@@ -139,6 +139,11 @@ def patch_mbox(request, patch_id):
 
     response = HttpResponse(content_type='text/plain')
     if series_id:
+        if not patch.series.count():
+            raise Http404('Patch does not have an associated series. This is '
+                          'because the patch was processed with an older '
+                          'version of Patchwork. It is not possible to '
+                          'provide dependencies for this patch.')
         response.write(series_patch_to_mbox(patch, series_id))
     else:
         response.write(patch_to_mbox(patch))

releasenotes/notes/issue-189-fe4024f33e1b5203.yaml
@@ -1,0 +1,7 @@
+---
+fixes:
+  - |
+    If a patch was processed by Patchwork before series support was added, it
+    will not have a series associated with it. As a result, it is not possible
+    to extract the dependencies for that patch from the series. This was not
+    previously handled correctly. A 404 is now raised if this occurs.

patchwork/tests/test_mboxviews.py
@@ -31,6 +31,7 @@ from patchwork.tests.utils import create_comment
 from patchwork.tests.utils import create_patch
 from patchwork.tests.utils import create_project
 from patchwork.tests.utils import create_person
+from patchwork.tests.utils import create_series_patch
 from patchwork.tests.utils import create_user
 
 
@@ -206,3 +207,26 @@ class MboxCommentPostcriptUnchangedTest(TestCase):
 
         self.assertContains(response, content)
         self.assertNotContains(response, content + '\n')
+
+
+class MboxSeriesDependencies(TestCase):
+
+    def test_patch_with_dependencies(self):
+        patch_a = create_series_patch()
+        patch_b = create_series_patch(series=patch_a.series)
+
+        response = self.client.get('%s?series=*' % reverse(
+            'patch-mbox', args=[patch_b.patch.id]))
+
+        self.assertContains(response, patch_a.patch.content)
+        self.assertContains(response, patch_b.patch.content)
+
+    def test_legacy_patch(self):
+        """Validate a patch with non-existent dependencies raises a 404."""
+        # we're explicitly creating a patch without a series
+        patch = create_patch()
+
+        response = self.client.get('%s?series=*' % reverse(
+            'patch-mbox', args=[patch.id]))
+
+        self.assertEqual(response.status_code, 404)