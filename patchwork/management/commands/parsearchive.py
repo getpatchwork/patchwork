getpatchwork/patchwork

commit 133091da0a527ef8fe89aea38c4855973b48d5f1

parsearchive: Fix logging
We should use a counter normally, avoid using the counter and emit logs
when more detailed output is requested, and emit nothing when no output
is requested.

In addition, the default logging level for the parser module is set to
'WARNING' to make it less chatty.

Signed-off-by: Stephen Finucane <stephen@that.guru>

patchwork/management/commands/parsearchive.py
@@ -37,10 +37,24 @@ class Command(BaseCommand):
         dropped = 0
         errors = 0
 
+        verbosity = int(options['verbosity'])
+        if not verbosity:
+            level = logging.CRITICAL
+        elif verbosity == 1:
+            level = logging.ERROR
+        elif verbosity == 2:
+            level = logging.INFO
+        else:  # verbosity == 3
+            level = logging.DEBUG
+
+        if level:
+            logger.setLevel(level)
+            logging.getLogger('patchwork.parser').setLevel(level)
+
         # TODO(stephenfin): Support passing via stdin
         path = args and args[0] or options['infile']
         if not os.path.exists(path):
-            self.stdout.write('Invalid path: %s' % path)
+            logger.error('Invalid path: %s', path)
             sys.exit(1)
 
         # assume if <infile> is a directory, then we're passing a maildir
@@ -65,7 +79,7 @@ class Command(BaseCommand):
             for m in mbox:
                 pass
         except AttributeError:
-            logger.warning('Broken mbox/Maildir, aborting')
+            logger.error('Broken mbox/Maildir, aborting')
             return
 
         logger.info('Parsing %d mails', count)
@@ -81,9 +95,14 @@ class Command(BaseCommand):
                 # somewhere for future reference?
                 errors += 1
 
-            if (i % 10) == 0:
+            if verbosity < 3 and (i % 10) == 0:
                 self.stdout.write('%06d/%06d\r' % (i, count), ending='')
                 self.stdout.flush()
+
+        mbox.close()
+
+        if not verbosity:
+            return
 
         self.stdout.write(
             'Processed %(total)d messages -->\n'
@@ -101,4 +120,3 @@ class Command(BaseCommand):
                 'errors': errors,
                 'new': count - dropped - errors,
             })
-        mbox.close()

patchwork/parser.py
@@ -950,7 +950,7 @@ def parse_mail(mail, list_id=None):
 
     hint = clean_header(mail.get('X-Patchwork-Hint', ''))
     if hint and hint.lower() == 'ignore':
-        logger.debug("Ignoring email due to 'ignore' hint")
+        logger.info("Ignoring email due to 'ignore' hint")
         return
 
     project = find_project(mail, list_id)

patchwork/settings/base.py
@@ -177,17 +177,17 @@ LOGGING = {
     'loggers': {
         'django': {
             'handlers': ['console'],
-            'level': 'INFO',
+            'level': 'WARNING',
             'propagate': True,
         },
         'patchwork.parser': {
             'handlers': ['console'],
-            'level': 'DEBUG',
+            'level': 'WARNING',
             'propagate': False,
         },
         'patchwork.management.commands': {
             'handlers': ['console', 'mail_admins'],
-            'level': 'INFO',
+            'level': 'WARNING',
             'propagate': True,
         },
     },

commit aa266a28c6a022ad6952fac8f36afff0c540dccf

settings: Don't configure logging for parsearchive
A recent change added additional ERROR level logs to the 'parsearchive'
tool. This highlighted an issue, whereby we had configured all modules
in 'patchwork.management.command' to email administrators on ERROR logs.
We clearly shouldn't be doing this for the 'parsearchive' command or for
anything other than 'parsemail', so fix this.

Along the way, we remove a now-unnecessary 'extra' argument to one of
the logging calls in 'parsearchive' and resolve a pep8 issue.

Signed-off-by: Stephen Finucane <stephen@that.guru>
Fixes: 133091da ("parsearchive: Fix logging")
Cc: Daniel Axtens <dja@axtens.net>

patchwork/management/commands/parsearchive.py
@@ -97,8 +97,7 @@ class Command(BaseCommand):
                 logger.warning('Duplicate mail for message ID %s', exc.msgid)
             except (ValueError, Exception) as exc:
                 errors += 1
-                logger.warning('Invalid mail: %s', exc.message,
-                               extra={'mail': mail.as_string()})
+                logger.warning('Invalid mail: %s', exc.message)
 
             if verbosity < 3 and (i % 10) == 0:
                 self.stdout.write('%06d/%06d\r' % (i, count), ending='')

patchwork/parser.py
@@ -1153,7 +1153,6 @@ def parse_mail(mail, list_id=None):
 
     author = get_or_create_author(mail)
 
-
     try:
         comment = Comment.objects.create(
             submission=submission,

patchwork/settings/base.py
@@ -185,7 +185,7 @@ LOGGING = {
             'level': 'WARNING',
             'propagate': False,
         },
-        'patchwork.management.commands': {
+        'patchwork.management.commands.parsemail': {
             'handlers': ['console', 'mail_admins'],
             'level': 'WARNING',
             'propagate': True,