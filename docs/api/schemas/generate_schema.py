7a557ae9ea09f5d1d3801586336f2743fbc7ccea
Stephen Finucane <stephen@that.guru>
2018-10-27 00:49:17 +0100

    docs: Make API document versioned
    OpenAPI doesn't appear to support versioning natively, suggesting
    instead that separate documents are kept. Rather than doing this
    manually, let's use a templating tool - Jinja2, in this case - to
    generate these document for us from a single master document.
    
    Note that while we can now auto-generate these whenever we need them
    (and we tend to avoid storing auto-generated assets in VCS), these
    change so rarely that it's easier to just store them. This also means we
    can reference the schemas themselves online. We do this in a following
    change.
    
    Signed-off-by: Stephen Finucane <stephen@that.guru>

---
+++ docs/api/schemas/generate_schema.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import os
+
+import jinja2
+
+ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
+VERSIONS = [(1, 0), (1, 1), None]
+LATEST_VERSION = (1, 1)
+
+
+def generate_schema():
+    env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(ROOT_DIR),
+        trim_blocks=True,
+        lstrip_blocks=True)
+    template = env.get_template('patchwork.j2')
+
+    for version in VERSIONS:
+        version_dir = os.path.join(
+            ROOT_DIR, 'v%d.%d' % version if version else 'latest')
+
+        if not os.path.exists(version_dir):
+            os.mkdir(version_dir)
+
+        version_str = '%d.%d' % (version or LATEST_VERSION)
+        version_url = '%d.%d/' % version if version else ''
+        version = version or LATEST_VERSION
+
+        with open(os.path.join(version_dir, 'patchwork.yaml'), 'wb') as fh:
+            template.stream(version=version, version_str=version_str,
+                            version_url=version_url).dump(fh, encoding='utf-8')
+
+
+if __name__ == '__main__':
+    generate_schema()
