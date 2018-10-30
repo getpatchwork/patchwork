getpatchwork/patchwork

commit 780b1a2d07adea0ae2372665b2caefc85f432c9f

views: Use select_related, not prefetch_related
Now that there's a 1:N rather than N:M relationship between series and
patches, we should be using select_related rather than prefetch_related.

Tested-by: Daniel Axtens <dja@axtens.net>
Signed-off-by: Stephen Finucane <stephen@that.guru>
Closes: #223
Fixes: 76505e91 ("models: Convert Series-Patch relationship to 1:N")
Signed-off-by: Daniel Axtens <dja@axtens.net>

patchwork/views/__init__.py
@@ -273,17 +273,16 @@ def generic_list(request, project, view, view_args=None, filter_settings=None,
 
     # but we will need to follow the state and submitter relations for
     # rendering the list template
-    patches = patches.select_related('state', 'submitter', 'delegate')
+    patches = patches.select_related('state', 'submitter', 'delegate',
+                                     'series')
 
     patches = patches.only('state', 'submitter', 'delegate', 'project',
-                           'name', 'date')
+                           'series__name', 'name', 'date')
 
     # we also need checks and series
     patches = patches.prefetch_related(
         Prefetch('check_set', queryset=Check.objects.only(
             'context', 'user_id', 'patch_id', 'state', 'date')))
-    patches = patches.prefetch_related(
-        Prefetch('series', queryset=Series.objects.only('name')))
 
     paginator = Paginator(request, patches)
 

commit ee20fdea757e393f5951aaf640331176fc6cb807

views: no longer import Series in __init__.py
Fix a pep8 error due to an unused import.

Fixes: 780b1a2d07ad ("views: Use select_related, not prefetch_related")
Signed-off-by: Daniel Axtens <dja@axtens.net>

patchwork/views/__init__.py
@@ -14,7 +14,6 @@ from patchwork.models import BundlePatch
 from patchwork.models import Patch
 from patchwork.models import Project
 from patchwork.models import Check
-from patchwork.models import Series
 from patchwork.paginator import Paginator
 
 