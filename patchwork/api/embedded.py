getpatchwork/patchwork

commit 9c179bf4ce968f87b1e960063f4a6dbb9cfae0f9

REST: Add 'web_url' link to API responses
This provides an easy way for clients to navigate to the web view. The
URL is added to four resources: bundles, comments, cover letters and
series. We could also extend this to projects and users in the future,
but the latter would require renaming an existing property while the
latter would require a public "user" page which does not currently
exists.

Signed-off-by: Stephen Finucane <stephen@that.guru>
Reviewed-by: Daniel Axtens <dja@axtens.net>

patchwork/api/embedded.py
@@ -32,7 +32,7 @@ from patchwork import models
 
 
 class MboxMixin(BaseHyperlinkedModelSerializer):
-    """Embed an link to the mbox URL.
+    """Embed a link to the mbox URL.
 
     This field is just way too useful to leave out of even the embedded
     serialization.
@@ -45,12 +45,25 @@ class MboxMixin(BaseHyperlinkedModelSerializer):
         return request.build_absolute_uri(instance.get_mbox_url())
 
 
-class BundleSerializer(MboxMixin, BaseHyperlinkedModelSerializer):
+class WebURLMixin(BaseHyperlinkedModelSerializer):
+    """Embed a link to the web URL."""
+
+    web_url = SerializerMethodField()
+
+    def get_web_url(self, instance):
+        request = self.context.get('request')
+        return request.build_absolute_uri(instance.get_absolute_url())
+
+
+class BundleSerializer(MboxMixin, WebURLMixin, BaseHyperlinkedModelSerializer):
 
     class Meta:
         model = models.Bundle
-        fields = ('id', 'url', 'name', 'mbox')
+        fields = ('id', 'url', 'web_url', 'name', 'mbox')
         read_only_fields = fields
+        versioned_field = {
+            '1.1': ('web_url', ),
+        }
         extra_kwargs = {
             'url': {'view_name': 'api-bundle-detail'},
         }
@@ -75,26 +88,30 @@ class CheckSerializer(BaseHyperlinkedModelSerializer):
         }
 
 
-class CoverLetterSerializer(MboxMixin, BaseHyperlinkedModelSerializer):
+class CoverLetterSerializer(MboxMixin, WebURLMixin,
+                            BaseHyperlinkedModelSerializer):
 
     class Meta:
         model = models.CoverLetter
-        fields = ('id', 'url', 'msgid', 'date', 'name', 'mbox')
+        fields = ('id', 'url', 'web_url', 'msgid', 'date', 'name', 'mbox')
         read_only_fields = fields
         versioned_field = {
-            '1.1': ('mbox', ),
+            '1.1': ('web_url', 'mbox', ),
         }
         extra_kwargs = {
             'url': {'view_name': 'api-cover-detail'},
         }
 
 
-class PatchSerializer(MboxMixin, BaseHyperlinkedModelSerializer):
+class PatchSerializer(MboxMixin, WebURLMixin, BaseHyperlinkedModelSerializer):
 
     class Meta:
         model = models.Patch
-        fields = ('id', 'url', 'msgid', 'date', 'name', 'mbox')
+        fields = ('id', 'url', 'web_url', 'msgid', 'date', 'name', 'mbox')
         read_only_fields = fields
+        versioned_field = {
+            '1.1': ('web_url', ),
+        }
         extra_kwargs = {
             'url': {'view_name': 'api-patch-detail'},
         }
@@ -127,12 +144,16 @@ class ProjectSerializer(BaseHyperlinkedModelSerializer):
         }
 
 
-class SeriesSerializer(MboxMixin, BaseHyperlinkedModelSerializer):
+class SeriesSerializer(MboxMixin, WebURLMixin,
+                       BaseHyperlinkedModelSerializer):
 
     class Meta:
         model = models.Series
         fields = ('id', 'url', 'date', 'name', 'version', 'mbox')
         read_only_fields = fields
+        versioned_field = {
+            '1.1': ('web_url', ),
+        }
         extra_kwargs = {
             'url': {'view_name': 'api-series-detail'},
         }

patchwork/api/bundle.py
@@ -20,9 +20,9 @@
 from django.db.models import Q
 from rest_framework.generics import ListAPIView
 from rest_framework.generics import RetrieveAPIView
-from rest_framework.serializers import HyperlinkedModelSerializer
 from rest_framework.serializers import SerializerMethodField
 
+from patchwork.api.base import BaseHyperlinkedModelSerializer
 from patchwork.api.base import PatchworkPermission
 from patchwork.api.filters import BundleFilterSet
 from patchwork.api.embedded import PatchSerializer
@@ -32,12 +32,17 @@ from patchwork.compat import is_authenticated
 from patchwork.models import Bundle
 
 
-class BundleSerializer(HyperlinkedModelSerializer):
+class BundleSerializer(BaseHyperlinkedModelSerializer):
 
+    web_url = SerializerMethodField()
     project = ProjectSerializer(read_only=True)
     mbox = SerializerMethodField()
     owner = UserSerializer(read_only=True)
     patches = PatchSerializer(many=True, read_only=True)
+
+    def get_web_url(self, instance):
+        request = self.context.get('request')
+        return request.build_absolute_uri(instance.get_absolute_url())
 
     def get_mbox(self, instance):
         request = self.context.get('request')
@@ -45,9 +50,12 @@ class BundleSerializer(HyperlinkedModelSerializer):
 
     class Meta:
         model = Bundle
-        fields = ('id', 'url', 'project', 'name', 'owner', 'patches',
-                  'public', 'mbox')
+        fields = ('id', 'url', 'web_url', 'project', 'name', 'owner',
+                  'patches', 'public', 'mbox')
         read_only_fields = ('owner', 'patches', 'mbox')
+        versioned_fields = {
+            '1.1': ('web_url', ),
+        }
         extra_kwargs = {
             'url': {'view_name': 'api-bundle-detail'},
         }

patchwork/api/comment.py
@@ -30,9 +30,14 @@ from patchwork.models import Comment
 
 class CommentListSerializer(BaseHyperlinkedModelSerializer):
 
+    web_url = SerializerMethodField()
     subject = SerializerMethodField()
     headers = SerializerMethodField()
     submitter = PersonSerializer(read_only=True)
+
+    def get_web_url(self, instance):
+        request = self.context.get('request')
+        return request.build_absolute_uri(instance.get_absolute_url())
 
     def get_subject(self, comment):
         return email.parser.Parser().parsestr(comment.headers,
@@ -54,9 +59,12 @@ class CommentListSerializer(BaseHyperlinkedModelSerializer):
 
     class Meta:
         model = Comment
-        fields = ('id', 'msgid', 'date', 'subject', 'submitter', 'content',
-                  'headers')
+        fields = ('id', 'web_url', 'msgid', 'date', 'subject', 'submitter',
+                  'content', 'headers')
         read_only_fields = fields
+        versioned_fields = {
+            '1.1': ('web_url', ),
+        }
 
 
 class CommentList(ListAPIView):

patchwork/api/cover.py
@@ -34,11 +34,16 @@ from patchwork.models import CoverLetter
 
 class CoverLetterListSerializer(BaseHyperlinkedModelSerializer):
 
+    web_url = SerializerMethodField()
     project = ProjectSerializer(read_only=True)
     submitter = PersonSerializer(read_only=True)
     mbox = SerializerMethodField()
     series = SeriesSerializer(many=True, read_only=True)
     comments = SerializerMethodField()
+
+    def get_web_url(self, instance):
+        request = self.context.get('request')
+        return request.build_absolute_uri(instance.get_absolute_url())
 
     def get_mbox(self, instance):
         request = self.context.get('request')
@@ -50,11 +55,11 @@ class CoverLetterListSerializer(BaseHyperlinkedModelSerializer):
 
     class Meta:
         model = CoverLetter
-        fields = ('id', 'url', 'project', 'msgid', 'date', 'name', 'submitter',
-                  'mbox', 'series', 'comments')
+        fields = ('id', 'url', 'web_url', 'project', 'msgid', 'date', 'name',
+                  'submitter', 'mbox', 'series', 'comments')
         read_only_fields = fields
         versioned_fields = {
-            '1.1': ('mbox', 'comments'),
+            '1.1': ('web_url', 'mbox', 'comments'),
         }
         extra_kwargs = {
             'url': {'view_name': 'api-cover-detail'},

patchwork/api/patch.py
@@ -77,6 +77,7 @@ class StateField(RelatedField):
 
 class PatchListSerializer(BaseHyperlinkedModelSerializer):
 
+    web_url = SerializerMethodField()
     project = ProjectSerializer(read_only=True)
     state = StateField()
     submitter = PersonSerializer(read_only=True)
@@ -87,6 +88,10 @@ class PatchListSerializer(BaseHyperlinkedModelSerializer):
     check = SerializerMethodField()
     checks = SerializerMethodField()
     tags = SerializerMethodField()
+
+    def get_web_url(self, instance):
+        request = self.context.get('request')
+        return request.build_absolute_uri(instance.get_absolute_url())
 
     def get_mbox(self, instance):
         request = self.context.get('request')
@@ -110,15 +115,15 @@ class PatchListSerializer(BaseHyperlinkedModelSerializer):
 
     class Meta:
         model = Patch
-        fields = ('id', 'url', 'project', 'msgid', 'date', 'name',
+        fields = ('id', 'url', 'web_url', 'project', 'msgid', 'date', 'name',
                   'commit_ref', 'pull_url', 'state', 'archived', 'hash',
                   'submitter', 'delegate', 'mbox', 'series', 'comments',
                   'check', 'checks', 'tags')
-        read_only_fields = ('project', 'msgid', 'date', 'name', 'hash',
-                            'submitter', 'mbox', 'mbox', 'series', 'comments',
+        read_only_fields = ('web_url', 'project', 'msgid', 'date', 'name',
+                            'hash', 'submitter', 'mbox', 'series', 'comments',
                             'check', 'checks', 'tags')
         versioned_fields = {
-            '1.1': ('comments', ),
+            '1.1': ('comments', 'web_url'),
         }
         extra_kwargs = {
             'url': {'view_name': 'api-patch-detail'},

patchwork/api/series.py
@@ -19,9 +19,9 @@
 
 from rest_framework.generics import ListAPIView
 from rest_framework.generics import RetrieveAPIView
-from rest_framework.serializers import HyperlinkedModelSerializer
 from rest_framework.serializers import SerializerMethodField
 
+from patchwork.api.base import BaseHyperlinkedModelSerializer
 from patchwork.api.base import PatchworkPermission
 from patchwork.api.filters import SeriesFilterSet
 from patchwork.api.embedded import CoverLetterSerializer
@@ -31,13 +31,18 @@ from patchwork.api.embedded import ProjectSerializer
 from patchwork.models import Series
 
 
-class SeriesSerializer(HyperlinkedModelSerializer):
+class SeriesSerializer(BaseHyperlinkedModelSerializer):
 
+    web_url = SerializerMethodField()
     project = ProjectSerializer(read_only=True)
     submitter = PersonSerializer(read_only=True)
     mbox = SerializerMethodField()
     cover_letter = CoverLetterSerializer(read_only=True)
     patches = PatchSerializer(read_only=True, many=True)
+
+    def get_web_url(self, instance):
+        request = self.context.get('request')
+        return request.build_absolute_uri(instance.get_absolute_url())
 
     def get_mbox(self, instance):
         request = self.context.get('request')
@@ -45,11 +50,14 @@ class SeriesSerializer(HyperlinkedModelSerializer):
 
     class Meta:
         model = Series
-        fields = ('id', 'url', 'project', 'name', 'date', 'submitter',
-                  'version', 'total', 'received_total', 'received_all',
-                  'mbox', 'cover_letter', 'patches')
+        fields = ('id', 'url', 'web_url', 'project', 'name', 'date',
+                  'submitter', 'version', 'total', 'received_total',
+                  'received_all', 'mbox', 'cover_letter', 'patches')
         read_only_fields = ('date', 'submitter', 'total', 'received_total',
                             'received_all', 'mbox', 'cover_letter', 'patches')
+        versioned_fields = {
+            '1.1': ('web_url', ),
+        }
         extra_kwargs = {
             'url': {'view_name': 'api-series-detail'},
         }

patchwork/models.py
@@ -412,6 +412,9 @@ class SeriesMixin(object):
 
 class CoverLetter(SeriesMixin, Submission):
 
+    def get_absolute_url(self):
+        return reverse('cover-detail', kwargs={'cover_id': self.id})
+
     def get_mbox_url(self):
         return reverse('cover-mbox', kwargs={'cover_id': self.id})
 
@@ -603,6 +606,9 @@ class Comment(EmailMixin, models.Model):
                                    related_query_name='comment',
                                    on_delete=models.CASCADE)
 
+    def get_absolute_url(self):
+        return reverse('comment-redirect', kwargs={'comment_id': self.id})
+
     def save(self, *args, **kwargs):
         super(Comment, self).save(*args, **kwargs)
         if hasattr(self.submission, 'patch'):
@@ -727,6 +733,12 @@ class Series(FilenameMixin, models.Model):
         return SeriesPatch.objects.create(series=self,
                                           patch=patch,
                                           number=number)
+
+    def get_absolute_url(self):
+        # TODO(stephenfin): We really need a proper series view
+        return reverse('patch-list',
+                       kwargs={'project_id': self.project.linkname}) + (
+            '?series=%d' % self.id)
 
     def get_mbox_url(self):
         return reverse('series-mbox', kwargs={'series_id': self.id})

patchwork/tests/api/test_bundle.py
@@ -41,16 +41,22 @@ class TestBundleAPI(APITestCase):
     fixtures = ['default_tags']
 
     @staticmethod
-    def api_url(item=None):
+    def api_url(item=None, version=None):
+        kwargs = {}
+        if version:
+            kwargs['version'] = version
+
         if item is None:
-            return reverse('api-bundle-list')
-        return reverse('api-bundle-detail', args=[item])
+            return reverse('api-bundle-list', kwargs=kwargs)
+        kwargs['pk'] = item
+        return reverse('api-bundle-detail', kwargs=kwargs)
 
     def assertSerialized(self, bundle_obj, bundle_json):
         self.assertEqual(bundle_obj.id, bundle_json['id'])
         self.assertEqual(bundle_obj.name, bundle_json['name'])
         self.assertEqual(bundle_obj.public, bundle_json['public'])
         self.assertIn(bundle_obj.get_mbox_url(), bundle_json['mbox'])
+        self.assertIn(bundle_obj.get_absolute_url(), bundle_json['web_url'])
 
         # nested fields
 
@@ -109,6 +115,16 @@ class TestBundleAPI(APITestCase):
         resp = self.client.get(self.api_url(), {'owner': 'otheruser'})
         self.assertEqual(0, len(resp.data))
 
+    def test_list_version_1_0(self):
+        """Validate that newer fields are dropped for older API versions."""
+        create_bundle(public=True)
+
+        resp = self.client.get(self.api_url(version='1.0'))
+        self.assertEqual(status.HTTP_200_OK, resp.status_code)
+        self.assertEqual(1, len(resp.data))
+        self.assertIn('url', resp.data[0])
+        self.assertNotIn('web_url', resp.data[0])
+
     def test_detail(self):
         """Validate we can get a specific bundle."""
         bundle = create_bundle(public=True)
@@ -116,6 +132,13 @@ class TestBundleAPI(APITestCase):
         resp = self.client.get(self.api_url(bundle.id))
         self.assertEqual(status.HTTP_200_OK, resp.status_code)
         self.assertSerialized(bundle, resp.data)
+
+    def test_detail_version_1_0(self):
+        bundle = create_bundle(public=True)
+
+        resp = self.client.get(self.api_url(bundle.id, version='1.0'))
+        self.assertIn('url', resp.data)
+        self.assertNotIn('web_url', resp.data)
 
     def test_create_update_delete(self):
         """Ensure creates, updates and deletes aren't allowed"""

commit ee58fb3be6eaa0deeae966ef3b95fc757df4519b

REST: Show 'web_url' in embedded series responses
Signed-off-by: Stephen Finucane <stephen@that.guru>
Closes: #224
Fixes: 9c179bf4c ("REST: Add 'web_url' link to API responses")

patchwork/api/embedded.py
@@ -187,7 +187,8 @@ class SeriesSerializer(SerializedRelatedField):
 
         class Meta:
             model = models.Series
-            fields = ('id', 'url', 'date', 'name', 'version', 'mbox')
+            fields = ('id', 'url', 'web_url', 'date', 'name', 'version',
+                      'mbox')
             read_only_fields = fields
             versioned_field = {
                 '1.1': ('web_url', ),

releasenotes/notes/issue-224-8f1c4207aa273ac6.yaml
@@ -1,0 +1,5 @@
+---
+fixes:
+  - |
+    API resources with embedded series were not showing the ``web_url`` value
+    for these series. This is now shown.