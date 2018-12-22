getpatchwork/patchwork

commit 76505e910d7da46e94fb136cfcd299f29a30a138

models: Convert Series-Patch relationship to 1:N
Late in the development of the series feature, it was decided that there
were advantages to allowing an N:M relationship between series and
patches. This would allow us to do things like create complete series
where a sole vN patch was sent to a list rather than the full series.
After some time using series in the wild, it's apparent that such
features are very difficult to implement correctly and will likely never
be implemented. As such, it's time to start cleaning up the mess, paving
the way for things like an improved tagging feature.

There are some significant changes to the model required:

- models.py, migrations/0027, migrations/0028, migrations/0029

  The migrations make the following changes:

  1. - Add 'Patch.series_alt' and 'Patch.number' fields.
  2. - Populate the 'Patch.series_alt' and 'Patch.number' fields from
       their 'SeriesPatch' equivalents.
  3. - Remove the 'SeriesPatch' model.
     - Rename 'Patch.series_alt' to 'Patch.series'.
     - Change 'Series.cover_letter' to a 'OneToOneField' since a cover
       letter can no longer be assigned to multiple series.

  Note that the migrations have to be split into multiple parts as the
  combined migration raises an OperationalError as below.

    (1072, "Key column 'series_alt_id' doesn't exist in table")

  This is due to Django's penchant for creating indexes for newly
  created fields, as noted here: https://stackoverflow.com/q/35158530/

Aside from the model changes, there are numerous other changes required:

- admin.py

  Reflect model changes for the 'PatchInline' inline used by
  'SeriesAdmin'

- api/cover.py, api/patch.py

  Update the 'series' field for the cover letter and patch resources to
  reflect the model changes. A 'to_representation' function is added in
  both cases to post-process this field and make it look like a list
  again. This is necessary to avoid breaking clients.

- parser.py

patchwork/signals.py
@@ -15,7 +15,6 @@ from patchwork.models import Event
 from patchwork.models import Patch
 from patchwork.models import PatchChangeNotification
 from patchwork.models import Series
-from patchwork.models import SeriesPatch
 
 
 @receiver(pre_save, sender=Patch)
@@ -133,39 +132,46 @@ def create_patch_delegated_event(sender, instance, raw, **kwargs):
     create_event(instance, orig_patch.delegate, instance.delegate)
 
 
-@receiver(post_save, sender=SeriesPatch)
-def create_patch_completed_event(sender, instance, created, raw, **kwargs):
-    """Create patch completed event for patches with series."""
+@receiver(pre_save, sender=Patch)
+def create_patch_completed_event(sender, instance, raw, **kwargs):
 
-    def create_event(patch, series):
+    def create_event(patch):
         return Event.objects.create(
             category=Event.CATEGORY_PATCH_COMPLETED,
             project=patch.project,
             patch=patch,
-            series=series)
+            series=patch.series)
 
-    # don't trigger for items loaded from fixtures or existing items
-    if raw or not created:
+    # don't trigger for items loaded from fixtures, new items or items that
+    # (still) don't have a series
+    if raw or not instance.pk or not instance.series:
+        return
+
+    orig_patch = Patch.objects.get(pk=instance.pk)
+
+    # we don't currently allow users to change a series, though this might
+    # change in the future. However, we handle that here nonetheless
+    if orig_patch.series == instance.series:
         return
 
     # if dependencies not met, don't raise event. There's also no point raising
     # events for successors since they'll have the same issue
-    predecessors = SeriesPatch.objects.filter(
+    predecessors = Patch.objects.filter(
         series=instance.series, number__lt=instance.number)
     if predecessors.count() != instance.number - 1:
         return
 
-    create_event(instance.patch, instance.series)
+    create_event(instance)
 
     # if this satisfies dependencies for successor patch, raise events for
     # those
     count = instance.number + 1
-    for successor in SeriesPatch.objects.filter(
+    for successor in Patch.objects.order_by('number').filter(
             series=instance.series, number__gt=instance.number):
         if successor.number != count:
             break
 
-        create_event(successor.patch, successor.series)
+        create_event(successor)
         count += 1
 
 
@@ -204,14 +210,8 @@ def create_series_created_event(sender, instance, created, raw, **kwargs):
     create_event(instance)
 
 
-@receiver(post_save, sender=SeriesPatch)
+@receiver(post_save, sender=Patch)
 def create_series_completed_event(sender, instance, created, raw, **kwargs):
-
-    # NOTE(stephenfin): We subscribe to the SeriesPatch.post_save signal
-    # instead of Series.m2m_changed to minimize the amount of times this is
-    # fired. The m2m_changed signal doesn't support a 'changed' parameter,
-    # which we could use to quick skip the signal when a patch is merely
-    # updated instead of added to the series.
 
     # NOTE(stephenfin): It's actually possible for this event to be fired
     # multiple times for a given series. To trigger this case, you would need
@@ -229,5 +229,5 @@ def create_series_completed_event(sender, instance, created, raw, **kwargs):
     if raw or not created:
         return
 
-    if instance.series.received_all:
+    if instance.series and instance.series.received_all:
         create_event(instance.series)

patchwork/tests/test_events.py
@@ -34,8 +34,8 @@ class PatchCreateTest(_BaseTestCase):
         """No series, so patch dependencies implicitly exist."""
         patch = utils.create_patch()
 
-        # This should raise both the CATEGORY_PATCH_CREATED and
-        # CATEGORY_PATCH_COMPLETED events as there are no specific dependencies
+        # This should raise the CATEGORY_PATCH_CREATED event only as there is
+        # no series
         events = _get_events(patch=patch)
         self.assertEqual(events.count(), 1)
         self.assertEqual(events[0].category, Event.CATEGORY_PATCH_CREATED)
@@ -56,6 +56,13 @@ class PatchCreateTest(_BaseTestCase):
         self.assertEqual(events[1].project, series_patch.patch.project)
         self.assertEventFields(events[0])
         self.assertEventFields(events[1])
+
+        # This shouldn't be affected by another update to the patch
+        series_patch.patch.commit_ref = 'aac76f0b0f8dd657ff07bb'
+        series_patch.patch.save()
+
+        events = _get_events(patch=series_patch.patch)
+        self.assertEqual(events.count(), 2)
 
     def test_patch_dependencies_out_of_order(self):
         series = utils.create_series()

lib/sql/grant-all.mysql.sql
@@ -27,7 +27,6 @@ GRANT SELECT, UPDATE, INSERT, DELETE ON patchwork_patchtag TO 'www-data'@localho
 GRANT SELECT, UPDATE, INSERT, DELETE ON patchwork_person TO 'www-data'@localhost;
 GRANT SELECT, UPDATE, INSERT, DELETE ON patchwork_project TO 'www-data'@localhost;
 GRANT SELECT, UPDATE, INSERT, DELETE ON patchwork_series TO 'www-data'@localhost;
-GRANT SELECT, UPDATE, INSERT, DELETE ON patchwork_seriespatch TO 'www-data'@localhost;
 GRANT SELECT, UPDATE, INSERT, DELETE ON patchwork_seriesreference TO 'www-data'@localhost;
 GRANT SELECT, UPDATE, INSERT, DELETE ON patchwork_state TO 'www-data'@localhost;
 GRANT SELECT, UPDATE, INSERT, DELETE ON patchwork_submission TO 'www-data'@localhost;
@@ -42,7 +41,6 @@ GRANT INSERT, SELECT ON patchwork_coverletter TO 'nobody'@localhost;
 GRANT INSERT, SELECT ON patchwork_patch TO 'nobody'@localhost;
 GRANT INSERT, SELECT ON patchwork_person TO 'nobody'@localhost;
 GRANT INSERT, SELECT ON patchwork_series TO 'nobody'@localhost;
-GRANT INSERT, SELECT ON patchwork_seriespatch TO 'nobody'@localhost;
 GRANT INSERT, SELECT ON patchwork_seriesreference TO 'nobody'@localhost;
 GRANT INSERT, SELECT ON patchwork_submission TO 'nobody'@localhost;
 GRANT INSERT, SELECT, UPDATE, DELETE ON patchwork_patchtag TO 'nobody'@localhost;

lib/sql/grant-all.postgres.sql
@@ -27,7 +27,6 @@ GRANT SELECT, UPDATE, INSERT, DELETE ON
 	patchwork_person,
 	patchwork_project,
 	patchwork_series,
-	patchwork_seriespatch,
 	patchwork_seriesreference,
 	patchwork_state,
 	patchwork_submission,
@@ -56,7 +55,6 @@ GRANT SELECT, UPDATE ON
 	patchwork_person_id_seq,
 	patchwork_project_id_seq,
 	patchwork_series_id_seq,
-	patchwork_seriespatch_id_seq,
 	patchwork_seriesreference_id_seq,
 	patchwork_state_id_seq,
 	patchwork_tag_id_seq,
@@ -70,7 +68,6 @@ GRANT INSERT, SELECT ON
 	patchwork_comment,
 	patchwork_coverletter,
 	patchwork_event
-	patchwork_seriespatch,
 	patchwork_seriesreference,
 	patchwork_submission,
 TO "nobody";
@@ -93,7 +90,6 @@ GRANT UPDATE, SELECT ON
 	patchwork_patchtag_id_seq,
 	patchwork_person_id_seq,
 	patchwork_series_id_seq,
-	patchwork_seriespatch_id_seq,
 	patchwork_seriesreference_id_seq,
 TO "nobody";
 

patchwork/admin.py
@@ -114,7 +114,7 @@ admin.site.register(Comment, CommentAdmin)
 
 
 class PatchInline(admin.StackedInline):
-    model = Series.patches.through
+    model = Patch
     extra = 0
 
 

patchwork/api/cover.py
@@ -24,7 +24,7 @@ class CoverLetterListSerializer(BaseHyperlinkedModelSerializer):
     project = ProjectSerializer(read_only=True)
     submitter = PersonSerializer(read_only=True)
     mbox = SerializerMethodField()
-    series = SeriesSerializer(many=True, read_only=True)
+    series = SeriesSerializer(read_only=True)
     comments = SerializerMethodField()
 
     def get_web_url(self, instance):
@@ -38,6 +38,15 @@ class CoverLetterListSerializer(BaseHyperlinkedModelSerializer):
     def get_comments(self, cover):
         return self.context.get('request').build_absolute_uri(
             reverse('api-cover-comment-list', kwargs={'pk': cover.id}))
+
+    def to_representation(self, instance):
+        # NOTE(stephenfin): This is here to ensure our API looks the same even
+        # after we changed the series-patch relationship from M:N to 1:N. It
+        # will be removed in API v2
+        data = super(CoverLetterListSerializer, self).to_representation(
+            instance)
+        data['series'] = [data['series']]
+        return data
 
     class Meta:
         model = CoverLetter
@@ -89,8 +98,8 @@ class CoverLetterList(ListAPIView):
     ordering = 'id'
 
     def get_queryset(self):
-        return CoverLetter.objects.all().prefetch_related('series')\
-            .select_related('project', 'submitter')\
+        return CoverLetter.objects.all()\
+            .select_related('project', 'submitter', 'series')\
             .defer('content', 'headers')
 
 
@@ -100,5 +109,5 @@ class CoverLetterDetail(RetrieveAPIView):
     serializer_class = CoverLetterDetailSerializer
 
     def get_queryset(self):
-        return CoverLetter.objects.all().prefetch_related('series')\
-            .select_related('project', 'submitter')
+        return CoverLetter.objects.all()\
+            .select_related('project', 'submitter', 'series')

patchwork/api/patch.py
@@ -70,7 +70,7 @@ class PatchListSerializer(BaseHyperlinkedModelSerializer):
     submitter = PersonSerializer(read_only=True)
     delegate = UserSerializer(allow_null=True)
     mbox = SerializerMethodField()
-    series = SeriesSerializer(many=True, read_only=True)
+    series = SeriesSerializer(read_only=True)
     comments = SerializerMethodField()
     check = SerializerMethodField()
     checks = SerializerMethodField()
@@ -110,6 +110,14 @@ class PatchListSerializer(BaseHyperlinkedModelSerializer):
             raise ValidationError("User '%s' is not a maintainer for project "
                                   "'%s'" % (value, self.instance.project))
         return value
+
+    def to_representation(self, instance):
+        # NOTE(stephenfin): This is here to ensure our API looks the same even
+        # after we changed the series-patch relationship from M:N to 1:N. It
+        # will be removed in API v2
+        data = super(PatchListSerializer, self).to_representation(instance)
+        data['series'] = [data['series']]
+        return data
 
     class Meta:
         model = Patch
@@ -173,8 +181,9 @@ class PatchList(ListAPIView):
 
     def get_queryset(self):
         return Patch.objects.all()\
-            .prefetch_related('series', 'check_set')\
-            .select_related('project', 'state', 'submitter', 'delegate')\
+            .prefetch_related('check_set')\
+            .select_related('project', 'state', 'submitter', 'delegate',
+                            'series')\
             .defer('content', 'diff', 'headers')
 
 
@@ -186,5 +195,6 @@ class PatchDetail(RetrieveUpdateAPIView):
 
     def get_queryset(self):
         return Patch.objects.all()\
-            .prefetch_related('series', 'check_set')\
-            .select_related('project', 'state', 'submitter', 'delegate')
+            .prefetch_related('check_set')\
+            .select_related('project', 'state', 'submitter', 'delegate',
+                            'series')

patchwork/migrations/0031_add_patch_series_fields.py
@@ -1,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.db.models import Count
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('patchwork', '0030_add_submission_covering_index'),
+    ]
+
+    operations = [
+        # Add Patch.series_alt, Patch.number fields. This will store the fields
+        # currently stored in SeriesPatch
+        migrations.AddField(
+            model_name='patch',
+            name='number',
+            field=models.PositiveSmallIntegerField(default=None, help_text=b'The number assigned to this patch in the series', null=True),
+        ),
+        migrations.AddField(
+            model_name='patch',
+            name='series_alt',
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='patchwork.Series'),
+        ),
+        migrations.AlterUniqueTogether(
+            name='patch',
+            unique_together=set([('series_alt', 'number')]),
+        ),
+    ]

commit 087fe500f8d2e7bdad15f2257d7f78a2337cf702

signals: Fix 'series-completed' event
I'm not sure how I ever intended this to work and there were no tests
verifying things. Fix it now and add tests to prevent it regressing.

Signed-off-by: Stephen Finucane <stephen@that.guru>
Fixes: 76505e91 ("models: Convert Series-Patch relationship to 1:N")

patchwork/signals.py
@@ -210,8 +210,8 @@ def create_series_created_event(sender, instance, created, raw, **kwargs):
     create_event(instance)
 
 
-@receiver(post_save, sender=Patch)
-def create_series_completed_event(sender, instance, created, raw, **kwargs):
+@receiver(pre_save, sender=Patch)
+def create_series_completed_event(sender, instance, raw, **kwargs):
 
     # NOTE(stephenfin): It's actually possible for this event to be fired
     # multiple times for a given series. To trigger this case, you would need
@@ -225,9 +225,20 @@ def create_series_completed_event(sender, instance, created, raw, **kwargs):
             project=series.project,
             series=series)
 
-    # don't trigger for items loaded from fixtures or existing items
-    if raw or not created:
+    # don't trigger for items loaded from fixtures, new items or items that
+    # (still) don't have a series
+    if raw or not instance.pk or not instance.series:
         return
 
-    if instance.series and instance.series.received_all:
+    orig_patch = Patch.objects.get(pk=instance.pk)
+
+    # we don't currently allow users to change a series (though this might
+    # change in the future) meaning if the patch already had a series, there's
+    # nothing to notify about
+    if orig_patch.series:
+        return
+
+    # we can't use "series.received_all" here since we haven't actually saved
+    # the instance yet so we duplicate that logic here but with an offset
+    if (instance.series.received_total + 1) >= instance.series.total:
         create_event(instance.series)

patchwork/tests/api/test_event.py
@@ -86,9 +86,7 @@ class TestEventAPI(APITestCase):
 
         resp = self.client.get(self.api_url())
         self.assertEqual(status.HTTP_200_OK, resp.status_code)
-        # FIXME(stephenfin): This should actually return 8 events but
-        # 'series-completed' events are not currently being generated
-        self.assertEqual(7, len(resp.data), [x['category'] for x in resp.data])
+        self.assertEqual(8, len(resp.data), [x['category'] for x in resp.data])
         for event_rsp in resp.data:
             event_obj = events.get(category=event_rsp['category'])
             self.assertSerialized(event_obj, event_rsp)
@@ -101,9 +99,7 @@ class TestEventAPI(APITestCase):
 
         resp = self.client.get(self.api_url(), {'project': project.pk})
         # All but one event belongs to the same project
-        # FIXME(stephenfin): This should actually return 8 events but
-        # 'series-completed' events are not currently being generated
-        self.assertEqual(7, len(resp.data))
+        self.assertEqual(8, len(resp.data))
 
         resp = self.client.get(self.api_url(), {'project': 'invalidproject'})
         self.assertEqual(0, len(resp.data))
@@ -153,9 +149,7 @@ class TestEventAPI(APITestCase):
         resp = self.client.get(self.api_url(), {'series': series.pk})
         # There should be three - series-created, patch-completed and
         # series-completed
-        # FIXME(stephenfin): This should actually return 3 events but
-        # 'series-completed' events are not currently being generated
-        self.assertEqual(2, len(resp.data))
+        self.assertEqual(3, len(resp.data))
 
         resp = self.client.get(self.api_url(), {'series': 999999})
         self.assertEqual(0, len(resp.data))

patchwork/tests/test_events.py
@@ -28,7 +28,7 @@ class _BaseTestCase(TestCase):
                 self.assertIsNone(field)
 
 
-class PatchCreateTest(_BaseTestCase):
+class PatchCreatedTest(_BaseTestCase):
 
     def test_patch_created(self):
         """No series, so patch dependencies implicitly exist."""
@@ -165,7 +165,7 @@ class PatchChangedTest(_BaseTestCase):
         self.assertEventFields(events[3], previous_delegate=delegate_b)
 
 
-class CheckCreateTest(_BaseTestCase):
+class CheckCreatedTest(_BaseTestCase):
 
     def test_check_created(self):
         check = utils.create_check()
@@ -176,7 +176,7 @@ class CheckCreateTest(_BaseTestCase):
         self.assertEventFields(events[0])
 
 
-class CoverCreateTest(_BaseTestCase):
+class CoverCreatedTest(_BaseTestCase):
 
     def test_cover_created(self):
         cover = utils.create_cover()
@@ -187,7 +187,7 @@ class CoverCreateTest(_BaseTestCase):
         self.assertEventFields(events[0])
 
 
-class SeriesCreateTest(_BaseTestCase):
+class SeriesCreatedTest(_BaseTestCase):
 
     def test_series_created(self):
         series = utils.create_series()
@@ -196,3 +196,28 @@ class SeriesCreateTest(_BaseTestCase):
         self.assertEqual(events[0].category, Event.CATEGORY_SERIES_CREATED)
         self.assertEqual(events[0].project, series.project)
         self.assertEventFields(events[0])
+
+
+class SeriesChangedTest(_BaseTestCase):
+
+    def test_series_completed(self):
+        """Validate 'series-completed' events."""
+        series = utils.create_series(total=2)
+
+        # the series has no patches associated with it so it's not yet complete
+        events = _get_events(series=series)
+        self.assertNotIn(Event.CATEGORY_SERIES_COMPLETED,
+                         [x.category for x in events])
+
+        # create the second of two patches in the series; series is still not
+        # complete
+        utils.create_patch(series=series, number=2)
+        events = _get_events(series=series)
+        self.assertNotIn(Event.CATEGORY_SERIES_COMPLETED,
+                         [x.category for x in events])
+
+        # now create the first patch, which will "complete" the series
+        utils.create_patch(series=series, number=1)
+        events = _get_events(series=series)
+        self.assertIn(Event.CATEGORY_SERIES_COMPLETED,
+                      [x.category for x in events])