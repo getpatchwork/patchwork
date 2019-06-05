getpatchwork/patchwork

commit f439f5414206c94d486c60801a86acc57ad3f273

Add delegate filter autocomplete support
It would be helpful to provide autocomplete functionality for the
delegate filter field, similar to that provided for the submitter
field. This will provide a way to handle larger delegate lists without
overloading the user with options.

Add this functionality through use of Selectize, which is already used
to provide similar functionality for filtering of "submitters".

Signed-off-by: Stephen Finucane <stephen.finucane@intel.com>

patchwork/filters.py
@@ -330,82 +330,64 @@ class ArchiveFilter(Filter):
 
 class DelegateFilter(Filter):
     param = 'delegate'
-    no_delegate_key = '-'
-    no_delegate_str = 'Nobody'
-    AnyDelegate = 1
 
     def __init__(self, filters):
         super(DelegateFilter, self).__init__(filters)
         self.name = 'Delegate'
-        self.param = 'delegate'
         self.delegate = None
+        self.delegate_match = None
 
-    def _set_key(self, str):
-        if str == self.no_delegate_key:
-            self.applied = True
-            self.delegate = None
+    def _set_key(self, key):
+        self.delegate = None
+        self.delegate_match = None
+        delegate_id = None
+
+        key = key.strip()
+        if not key:
             return
 
         try:
-            self.delegate = User.objects.get(id=str)
-            self.applied = True
-        except:
+            delegate_id = int(key)
+        except ValueError:
             pass
+        except:
+            return
+
+        if delegate_id:
+            self.delegate = User.objects.get(id=int(key))
+            self.applied = True
+            return
+
+        delegates = User.objects.filter(username__icontains=key)
+        if not delegates:
+            return
+
+        self.delegate_match = key
+        self.applied = True
 
     def kwargs(self):
-        if not self.applied:
-            return {}
-        return {'delegate': self.delegate}
+        if self.delegate:
+            return {'delegate': self.delegate}
+
+        if self.delegate_match:
+            return {'delegate__username__icontains': self.delegate_match}
+        return {}
 
     def condition(self):
         if self.delegate:
-            return self.delegate.profile.name()
-        return self.no_delegate_str
+            return str(self.delegate)
+        elif self.delegate_match:
+            return self.delegate_match
+        return ''
 
     def _form(self):
-        delegates = User.objects.filter(
-            profile__maintainer_projects=self.filters.project)
-
-        str = '<select name="delegate" class="form-control">'
-
-        selected = ''
-        if not self.applied:
-            selected = 'selected'
-
-        str += '<option %s value="">------</option>' % selected
-
-        selected = ''
-        if self.applied and self.delegate is None:
-            selected = 'selected'
-
-        str += '<option %s value="%s">%s</option>' % \
-            (selected, self.no_delegate_key, self.no_delegate_str)
-
-        for d in delegates:
-            selected = ''
-            if d == self.delegate:
-                selected = ' selected'
-
-            str += '<option %s value="%s">%s</option>' % (
-                selected, d.id, d.profile.name())
-        str += '</select>'
-
-        return mark_safe(str)
+        return mark_safe('<input type="text" name="delegate" '
+                         'id="delegate_input" class="form-control">')
 
     def key(self):
         if self.delegate:
             return self.delegate.id
-        if self.applied:
-            return self.no_delegate_key
-        return None
-
-    def set_status(self, *args, **kwargs):
-        if 'delegate' in kwargs:
-            self.applied = self.forced = True
-            self.delegate = kwargs['delegate']
-        if self.AnyDelegate in args:
-            self.applied = False
-            self.forced = True
+        return self.delegate_match
 
 filterclasses = [SubmitterFilter,
                  StateFilter,

patchwork/templates/patchwork/filters.html
@@ -82,6 +82,48 @@ $(document).ready(function() {
         }
     });
 });
+
+
+$(document).ready(function() {
+    $('#delegate_input').selectize({
+        valueField: 'pk',
+        labelField: 'name',
+        searchField: ['name'],
+        plugins: ['enter_key_submit'],
+        maxItems: 1,
+        persist: false,
+        onInitialize: function() {
+            this.on('submit', function() {
+                if (!this.items.length)
+                    this.$input.val(this.lastValue);
+                this.$input.closest('form').submit();
+            }, this);
+        },
+        render: {
+            option: function(item, escape) {
+                if (item.email)
+                    return '<div>' + escape(item.name) + ' &lt;' +
+                                     escape(item.email) + '&gt;' + '</div>';
+                return '<div>' + escape(item.name) + '</div>';
+            },
+            item: function(item, escape) {
+                return '<div>' + escape(item.name) + '</div>';
+            }
+        },
+        load: function(query, callback) {
+            req = $.ajax({
+                url: '{% url 'api-delegates' %}?q=' +
+                      encodeURIComponent(query) + '&l=10',
+                error: function() {
+                    callback();
+                },
+                success: function(res) {
+                    callback(res);
+                }
+            });
+        }
+    });
+});
 </script>
 
 <div class="filters">

patchwork/urls.py
@@ -109,6 +109,7 @@ urlpatterns = [
 
     # submitter autocomplete
     url(r'^submitter/$', api_views.submitters, name='api-submitters'),
+    url(r'^delegate/$', api_views.delegates, name='api-delegates'),
 
     # email setup
     url(r'^mail/$', mail_views.settings, name='mail-settings'),

patchwork/views/api.py
@@ -22,18 +22,20 @@ import json
 from django.db.models import Q
 from django.http import HttpResponse
 
-from patchwork.models import Person
+from patchwork.models import Person, User
 
 
-def submitters(request):
+MINIMUM_CHARACTERS = 3
+
+
+def _handle_request(request, queryset_fn, formatter):
     search = request.GET.get('q', '')
     limit = request.GET.get('l', None)
 
-    if len(search) <= 3:
-        return HttpResponse(content_type="application/json")
+    if len(search) < MINIMUM_CHARACTERS:
+        return HttpResponse(content_type='application/json')
 
-    queryset = Person.objects.filter(Q(name__icontains=search) |
-                                     Q(email__icontains=search))
+    queryset = queryset_fn(search)
     if limit is not None:
         try:
             limit = int(limit)
@@ -44,11 +46,37 @@ def submitters(request):
         queryset = queryset[:limit]
 
     data = []
-    for submitter in queryset:
-        item = {}
-        item['pk'] = submitter.id
-        item['name'] = submitter.name
-        item['email'] = submitter.email
-        data.append(item)
+    for item in queryset:
+        data.append(formatter(item))
 
-    return HttpResponse(json.dumps(data), content_type="application/json")
+    return HttpResponse(json.dumps(data), content_type='application/json')
+
+
+def submitters(request):
+    def queryset(search):
+        return Person.objects.filter(Q(name__icontains=search) |
+                                     Q(email__icontains=search))
+
+    def formatter(submitter):
+        return {
+            'pk': submitter.id,
+            'name': submitter.name,
+            'email': submitter.email,
+        }
+
+    return _handle_request(request, queryset, formatter)
+
+
+def delegates(request):
+    def queryset(search):
+        return User.objects.filter(Q(username__icontains=search) |
+                                   Q(first_name__icontains=search) |
+                                   Q(last_name__icontains=search))
+
+    def formatter(user):
+        return {
+            'pk': user.id,
+            'name': str(user),
+        }
+
+    return _handle_request(request, queryset, formatter)

commit e99490cf28e8104f42746d71f11618f4e6f427e1

filters: re-add the possibility of filtering undelegated patches
The filters.py redesign that happened for patchwork 1.1 removed
a functionality that we use a lot: to filter patches that weren't
delegated to anyone.

Also, it is a way harder to find someone to delegate with a free
text input. Use, instead a combo-box just like before.

Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>
Signed-off-by: Stephen Finucane <stephen@that.guru>
Fixes: f439f541 ("Add delegate filter autocomplete support")
Closes: #60
[stephenfin: Rework release note and fix some style issues]

patchwork/filters.py
@@ -385,6 +385,7 @@ class ArchiveFilter(Filter):
 class DelegateFilter(Filter):
     name = 'Delegate'
     param = 'delegate'
+    no_delegate_str = 'Nobody'
     ANY_DELEGATE = object()
 
     def __init__(self, filters):
@@ -416,6 +417,11 @@ class DelegateFilter(Filter):
         if not key:
             return
 
+        if key == self.no_delegate_str:
+            self.delegate_match = key
+            self.applied = True
+            return
+
         try:
             self.delegate = User.objects.get(id=int(key))
         except (ValueError, User.DoesNotExist):
@@ -436,6 +442,9 @@ class DelegateFilter(Filter):
         if self.delegate:
             return {'delegate': self.delegate}
 
+        if self.delegate_match == self.no_delegate_str:
+            return {'delegate__username__isnull': True}
+
         if self.delegate_match:
             return {'delegate__username__icontains': self.delegate_match}
 
@@ -447,8 +456,31 @@ class DelegateFilter(Filter):
             return mark_safe('<input type="hidden" value="%s">%s' % (
                 self.param, self.condition))
 
-        return mark_safe('<input type="text" name="delegate" '
-                         'id="delegate_input" class="form-control">')
+        delegates = User.objects.filter(
+            profile__maintainer_projects__isnull=False)
+
+        out = '<select name="delegate" class="form-control">'
+
+        selected = ''
+        if not self.applied:
+            selected = 'selected'
+        out += '<option %s value="">------</option>' % selected
+
+        selected = ''
+        if self.applied and self.delegate is None:
+            selected = 'selected'
+        out += '<option %s value="%s">%s</option>' % (
+            selected, self.no_delegate_str, self.no_delegate_str)
+
+        for delegate in delegates:
+            selected = ''
+            if delegate == self.delegate:
+                selected = ' selected'
+
+            out += '<option %s value="%s">%s</option>' % (
+                selected, delegate.id, delegate.username)
+        out += '</select>'
+        return mark_safe(out)
 
     def set_status(self, *args, **kwargs):
         if 'delegate' in kwargs:

patchwork/templates/patchwork/partials/filters.html
@@ -76,44 +76,6 @@ $(document).ready(function() {
             });
         }
     });
-
-    $('#delegate_input').selectize({
-        plugins: ['enter_key_submit'],
-        maxItems: 1,
-        persist: false,
-        onInitialize: function() {
-            this.on('submit', function() {
-                if (!this.items.length)
-                    this.$input.val(this.lastValue);
-                this.$input.closest('form').submit();
-            }, this);
-        },
-{% if "delegate" in filters.applied_filters %}
-{% with delegate_filter=filters.applied_filters.delegate %}
-        options: [
-            {
-                value: "{{ delegate_filter.key }}",
-                text: "{{ delegate_filter.condition }}",
-            },
-        ],
-        items: ["{{ delegate_filter.key }}"],
-{% endwith %}
-{% endif %}
-        load: function(query, callback) {
-            req = $.ajax({
-                url: "{% url 'api-delegates' %}",
-                data: {q: query, l: 10},
-                error: function() {
-                    callback();
-                },
-                success: function(res) {
-                    callback($.map(res, function (obj) {
-                        return {value: obj.pk, text: obj.name};
-                    }));
-                }
-            });
-        }
-    });
 });
 </script>
 

releasenotes/notes/issue-60-9d4fc111242f7db6.yaml
@@ -1,0 +1,8 @@
+---
+fixes:
+  - |
+    In the past, Patchwork used to support filtering patches that weren't
+    delegated to anyone. This feature was removed in v1.1.0, as part of a patch
+    designed to support delegation to anyone. However, that feature didn't scale
+    and was later removed. The ability to delegate to anyone is now itself
+    re-introduced.