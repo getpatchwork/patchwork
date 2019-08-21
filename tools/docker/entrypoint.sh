getpatchwork/patchwork

commit 0b5b4e8cb3d04e6f25ff73825d34632c61b353d1

docker: Don't require rebuilding if unnecessary
Now that we're pinning versions, we're going to see more frequent
dependency version changes. Requiring a rebuild after every one of these
is tiresome so don't force it and instead display a helpful message
merely suggesting that a rebuild may be necessary.

Signed-off-by: Stephen Finucane <stephen@that.guru>
Cc: Daniel Axtens <dja@axtens.net>
Acked-by: Daniel Axtens <dja@axtens.net>
[dja: we're not currently pinning versions because I dropped that
 patch as it broke postgres. But this can stay - sfin has been
 asking for it for ages.]
Signed-off-by: Daniel Axtens <dja@axtens.net>

tools/docker/entrypoint.sh
@@ -69,9 +69,10 @@ fi
 for x in /tmp/requirements-*.txt; do
     if ! cmp $x ~/patchwork/$(basename $x); then
         echo "A requirements file has changed."
-        echo "Please rebuild the patchwork image:"
+        echo "You may need to rebuild the patchwork image:"
         echo "    docker-compose build web"
-        exit 1
+        echo ""
+        diff -u $x ~/patchwork/$(basename $x)
     fi
 done
 

commit 0fab75276b1ba2e065e1a6e0c2ed6dc1e19444f3

docker: *Actually* don't require rebuilding if unnecessary
Because we were using 'set -e', we were erroring out as soon as
something - a diff in this case - failed. Temporarily disable it for
this one check.

Signed-off-by: Stephen Finucane <stephen@that.guru>
Fixes: 0b5b4e8c ("docker: Don't require rebuilding if unnecessary")
Cc: Daniel Axtens <dja@axtens.net>
Signed-off-by: Daniel Axtens <dja@axtens.net>

tools/docker/entrypoint.sh
@@ -67,6 +67,8 @@ EOF
     exit 1
 fi
 
+set +e
+
 # check if we need to rebuild because requirements changed
 for x in /tmp/requirements-*.txt; do
     if ! cmp $x ~/patchwork/$(basename $x); then
@@ -80,6 +82,8 @@ EOF
         diff -u $x ~/patchwork/$(basename $x)
     fi
 done
+
+set -e
 
 # check if db is connected
 if ! test_db_connection; then