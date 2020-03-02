getpatchwork/patchwork

commit 7f6685a2a0b12c08c39f9de84684de8d033c57f1

Fix CRLF newlines upon submission changes
After changing submission via admin interface, CRLF newlines are
suddenly present in the body. Replace them back to '\n'.

The issue was found after modifying submission via admin interface
using Python 2 and downloading the respective mbox file (git choked on
downloaded patch because of malformed line endings). Python 3's mail
module uses '\n' internally so the problem doesn't manifest there, but
the content received by Django/JS is still saved in the database with
CRLF line endings which shouldn't be there.

Signed-off-by: Veronika Kabatova <vkabatov@redhat.com>
Reviewed-by: Stephen Finucane <stephen@that.guru>

patchwork/models.py
@@ -328,6 +328,14 @@ class EmailMixin(models.Model):
         return ''.join([match.group(0) + '\n' for match in
                         self.response_re.finditer(self.content)])
 
+    def save(self, *args, **kwargs):
+        # Modifying a submission via admin interface changes '\n' newlines in
+        # message content to '\r\n'. We need to fix them to avoid problems,
+        # especially as git complains about malformed patches when PW runs
+        # on PY2
+        self.content = self.content.replace('\r\n', '\n')
+        super(EmailMixin, self).save(*args, **kwargs)
+
     class Meta:
         abstract = True
 

commit b8a1673e356f2ad49f83eff82262b8d3c066f2fd

models: Handle unset 'Submission.content'
It's unlikely but 'Submission.content' is nullable and we weren't
handling this here. Fix that.

Signed-off-by: Stephen Finucane <stephen@that.guru>
Fixes: 7f6685a2a ("Fix CRLF newlines upon submission changes")

patchwork/models.py
@@ -347,7 +347,9 @@ class EmailMixin(models.Model):
         # message content to '\r\n'. We need to fix them to avoid problems,
         # especially as git complains about malformed patches when PW runs
         # on PY2
-        self.content = self.content.replace('\r\n', '\n')
+        if self.content:
+            self.content = self.content.replace('\r\n', '\n')
+
         super(EmailMixin, self).save(*args, **kwargs)
 
     class Meta: