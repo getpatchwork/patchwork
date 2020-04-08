d3d4f9f36b2b16aa54837b67733f2de5cf93457b
Andrew Donnellan <ajd@linux.ibm.com>
2020-04-08 22:47:54 +0100

    Add Django 3.0 support
    Add the latest version of Django.
    
    Signed-off-by: Andrew Donnellan <ajd@linux.ibm.com>
    Signed-off-by: Stephen Finucane <stephen@that.guru>
    Closes: #311

--- patchwork/api/patch.py
+++ patchwork/api/patch.py
@@ -9,7 +9,7 @@ import email.parser
 
 from django.core.exceptions import ValidationError
 from django.utils.text import slugify
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from rest_framework import status
 from rest_framework.exceptions import APIException
 from rest_framework.exceptions import PermissionDenied
