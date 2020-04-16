91dce3e077ebdef833afd06a8407e1012929d929
Jeremy Kerr <jk@ozlabs.org>
2020-04-16 09:29:26 +0800

    parser: prevent IntegrityErrors
    Currently, the parser relies on causing (and catching) IntegrityErrors
    on patch insert to catch duplicate (msgid,project) mails.
    
    This change performs an atomic select -> insert instead.
    
    Signed-off-by: Jeremy Kerr <jk@ozlabs.org>
    Signed-off-by: Stephen Finucane <stephen@that.guru>
    [stephenfin: Remove 'expectedFailure' marker again]
    (cherry picked from commit 947c6aae94b7b554ca701c1d7e5baf000759ed2d)

--- patchwork/tests/test_parser.py
+++ patchwork/tests/test_parser.py
@@ -1161,7 +1161,6 @@ class DuplicateMailTest(TestCase):
 
         self.assertEqual(errors, [])
 
-    @unittest.expectedFailure
     def test_duplicate_patch(self):
         diff = read_patch('0001-add-line.patch')
         m = create_email(diff, listid=self.listid, msgid='1@example.com')
