781303bce0d8e94eae957bd3b30abe9a7849d981
Stephen Finucane <stephen@that.guru>
2020-04-23 10:46:46 +0100

    forms: Remove LoginForm
    This should have been removed in commit f1e089f773.
    
    Signed-off-by: Stephen Finucane <stephen@that.guru>

--- patchwork/forms.py
+++ patchwork/forms.py
@@ -45,11 +45,6 @@ class RegistrationForm(forms.Form):
         return self.cleaned_data
 
 
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=30)
-    password = forms.CharField(widget=forms.PasswordInput)
-
-
 class EmailForm(forms.Form):
     email = forms.EmailField(max_length=200)
 
