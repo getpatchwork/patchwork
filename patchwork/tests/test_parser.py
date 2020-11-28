getpatchwork/patchwork

commit 0686a736fbf6d869bd31bd135ba38080ac96de22

models: Split 'CoverLetter' from 'Submission'
We want to get rid of the split between 'Patch' and 'Submission' because
of the cost of using JOINs basically everywhere we use 'Patch'. Before
we do that, we need to move the other users of 'Submission' to other
models and other models that rely on these users sharing the common
'Submission' base. For the former, there is only one user,
'CoverLetter', while for the latter there is only the 'Comment' model.
As a result, we must do the following:

- Create a new 'Cover' model
- Create a new 'CoverComment' model
- Move everything from 'CoverLetter' to 'Cover' and all entries
  associated with a 'CoverLetter' from 'Comment' to 'CoverComment'
- Delete the 'CoverLetter' model
- Rename the 'Comment' model to 'PatchComment'

This means our model "hierarchy" goes from:

  Submission
    Patch
    CoverLetter
    Comment

To:

  Submission
    Patch
    PatchComment
  Cover
    CoverComment

A future change will flatten the 'Submission' and 'Patch' model.

Note that this actually highlighted a bug in Django, which has since
been reported upstream [1]. As noted there, the issue stems from MySQL's
refusal to remove an index from a foreign key when DB constraints are
used and the workaround is to remove the foreign key constraint before
altering the indexes and then re-add the constraint after.

[1] https://code.djangoproject.com/ticket/31335

Signed-off-by: Stephen Finucane <stephen@that.guru>

patchwork/parser.py
@@ -18,11 +18,12 @@ from django.contrib.auth.models import User
 from django.db.utils import IntegrityError
 from django.db import transaction
 
-from patchwork.models import Comment
-from patchwork.models import CoverLetter
+from patchwork.models import Cover
+from patchwork.models import CoverComment
 from patchwork.models import DelegationRule
 from patchwork.models import get_default_initial_patch_state
 from patchwork.models import Patch
+from patchwork.models import PatchComment
 from patchwork.models import Person
 from patchwork.models import Project
 from patchwork.models import Series
@@ -664,10 +665,12 @@ def find_submission_for_comment(project, refs):
 
         # see if we have comments that refer to a patch
         try:
-            comment = Comment.objects.get(submission__project=project,
-                                          msgid=ref)
+            comment = PatchComment.objects.get(
+                patch__project=project,
+                msgid=ref,
+            )
             return comment.submission
-        except Comment.MultipleObjectsReturned:
+        except PatchComment.MultipleObjectsReturned:
             # NOTE(stephenfin): This is a artifact of prior lack of support
             # for cover letters in Patchwork. Previously all replies to
             # patches were saved as comments. However, it's possible that
@@ -681,11 +684,36 @@ def find_submission_for_comment(project, refs):
             # apply the comment to the cover letter. Note that this only
             # happens when running 'parsearchive' or similar, so it should not
             # affect every day use in any way.
-            comments = Comment.objects.filter(submission__project=project,
-                                              msgid=ref)
+            comments = PatchComment.objects.filter(
+                patch__project=project,
+                msgid=ref,
+            )
             # The latter item will be the cover letter
             return comments.reverse()[0].submission
-        except Comment.DoesNotExist:
+        except PatchComment.DoesNotExist:
+            pass
+
+    return None
+
+
+def find_cover_for_comment(project, refs):
+    for ref in refs:
+        ref = ref[:255]
+        # first, check for a direct reply
+        try:
+            cover = Cover.objects.get(project=project, msgid=ref)
+            return cover
+        except Cover.DoesNotExist:
+            pass
+
+        # see if we have comments that refer to a patch
+        try:
+            comment = CoverComment.objects.get(
+                cover__project=project,
+                msgid=ref,
+            )
+            return comment.cover
+        except CoverComment.DoesNotExist:
             pass
 
     return None
@@ -1190,11 +1218,11 @@ def parse_mail(mail, list_id=None):
         if not is_comment:
             if not refs == []:
                 try:
-                    CoverLetter.objects.all().get(name=name)
-                except CoverLetter.DoesNotExist:
+                    Cover.objects.all().get(name=name)
+                except Cover.DoesNotExist:
                     # if no match, this is a new cover letter
                     is_cover_letter = True
-                except CoverLetter.MultipleObjectsReturned:
+                except Cover.MultipleObjectsReturned:
                     # if multiple cover letters are found, just ignore
                     pass
             else:
@@ -1228,10 +1256,10 @@ def parse_mail(mail, list_id=None):
                     msgid=msgid, project=project, series=series)
 
             with transaction.atomic():
-                if CoverLetter.objects.filter(project=project, msgid=msgid):
+                if Cover.objects.filter(project=project, msgid=msgid):
                     raise DuplicateMailError(msgid=msgid)
 
-                cover_letter = CoverLetter.objects.create(
+                cover_letter = Cover.objects.create(
                     msgid=msgid,
                     project=project,
                     name=name[:255],
@@ -1250,16 +1278,35 @@ def parse_mail(mail, list_id=None):
 
     # we only save comments if we have the parent email
     submission = find_submission_for_comment(project, refs)
-    if not submission:
+    if submission:
+        author = get_or_create_author(mail, project)
+
+        with transaction.atomic():
+            if PatchComment.objects.filter(patch=patch, msgid=msgid):
+                raise DuplicateMailError(msgid=msgid)
+            comment = PatchComment.objects.create(
+                patch=submission,
+                msgid=msgid,
+                date=date,
+                headers=headers,
+                submitter=author,
+                content=message)
+
+        logger.debug('Comment saved')
+
+        return comment
+
+    cover = find_cover_for_comment(project, refs)
+    if not cover:
         return
 
     author = get_or_create_author(mail, project)
 
     with transaction.atomic():
-        if Comment.objects.filter(submission=submission, msgid=msgid):
+        if CoverComment.objects.filter(cover=cover, msgid=msgid):
             raise DuplicateMailError(msgid=msgid)
-        comment = Comment.objects.create(
-            submission=submission,
+        comment = CoverComment.objects.create(
+            cover=cover,
             msgid=msgid,
             date=date,
             headers=headers,

patchwork/tests/test_parser.py
@@ -17,11 +17,11 @@ from django.test import TransactionTestCase
 from django.db.transaction import atomic
 from django.db import connection
 
-from patchwork.models import Comment
+from patchwork.models import Cover
 from patchwork.models import Patch
+from patchwork.models import PatchComment
 from patchwork.models import Person
 from patchwork.models import State
-from patchwork.models import CoverLetter
 from patchwork.parser import clean_subject
 from patchwork.parser import get_or_create_author
 from patchwork.parser import find_patch_content as find_content
@@ -551,7 +551,7 @@ class MultipleProjectPatchCommentTest(MultipleProjectPatchTest):
             patch = Patch.objects.filter(project=project)[0]
             # we should see the reply comment only
             self.assertEqual(
-                Comment.objects.filter(submission=patch).count(), 1)
+                PatchComment.objects.filter(patch=patch).count(), 1)
 
 
 class ListIdHeaderTest(TestCase):
@@ -1157,7 +1157,7 @@ class DuplicateMailTest(TestCase):
         self._test_duplicate_mail(m2)
 
         self.assertEqual(Patch.objects.count(), 1)
-        self.assertEqual(Comment.objects.count(), 1)
+        self.assertEqual(PatchComment.objects.count(), 1)
 
     def test_duplicate_coverletter(self):
         m = create_email('test', listid=self.listid, msgid='1@example.com')
@@ -1166,4 +1166,4 @@ class DuplicateMailTest(TestCase):
 
         self._test_duplicate_mail(m)
 
-        self.assertEqual(CoverLetter.objects.count(), 1)
+        self.assertEqual(Cover.objects.count(), 1)

patchwork/admin.py
@@ -10,10 +10,11 @@ from django.db.models import Prefetch
 
 from patchwork.models import Bundle
 from patchwork.models import Check
-from patchwork.models import Comment
-from patchwork.models import CoverLetter
+from patchwork.models import Cover
+from patchwork.models import CoverComment
 from patchwork.models import DelegationRule
 from patchwork.models import Patch
+from patchwork.models import PatchComment
 from patchwork.models import PatchRelation
 from patchwork.models import Person
 from patchwork.models import Project
@@ -75,14 +76,14 @@ class StateAdmin(admin.ModelAdmin):
 admin.site.register(State, StateAdmin)
 
 
-class CoverLetterAdmin(admin.ModelAdmin):
+class CoverAdmin(admin.ModelAdmin):
     list_display = ('name', 'submitter', 'project', 'date')
     list_filter = ('project', )
     search_fields = ('name', 'submitter__name', 'submitter__email')
     date_hierarchy = 'date'
 
 
-admin.site.register(CoverLetter, CoverLetterAdmin)
+admin.site.register(Cover, CoverAdmin)
 
 
 class PatchAdmin(admin.ModelAdmin):
@@ -104,13 +105,22 @@ class PatchAdmin(admin.ModelAdmin):
 admin.site.register(Patch, PatchAdmin)
 
 
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('submission', 'submitter', 'date')
-    search_fields = ('submission__name', 'submitter__name', 'submitter__email')
+class CoverCommentAdmin(admin.ModelAdmin):
+    list_display = ('cover', 'submitter', 'date')
+    search_fields = ('cover__name', 'submitter__name', 'submitter__email')
     date_hierarchy = 'date'
 
 
-admin.site.register(Comment, CommentAdmin)
+admin.site.register(CoverComment, CoverCommentAdmin)
+
+
+class PatchCommentAdmin(admin.ModelAdmin):
+    list_display = ('patch', 'submitter', 'date')
+    search_fields = ('patch__name', 'submitter__name', 'submitter__email')
+    date_hierarchy = 'date'
+
+
+admin.site.register(PatchComment, PatchCommentAdmin)
 
 
 class PatchInline(admin.StackedInline):

patchwork/api/comment.py
@@ -12,11 +12,13 @@ from rest_framework.serializers import SerializerMethodField
 from patchwork.api.base import BaseHyperlinkedModelSerializer
 from patchwork.api.base import PatchworkPermission
 from patchwork.api.embedded import PersonSerializer
-from patchwork.models import Comment
+from patchwork.models import Cover
+from patchwork.models import CoverComment
 from patchwork.models import Submission
+from patchwork.models import PatchComment
 
 
-class CommentListSerializer(BaseHyperlinkedModelSerializer):
+class BaseCommentListSerializer(BaseHyperlinkedModelSerializer):
 
     web_url = SerializerMethodField()
     subject = SerializerMethodField()
@@ -46,7 +48,6 @@ class CommentListSerializer(BaseHyperlinkedModelSerializer):
         return headers
 
     class Meta:
-        model = Comment
         fields = ('id', 'web_url', 'msgid', 'list_archive_url', 'date',
                   'subject', 'submitter', 'content', 'headers')
         read_only_fields = fields
@@ -56,11 +57,48 @@ class CommentListSerializer(BaseHyperlinkedModelSerializer):
         }
 
 
-class CommentList(ListAPIView):
+class CoverCommentListSerializer(BaseCommentListSerializer):
+
+    class Meta:
+        model = CoverComment
+        fields = BaseCommentListSerializer.Meta.fields
+        read_only_fields = fields
+        versioned_fields = BaseCommentListSerializer.Meta.versioned_fields
+
+
+class PatchCommentListSerializer(BaseCommentListSerializer):
+
+    class Meta:
+        model = PatchComment
+        fields = BaseCommentListSerializer.Meta.fields
+        read_only_fields = fields
+        versioned_fields = BaseCommentListSerializer.Meta.versioned_fields
+
+
+class CoverCommentList(ListAPIView):
+    """List cover comments"""
+
+    permission_classes = (PatchworkPermission,)
+    serializer_class = CoverCommentListSerializer
+    search_fields = ('subject',)
+    ordering_fields = ('id', 'subject', 'date', 'submitter')
+    ordering = 'id'
+    lookup_url_kwarg = 'pk'
+
+    def get_queryset(self):
+        if not Cover.objects.filter(pk=self.kwargs['pk']).exists():
+            raise Http404
+
+        return CoverComment.objects.filter(
+            cover=self.kwargs['pk']
+        ).select_related('submitter')
+
+
+class PatchCommentList(ListAPIView):
     """List comments"""
 
     permission_classes = (PatchworkPermission,)
-    serializer_class = CommentListSerializer
+    serializer_class = PatchCommentListSerializer
     search_fields = ('subject',)
     ordering_fields = ('id', 'subject', 'date', 'submitter')
     ordering = 'id'
@@ -70,6 +108,6 @@ class CommentList(ListAPIView):
         if not Submission.objects.filter(pk=self.kwargs['pk']).exists():
             raise Http404
 
-        return Comment.objects.filter(
-            submission=self.kwargs['pk']
+        return PatchComment.objects.filter(
+            patch=self.kwargs['pk']
         ).select_related('submitter')

patchwork/api/cover.py
@@ -15,7 +15,7 @@ from patchwork.api.filters import CoverFilterSet
 from patchwork.api.embedded import PersonSerializer
 from patchwork.api.embedded import ProjectSerializer
 from patchwork.api.embedded import SeriesSerializer
-from patchwork.models import CoverLetter
+from patchwork.models import Cover
 
 
 class CoverListSerializer(BaseHyperlinkedModelSerializer):
@@ -49,7 +49,7 @@ class CoverListSerializer(BaseHyperlinkedModelSerializer):
         return data
 
     class Meta:
-        model = CoverLetter
+        model = Cover
         fields = ('id', 'url', 'web_url', 'project', 'msgid',
                   'list_archive_url', 'date', 'name', 'submitter', 'mbox',
                   'series', 'comments')
@@ -82,7 +82,7 @@ class CoverDetailSerializer(CoverListSerializer):
         return headers
 
     class Meta:
-        model = CoverLetter
+        model = Cover
         fields = CoverListSerializer.Meta.fields + (
             'headers', 'content')
         read_only_fields = fields
@@ -100,7 +100,7 @@ class CoverList(ListAPIView):
     ordering = 'id'
 
     def get_queryset(self):
-        return CoverLetter.objects.all()\
+        return Cover.objects.all()\
             .prefetch_related('series__project')\
             .select_related('project', 'submitter', 'series')\
             .defer('content', 'headers')
@@ -112,5 +112,5 @@ class CoverDetail(RetrieveAPIView):
     serializer_class = CoverDetailSerializer
 
     def get_queryset(self):
-        return CoverLetter.objects.all()\
+        return Cover.objects.all()\
             .select_related('project', 'submitter', 'series')

patchwork/api/embedded.py
@@ -108,7 +108,7 @@ class CoverSerializer(SerializedRelatedField):
     class _Serializer(MboxMixin, WebURLMixin, BaseHyperlinkedModelSerializer):
 
         class Meta:
-            model = models.CoverLetter
+            model = models.Cover
             fields = ('id', 'url', 'web_url', 'msgid', 'list_archive_url',
                       'date', 'name', 'mbox')
             read_only_fields = fields

patchwork/api/filters.py
@@ -18,7 +18,7 @@ from rest_framework import exceptions
 from patchwork.api import utils
 from patchwork.models import Bundle
 from patchwork.models import Check
-from patchwork.models import CoverLetter
+from patchwork.models import Cover
 from patchwork.models import Event
 from patchwork.models import Patch
 from patchwork.models import Person
@@ -199,7 +199,7 @@ class CoverFilterSet(TimestampMixin, BaseFilterSet):
     msgid = CharFilter(method=msgid_filter)
 
     class Meta:
-        model = CoverLetter
+        model = Cover
         fields = ('project', 'series', 'submitter')
 
 
@@ -253,7 +253,7 @@ class EventFilterSet(TimestampMixin, BaseFilterSet):
     patch = BaseFilter(queryset=Patch.objects.all(),
                        widget=MultipleHiddenInput,
                        distinct=False)
-    cover = BaseFilter(queryset=CoverLetter.objects.all(),
+    cover = BaseFilter(queryset=Cover.objects.all(),
                        widget=MultipleHiddenInput,
                        distinct=False)
 

patchwork/management/commands/parsearchive.py
@@ -32,8 +32,9 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         results = {
             models.Patch: 0,
-            models.CoverLetter: 0,
-            models.Comment: 0,
+            models.Cover: 0,
+            models.PatchComment: 0,
+            models.CoverComment: 0,
         }
         duplicates = 0
         dropped = 0
@@ -118,9 +119,11 @@ class Command(BaseCommand):
             '  %(errors)4d errors\n'
             'Total: %(new)s new entries' % {
                 'total': count,
-                'covers': results[models.CoverLetter],
+                'covers': results[models.Cover],
                 'patches': results[models.Patch],
-                'comments': results[models.Comment],
+                'comments': (
+                    results[models.CoverComment] + results[models.PatchComment]
+                ),
                 'duplicates': duplicates,
                 'dropped': dropped,
                 'errors': errors,

commit 70c9db8b9c1bee8ddc32eb1d6fceff6ef616ba3e

parser: Update reference to PatchComment
Commit 0686a736fbf6d869bd31bd135ba38080ac96de22 split out 'CoverLetter'
from the old 'Submission' model, removing the common 'Comment' model in
favour of distinct 'CoverComment' and 'PatchComment' models in the
process. Unfortunately we misssed some references to the old model in
the 'patchwork.parser' module. Correct these now, adding unit tests to
prevent regressions.

Signed-off-by: Stephen Finucane <stephen@that.guru>
Fixes: 0686a736 ("models: Split 'CoverLetter' from 'Submission'")
Closes: #384

patchwork/parser.py
@@ -668,7 +668,7 @@ def find_patch_for_comment(project, refs):
                 patch__project=project,
                 msgid=ref,
             )
-            return comment.submission
+            return comment.patch
         except PatchComment.MultipleObjectsReturned:
             # NOTE(stephenfin): This is a artifact of prior lack of support
             # for cover letters in Patchwork. Previously all replies to
@@ -688,7 +688,7 @@ def find_patch_for_comment(project, refs):
                 msgid=ref,
             )
             # The latter item will be the cover letter
-            return comments.reverse()[0].submission
+            return comments.reverse()[0].patch
         except PatchComment.DoesNotExist:
             pass
 

patchwork/tests/test_parser.py
@@ -22,6 +22,7 @@ from patchwork.models import Patch
 from patchwork.models import PatchComment
 from patchwork.models import Person
 from patchwork.models import State
+from patchwork import parser
 from patchwork.parser import clean_subject
 from patchwork.parser import get_or_create_author
 from patchwork.parser import find_patch_content as find_content
@@ -37,6 +38,10 @@ from patchwork.parser import subject_check
 from patchwork.parser import DuplicateMailError
 from patchwork.tests import TEST_MAIL_DIR
 from patchwork.tests import TEST_FUZZ_DIR
+from patchwork.tests.utils import create_cover
+from patchwork.tests.utils import create_cover_comment
+from patchwork.tests.utils import create_patch
+from patchwork.tests.utils import create_patch_comment
 from patchwork.tests.utils import create_project
 from patchwork.tests.utils import create_series
 from patchwork.tests.utils import create_series_reference
@@ -1167,3 +1172,66 @@ class DuplicateMailTest(TestCase):
         self._test_duplicate_mail(m)
 
         self.assertEqual(Cover.objects.count(), 1)
+
+
+class TestCommentCorrelation(TestCase):
+
+    def test_find_patch_for_comment__no_reply(self):
+        """Test behavior for mails that don't match anything we have."""
+        project = create_project()
+        create_patch(project=project)
+
+        result = parser.find_patch_for_comment(project, ['foo'])
+
+        self.assertIsNone(result)
+
+    def test_find_patch_for_comment__direct_reply(self):
+        """Test behavior when we have a reference to the original patch."""
+        msgid = make_msgid()
+        project = create_project()
+        patch = create_patch(msgid=msgid, project=project)
+
+        result = parser.find_patch_for_comment(project, [msgid])
+
+        self.assertEqual(patch, result)
+
+    def test_find_patch_for_comment__indirect_reply(self):
+        """Test behavior when we only have a reference to a comment."""
+        msgid = make_msgid()
+        project = create_project()
+        patch = create_patch(project=project)
+        create_patch_comment(patch=patch, msgid=msgid)
+
+        result = parser.find_patch_for_comment(project, [msgid])
+
+        self.assertEqual(patch, result)
+
+    def test_find_cover_for_comment__no_reply(self):
+        """Test behavior for mails that don't match anything we have."""
+        project = create_project()
+        create_cover(project=project)
+
+        result = parser.find_cover_for_comment(project, ['foo'])
+
+        self.assertIsNone(result)
+
+    def test_find_cover_for_comment__direct_reply(self):
+        """Test behavior when we have a reference to the original cover."""
+        msgid = make_msgid()
+        project = create_project()
+        cover = create_cover(msgid=msgid, project=project)
+
+        result = parser.find_cover_for_comment(project, [msgid])
+
+        self.assertEqual(cover, result)
+
+    def test_find_cover_for_comment__indirect_reply(self):
+        """Test behavior when we only have a reference to a comment."""
+        msgid = make_msgid()
+        project = create_project()
+        cover = create_cover(project=project)
+        create_cover_comment(cover=cover, msgid=msgid)
+
+        result = parser.find_cover_for_comment(project, [msgid])
+
+        self.assertEqual(cover, result)