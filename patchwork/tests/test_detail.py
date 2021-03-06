a43d6acda0939c14e8135549968815876680d497
Stephen Finucane <stephen@that.guru>
2021-03-06 14:54:59 +0000

    Only show unique checks
    Commit e5c641fc4 optimized fetching of checks when displaying a patch by
    prefetching these checks ahead of time. Unfortunately we missed that
    this should exclude older versions of checks for a given context. Make
    the code used to generate the unique checks generic and allow us to use
    that as a filter to the checks provided to the template, restoring the
    correct behavior.
    
    Signed-off-by: Stephen Finucane <stephen@that.guru>
    Closes: #398
    Fixes: e5c641fc4 ("Optimise fetching checks when displaying a patch")

--- patchwork/tests/test_detail.py
+++ patchwork/tests/test_detail.py
@@ -3,14 +3,20 @@
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
 
+from datetime import datetime as dt
+from datetime import timedelta
+
 from django.test import TestCase
 from django.urls import reverse
 
+from patchwork.models import Check
+from patchwork.tests.utils import create_check
 from patchwork.tests.utils import create_cover
 from patchwork.tests.utils import create_cover_comment
 from patchwork.tests.utils import create_patch
 from patchwork.tests.utils import create_patch_comment
 from patchwork.tests.utils import create_project
+from patchwork.tests.utils import create_user
 
 
 class CoverViewTest(TestCase):
@@ -156,6 +162,38 @@ class PatchViewTest(TestCase):
         )
         response = self.client.get(requested_url)
         self.assertEqual(response.status_code, 404)
+
+    def test_patch_with_checks(self):
+        user = create_user()
+        patch = create_patch()
+        check_a = create_check(
+            patch=patch, user=user, context='foo', state=Check.STATE_FAIL,
+            date=(dt.utcnow() - timedelta(days=1)))
+        create_check(
+            patch=patch, user=user, context='foo', state=Check.STATE_SUCCESS)
+        check_b = create_check(
+            patch=patch, user=user, context='bar', state=Check.STATE_PENDING)
+        requested_url = reverse(
+            'patch-detail',
+            kwargs={
+                'project_id': patch.project.linkname,
+                'msgid': patch.url_msgid,
+            },
+        )
+        response = self.client.get(requested_url)
+
+        # the response should contain checks
+        self.assertContains(response, '<h2>Checks</h2>')
+
+        # and it should only show the unique checks
+        self.assertEqual(
+            1, response.content.decode().count(
+                f'<td>{check_a.user}/{check_a.context}</td>'
+            ))
+        self.assertEqual(
+            1, response.content.decode().count(
+                f'<td>{check_b.user}/{check_b.context}</td>'
+            ))
 
 
 class CommentRedirectTest(TestCase):
