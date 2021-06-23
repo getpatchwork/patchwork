getpatchwork/patchwork

commit 6025f0e2533f1b5dc2e4d36a6881397402ea3d6d

Add parallel testing
This saves us a good deal of time, even allowing for process startup and
general overhead. Before:

  real    6m39.103s
  user    1m9.561s
  sys     0m2.705s

After:

  real    4m5.267s
  user    1m24.527s
  sys     0m3.828s

This requires N databases, where N is the N of processors on the host.
We need to update the docker configuration for this.

Signed-off-by: Stephen Finucane <stephen@that.guru>

tools/docker/entrypoint.sh
@@ -26,7 +26,7 @@ reset_data_mysql() {
 DROP DATABASE IF EXISTS patchwork;
 CREATE DATABASE patchwork CHARACTER SET utf8;
 GRANT ALL ON patchwork.* TO 'patchwork' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON test_patchwork.* TO 'patchwork'@'%';
+GRANT ALL ON 'test_patchwork\_%'.* to 'patchwork'@'%';
 FLUSH PRIVILEGES;
 EOF
 }

tox.ini
@@ -28,7 +28,7 @@ passenv =
     PW_TEST_DB_TYPE PW_TEST_DB_USER PW_TEST_DB_PASS PW_TEST_DB_HOST
     PW_TEST_DB_PORT
 commands =
-    python {toxinidir}/manage.py test --noinput {posargs:patchwork}
+    python {toxinidir}/manage.py test --noinput --parallel -- {posargs:patchwork}
 
 [testenv:bashate]
 deps = bashate

commit d2174b9bd7cb6187e0dc6c16edf874cdce079067

tests: fix parallel tests
Parallel tests require:

 - the % wildcard to be in a token enclosed by backticks, not single
   quotes

 - that the user still be able to use 'test_patchwork' (so we don't want
   the \_ before the %)

Presumably this was skipped because if you get permissions working
manually but you miss part of the required permissions in the automated
script, you need to delete the old db data in order to observe the
issue.

Amusingly postgres worked the whole time.

Fixes: 6025f0e2533f ("Add parallel testing")
Signed-off-by: Daniel Axtens <dja@axtens.net>

tools/docker/entrypoint.sh
@@ -26,7 +26,7 @@ reset_data_mysql() {
 DROP DATABASE IF EXISTS patchwork;
 CREATE DATABASE patchwork CHARACTER SET utf8;
 GRANT ALL ON patchwork.* TO 'patchwork' IDENTIFIED BY 'password';
-GRANT ALL ON 'test_patchwork\_%'.* to 'patchwork'@'%';
+GRANT ALL ON \`test\\_patchwork%\`.* to 'patchwork'@'%';
 FLUSH PRIVILEGES;
 EOF
 }