getpatchwork/patchwork

commit e3d8f754804417c98208f322c04696020fa67ee2

REST: Add 'patch-comment-created', 'cover-comment-created' events
This patch stitches in "comment created" events for patches and cover
letter into the event queue.

Signed-off-by: DJ Delorie <dj@redhat.com>
Signed-off-by: Stephen Finucane <stephen@that.guru>
Closes: #424
[stephenfin: Extend to cover letters also. Fix some formatting issues.
 Add a release note]

patchwork/api/embedded.py
@@ -120,6 +120,24 @@ class CoverSerializer(SerializedRelatedField):
             }
 
 
+class CoverCommentSerializer(SerializedRelatedField):
+
+    class _Serializer(MboxMixin, WebURLMixin, BaseHyperlinkedModelSerializer):
+
+        class Meta:
+            model = models.CoverComment
+            fields = ('id', 'url', 'web_url', 'msgid', 'list_archive_url',
+                      'date', 'name')
+            read_only_fields = fields
+            versioned_fields = {
+                '1.1': ('web_url', 'mbox', ),
+                '1.2': ('list_archive_url',),
+            }
+            extra_kwargs = {
+                'url': {'view_name': 'api-cover-comment-detail'},
+            }
+
+
 class PatchSerializer(SerializedRelatedField):
 
     class _Serializer(MboxMixin, WebURLMixin, BaseHyperlinkedModelSerializer):
@@ -135,6 +153,24 @@ class PatchSerializer(SerializedRelatedField):
             }
             extra_kwargs = {
                 'url': {'view_name': 'api-patch-detail'},
+            }
+
+
+class PatchCommentSerializer(SerializedRelatedField):
+
+    class _Serializer(MboxMixin, WebURLMixin, BaseHyperlinkedModelSerializer):
+
+        class Meta:
+            model = models.PatchComment
+            fields = ('id', 'url', 'web_url', 'msgid', 'list_archive_url',
+                      'date', 'name')
+            read_only_fields = fields
+            versioned_fields = {
+                '1.1': ('web_url', 'mbox', ),
+                '1.2': ('list_archive_url',),
+            }
+            extra_kwargs = {
+                'url': {'view_name': 'api-patch-comment-detail'},
             }
 
 

patchwork/api/event.py
@@ -12,7 +12,9 @@ from rest_framework.serializers import SlugRelatedField
 
 from patchwork.api.embedded import CheckSerializer
 from patchwork.api.embedded import CoverSerializer
+from patchwork.api.embedded import CoverCommentSerializer
 from patchwork.api.embedded import PatchSerializer
+from patchwork.api.embedded import PatchCommentSerializer
 from patchwork.api.embedded import ProjectSerializer
 from patchwork.api.embedded import SeriesSerializer
 from patchwork.api.embedded import UserSerializer
@@ -35,20 +37,41 @@ class EventSerializer(ModelSerializer):
     created_check = CheckSerializer()
     previous_relation = SerializerMethodField()
     current_relation = SerializerMethodField()
+    cover_comment = CoverCommentSerializer()
+    patch_comment = PatchCommentSerializer()
 
+    # Mapping of event type to fields to include in the response
     _category_map = {
         Event.CATEGORY_COVER_CREATED: ['cover'],
         Event.CATEGORY_PATCH_CREATED: ['patch'],
         Event.CATEGORY_PATCH_COMPLETED: ['patch', 'series'],
-        Event.CATEGORY_PATCH_STATE_CHANGED: ['patch', 'previous_state',
-                                             'current_state'],
-        Event.CATEGORY_PATCH_DELEGATED: ['patch', 'previous_delegate',
-                                         'current_delegate'],
-        Event.CATEGORY_PATCH_RELATION_CHANGED: ['patch', 'previous_relation',
-                                                'current_relation'],
+        Event.CATEGORY_PATCH_STATE_CHANGED: [
+            'patch',
+            'previous_state',
+            'current_state',
+        ],
+        Event.CATEGORY_PATCH_DELEGATED: [
+            'patch',
+            'previous_delegate',
+            'current_delegate',
+        ],
+        Event.CATEGORY_PATCH_RELATION_CHANGED: [
+            'patch',
+            'previous_relation',
+            'current_relation',
+        ],
         Event.CATEGORY_CHECK_CREATED: ['patch', 'created_check'],
         Event.CATEGORY_SERIES_CREATED: ['series'],
         Event.CATEGORY_SERIES_COMPLETED: ['series'],
+        Event.CATEGORY_COVER_COMMENT_CREATED: ['cover', 'cover_comment'],
+        Event.CATEGORY_PATCH_COMMENT_CREATED: ['patch', 'patch_comment'],
+    }
+
+    # Mapping of database column names to REST API representations
+    _field_name_map = {
+        'created_check': 'check',
+        'cover_comment': 'comment',
+        'patch_comment': 'comment',
     }
 
     def get_previous_relation(self, instance):
@@ -61,13 +84,19 @@ class EventSerializer(ModelSerializer):
         data = super(EventSerializer, self).to_representation(instance)
         payload = OrderedDict()
         kept_fields = self._category_map[instance.category] + [
-            'id', 'category', 'project', 'date', 'actor']
+            'id',
+            'category',
+            'project',
+            'date',
+            'actor',
+        ]
 
         for field in [x for x in data]:
             if field not in kept_fields:
                 del data[field]
             elif field in self._category_map[instance.category]:
-                field_name = 'check' if field == 'created_check' else field
+                # remap fields if necessary
+                field_name = self._field_name_map.get(field, field)
                 payload[field_name] = data.pop(field)
 
         data['payload'] = payload
@@ -77,14 +106,27 @@ class EventSerializer(ModelSerializer):
     class Meta:
         model = Event
         fields = (
-            'id', 'category', 'project', 'date', 'actor', 'patch',
-            'series', 'cover', 'previous_state', 'current_state',
-            'previous_delegate', 'current_delegate', 'created_check',
-            'previous_relation', 'current_relation',
+            'id',
+            'category',
+            'project',
+            'date',
+            'actor',
+            'patch',
+            'series',
+            'cover',
+            'previous_state',
+            'current_state',
+            'previous_delegate',
+            'current_delegate',
+            'created_check',
+            'previous_relation',
+            'current_relation',
+            'cover_comment',
+            'patch_comment',
         )
         read_only_fields = fields
         versioned_fields = {
-            '1.2': ('actor', ),
+            '1.2': ('actor',),
         }
 
 
@@ -98,8 +140,16 @@ class EventList(ListAPIView):
     ordering = '-date'
 
     def get_queryset(self):
-        return Event.objects.all()\
-            .prefetch_related('project', 'patch__project', 'series__project',
-                              'cover', 'previous_state', 'current_state',
-                              'previous_delegate', 'current_delegate',
-                              'created_check')
+        return Event.objects.all().prefetch_related(
+            'project',
+            'patch__project',
+            'series__project',
+            'cover',
+            'previous_state',
+            'current_state',
+            'previous_delegate',
+            'current_delegate',
+            'created_check',
+            'cover_comment',
+            'patch_comment',
+        )

docs/api/schemas/latest/patchwork.yaml
@@ -420,6 +420,8 @@ paths:
               - check-created
               - series-created
               - series-completed
+              - cover-comment-created
+              - patch-comment-created
         - in: query
           name: series
           description: An ID of a series to filter events by.
@@ -459,6 +461,8 @@ paths:
                     - $ref: '#/components/schemas/EventCheckCreated'
                     - $ref: '#/components/schemas/EventSeriesCreated'
                     - $ref: '#/components/schemas/EventSeriesCompleted'
+                    - $ref: '#/components/schemas/EventCoverCommentCreated'
+                    - $ref: '#/components/schemas/EventPatchCommentCreated'
                   discriminator:
                     propertyName: category
                     mapping:
@@ -471,6 +475,8 @@ paths:
                       check-created: '#/components/schemas/EventCheckCreated'
                       series-created: '#/components/schemas/EventSeriesCreated'
                       series-completed: '#/components/schemas/EventSeriesCompleted'
+                      cover-comment-created: '#/components/schemas/EventCoverCommentCreated'
+                      patch-comment-created: '#/components/schemas/EventPatchCommentCreated'
       tags:
         - events
   /api/patches/:
@@ -1919,6 +1925,34 @@ components:
               properties:
                 series:
                   $ref: '#/components/schemas/SeriesEmbedded'
+    EventCoverCommentCreated:
+      allOf:
+        - $ref: '#/components/schemas/EventBase'
+        - type: object
+          properties:
+            category:
+              enum:
+                - cover-comment-created
+            payload:
+              properties:
+                cover:
+                  $ref: '#/components/schemas/CoverEmbedded'
+                comment:
+                  $ref: '#/components/schemas/CommentEmbedded'
+    EventPatchCommentCreated:
+      allOf:
+        - $ref: '#/components/schemas/EventBase'
+        - type: object
+          properties:
+            category:
+              enum:
+                - patch-comment-created
+            payload:
+              properties:
+                patch:
+                  $ref: '#/components/schemas/PatchEmbedded'
+                comment:
+                  $ref: '#/components/schemas/CommentEmbedded'
     PatchList:
       required:
         - state
@@ -2394,6 +2428,43 @@ components:
           maxLength: 255
           minLength: 1
           readOnly: true
+    CommentEmbedded:
+      type: object
+      properties:
+        id:
+          title: ID
+          type: integer
+          readOnly: true
+        url:
+          title: URL
+          type: string
+          format: uri
+          readOnly: true
+        web_url:
+          title: Web URL
+          type: string
+          format: uri
+          readOnly: true
+        msgid:
+          title: Message ID
+          type: string
+          readOnly: true
+          minLength: 1
+        list_archive_url:
+          title: List archive URL
+          type: string
+          readOnly: true
+          nullable: true
+        date:
+          title: Date
+          type: string
+          format: iso8601
+          readOnly: true
+        name:
+          title: Name
+          type: string
+          readOnly: true
+          minLength: 1
     CoverEmbedded:
       type: object
       properties:

docs/api/schemas/patchwork.j2
@@ -431,6 +431,10 @@ paths:
               - check-created
               - series-created
               - series-completed
+{% if version >= (1, 3) %}
+              - cover-comment-created
+              - patch-comment-created
+{% endif %}
         - in: query
           name: series
           description: An ID of a series to filter events by.
@@ -470,6 +474,8 @@ paths:
                     - $ref: '#/components/schemas/EventCheckCreated'
                     - $ref: '#/components/schemas/EventSeriesCreated'
                     - $ref: '#/components/schemas/EventSeriesCompleted'
+                    - $ref: '#/components/schemas/EventCoverCommentCreated'
+                    - $ref: '#/components/schemas/EventPatchCommentCreated'
                   discriminator:
                     propertyName: category
                     mapping:
@@ -482,6 +488,8 @@ paths:
                       check-created: '#/components/schemas/EventCheckCreated'
                       series-created: '#/components/schemas/EventSeriesCreated'
                       series-completed: '#/components/schemas/EventSeriesCompleted'
+                      cover-comment-created: '#/components/schemas/EventCoverCommentCreated'
+                      patch-comment-created: '#/components/schemas/EventPatchCommentCreated'
       tags:
         - events
   /api/{{ version_url }}patches/:
@@ -1989,6 +1997,34 @@ components:
               properties:
                 series:
                   $ref: '#/components/schemas/SeriesEmbedded'
+    EventCoverCommentCreated:
+      allOf:
+        - $ref: '#/components/schemas/EventBase'
+        - type: object
+          properties:
+            category:
+              enum:
+                - cover-comment-created
+            payload:
+              properties:
+                cover:
+                  $ref: '#/components/schemas/CoverEmbedded'
+                comment:
+                  $ref: '#/components/schemas/CommentEmbedded'
+    EventPatchCommentCreated:
+      allOf:
+        - $ref: '#/components/schemas/EventBase'
+        - type: object
+          properties:
+            category:
+              enum:
+                - patch-comment-created
+            payload:
+              properties:
+                patch:
+                  $ref: '#/components/schemas/PatchEmbedded'
+                comment:
+                  $ref: '#/components/schemas/CommentEmbedded'
     PatchList:
       required:
         - state
@@ -2482,6 +2518,47 @@ components:
           maxLength: 255
           minLength: 1
           readOnly: true
+    CommentEmbedded:
+      type: object
+      properties:
+        id:
+          title: ID
+          type: integer
+          readOnly: true
+        url:
+          title: URL
+          type: string
+          format: uri
+          readOnly: true
+{% if version >= (1, 1) %}
+        web_url:
+          title: Web URL
+          type: string
+          format: uri
+          readOnly: true
+{% endif %}
+        msgid:
+          title: Message ID
+          type: string
+          readOnly: true
+          minLength: 1
+{% if version >= (1, 2) %}
+        list_archive_url:
+          title: List archive URL
+          type: string
+          readOnly: true
+          nullable: true
+{% endif %}
+        date:
+          title: Date
+          type: string
+          format: iso8601
+          readOnly: true
+        name:
+          title: Name
+          type: string
+          readOnly: true
+          minLength: 1
     CoverEmbedded:
       type: object
       properties:

docs/api/schemas/v1.0/patchwork.yaml
@@ -285,6 +285,8 @@ paths:
                     - $ref: '#/components/schemas/EventCheckCreated'
                     - $ref: '#/components/schemas/EventSeriesCreated'
                     - $ref: '#/components/schemas/EventSeriesCompleted'
+                    - $ref: '#/components/schemas/EventCoverCommentCreated'
+                    - $ref: '#/components/schemas/EventPatchCommentCreated'
                   discriminator:
                     propertyName: category
                     mapping:
@@ -297,6 +299,8 @@ paths:
                       check-created: '#/components/schemas/EventCheckCreated'
                       series-created: '#/components/schemas/EventSeriesCreated'
                       series-completed: '#/components/schemas/EventSeriesCompleted'
+                      cover-comment-created: '#/components/schemas/EventCoverCommentCreated'
+                      patch-comment-created: '#/components/schemas/EventPatchCommentCreated'
       tags:
         - events
   /api/1.0/patches/:
@@ -1562,6 +1566,34 @@ components:
               properties:
                 series:
                   $ref: '#/components/schemas/SeriesEmbedded'
+    EventCoverCommentCreated:
+      allOf:
+        - $ref: '#/components/schemas/EventBase'
+        - type: object
+          properties:
+            category:
+              enum:
+                - cover-comment-created
+            payload:
+              properties:
+                cover:
+                  $ref: '#/components/schemas/CoverEmbedded'
+                comment:
+                  $ref: '#/components/schemas/CommentEmbedded'
+    EventPatchCommentCreated:
+      allOf:
+        - $ref: '#/components/schemas/EventBase'
+        - type: object
+          properties:
+            category:
+              enum:
+                - patch-comment-created
+            payload:
+              properties:
+                patch:
+                  $ref: '#/components/schemas/PatchEmbedded'
+                comment:
+                  $ref: '#/components/schemas/CommentEmbedded'
     PatchList:
       required:
         - state
@@ -1951,6 +1983,33 @@ components:
           maxLength: 255
           minLength: 1
           readOnly: true
+    CommentEmbedded:
+      type: object
+      properties:
+        id:
+          title: ID
+          type: integer
+          readOnly: true
+        url:
+          title: URL
+          type: string
+          format: uri
+          readOnly: true
+        msgid:
+          title: Message ID
+          type: string
+          readOnly: true
+          minLength: 1
+        date:
+          title: Date
+          type: string
+          format: iso8601
+          readOnly: true
+        name:
+          title: Name
+          type: string
+          readOnly: true
+          minLength: 1
     CoverEmbedded:
       type: object
       properties:

commit d05a2ff9b97e233036c7cc86e8cfbdccc3d313a0

REST: Fix issues with comment-related events
When we introduced this functionality, we missed the fact that these
resources use nested-style URLs that need to be specially handled. Fix
this now.

Signed-off-by: Stephen Finucane <stephen@that.guru>
Fixes: e3d8f7548 ("REST: Add 'patch-comment-created', 'cover-comment-created' events")
Cc: Siddhesh Poyarekar <sipoyare@redhat.com>
Cc: DJ Delorie <dj@redhat.com>
Cc: Carlos O'Donell <carlos@redhat.com>

patchwork/api/base.py
@@ -139,6 +139,40 @@ class CheckHyperlinkedIdentityField(HyperlinkedIdentityField):
         )
 
 
+class CoverCommentHyperlinkedIdentityField(HyperlinkedIdentityField):
+    def get_url(self, obj, view_name, request, format):
+        # Unsaved objects will not yet have a valid URL.
+        if obj.pk is None:
+            return None
+
+        return self.reverse(
+            view_name,
+            kwargs={
+                'cover_id': obj.cover.id,
+                'comment_id': obj.id,
+            },
+            request=request,
+            format=format,
+        )
+
+
+class PatchCommentHyperlinkedIdentityField(HyperlinkedIdentityField):
+    def get_url(self, obj, view_name, request, format):
+        # Unsaved objects will not yet have a valid URL.
+        if obj.pk is None:
+            return None
+
+        return self.reverse(
+            view_name,
+            kwargs={
+                'patch_id': obj.patch.id,
+                'comment_id': obj.id,
+            },
+            request=request,
+            format=format,
+        )
+
+
 class BaseHyperlinkedModelSerializer(HyperlinkedModelSerializer):
     def to_representation(self, instance):
         data = super(BaseHyperlinkedModelSerializer, self).to_representation(

patchwork/api/embedded.py
@@ -17,6 +17,8 @@ from rest_framework.serializers import SerializerMethodField
 
 from patchwork.api.base import BaseHyperlinkedModelSerializer
 from patchwork.api.base import CheckHyperlinkedIdentityField
+from patchwork.api.base import CoverCommentHyperlinkedIdentityField
+from patchwork.api.base import PatchCommentHyperlinkedIdentityField
 from patchwork import models
 
 
@@ -127,6 +129,9 @@ class CoverSerializer(SerializedRelatedField):
 
 class CoverCommentSerializer(SerializedRelatedField):
     class _Serializer(MboxMixin, WebURLMixin, BaseHyperlinkedModelSerializer):
+
+        url = CoverCommentHyperlinkedIdentityField('api-cover-comment-detail')
+
         class Meta:
             model = models.CoverComment
             fields = (
@@ -136,7 +141,6 @@ class CoverCommentSerializer(SerializedRelatedField):
                 'msgid',
                 'list_archive_url',
                 'date',
-                'name',
             )
             read_only_fields = fields
             versioned_fields = {
@@ -177,6 +181,9 @@ class PatchSerializer(SerializedRelatedField):
 
 class PatchCommentSerializer(SerializedRelatedField):
     class _Serializer(MboxMixin, WebURLMixin, BaseHyperlinkedModelSerializer):
+
+        url = PatchCommentHyperlinkedIdentityField('api-patch-comment-detail')
+
         class Meta:
             model = models.PatchComment
             fields = (
@@ -186,7 +193,6 @@ class PatchCommentSerializer(SerializedRelatedField):
                 'msgid',
                 'list_archive_url',
                 'date',
-                'name',
             )
             read_only_fields = fields
             versioned_fields = {

patchwork/api/event.py
@@ -19,6 +19,7 @@ from patchwork.api.embedded import ProjectSerializer
 from patchwork.api.embedded import SeriesSerializer
 from patchwork.api.embedded import UserSerializer
 from patchwork.api.filters import EventFilterSet
+from patchwork.api import utils
 from patchwork.models import Event
 
 
@@ -140,7 +141,7 @@ class EventList(ListAPIView):
     ordering = '-date'
 
     def get_queryset(self):
-        return Event.objects.all().prefetch_related(
+        events = Event.objects.all().prefetch_related(
             'project',
             'patch__project',
             'series__project',
@@ -150,6 +151,24 @@ class EventList(ListAPIView):
             'previous_delegate',
             'current_delegate',
             'created_check',
-            'cover_comment',
-            'patch_comment',
         )
+        # NOTE(stephenfin): We need to exclude comment-related events because
+        # until API v1.3, we didn't have an comment detail API to point to.
+        # This goes against our pledge to version events in the docs but must
+        # be done.
+        # TODO(stephenfin): Make this more generic.
+        if utils.has_version(self.request, '1.3'):
+            events = events.prefetch_related(
+                'cover_comment',
+                'cover_comment__cover__project',
+                'patch_comment',
+                'patch_comment__patch__project',
+            )
+        else:
+            events = events.exclude(
+                category__in=[
+                    Event.CATEGORY_COVER_COMMENT_CREATED,
+                    Event.CATEGORY_PATCH_COMMENT_CREATED,
+                ]
+            )
+        return events

releasenotes/notes/issue-499-1ee87a1e9d502dc6.yaml
@@ -1,0 +1,5 @@
+---
+fixes:
+  - |
+    Fix an issue whereby comment-based events would cause a HTTP 500 error
+    for the events API (``/api/events``).

patchwork/tests/api/test_event.py
@@ -12,8 +12,10 @@ from patchwork.models import Event
 from patchwork.tests.api import utils
 from patchwork.tests.utils import create_check
 from patchwork.tests.utils import create_cover
+from patchwork.tests.utils import create_cover_comment
 from patchwork.tests.utils import create_maintainer
 from patchwork.tests.utils import create_patch
+from patchwork.tests.utils import create_patch_comment
 from patchwork.tests.utils import create_series
 from patchwork.tests.utils import create_state
 
@@ -70,7 +72,7 @@ class TestEventAPI(APITestCase):
         # patch-created, patch-completed, series-completed
         patch = create_patch(series=series)
         # cover-created
-        create_cover(series=series)
+        cover = create_cover(series=series)
         # check-created
         create_check(patch=patch)
         # patch-delegated, patch-state-changed
@@ -81,6 +83,9 @@ class TestEventAPI(APITestCase):
         patch.state = state
         self.assertTrue(patch.is_editable(actor))
         patch.save()
+        # patch-cover-created, cover-comment-created
+        create_patch_comment(patch=patch, submitter=patch.submitter)
+        create_cover_comment(cover=cover, submitter=cover.submitter)
 
         return Event.objects.all()
 
@@ -91,7 +96,9 @@ class TestEventAPI(APITestCase):
 
         resp = self.client.get(self.api_url())
         self.assertEqual(status.HTTP_200_OK, resp.status_code)
-        self.assertEqual(8, len(resp.data), [x['category'] for x in resp.data])
+        self.assertEqual(
+            10, len(resp.data), [x['category'] for x in resp.data]
+        )
         for event_rsp in resp.data:
             event_obj = events.get(category=event_rsp['category'])
             self.assertSerialized(event_obj, event_rsp)
@@ -104,7 +111,7 @@ class TestEventAPI(APITestCase):
 
         resp = self.client.get(self.api_url(), {'project': project.pk})
         # All but one event belongs to the same project
-        self.assertEqual(8, len(resp.data))
+        self.assertEqual(10, len(resp.data))
 
         resp = self.client.get(self.api_url(), {'project': 'invalidproject'})
         self.assertEqual(0, len(resp.data))
@@ -132,9 +139,9 @@ class TestEventAPI(APITestCase):
 
         patch = events.get(category='patch-created').patch
         resp = self.client.get(self.api_url(), {'patch': patch.pk})
-        # There should be five - patch-created, patch-completed, check-created,
-        # patch-state-changed and patch-delegated
-        self.assertEqual(5, len(resp.data))
+        # There should be six - patch-created, patch-completed, check-created,
+        # patch-state-changed, patch-delegated and patch-comment-created
+        self.assertEqual(6, len(resp.data))
 
         resp = self.client.get(self.api_url(), {'patch': 999999})
         self.assertEqual(0, len(resp.data))
@@ -145,8 +152,8 @@ class TestEventAPI(APITestCase):
 
         cover = events.get(category='cover-created').cover
         resp = self.client.get(self.api_url(), {'cover': cover.pk})
-        # There should only be one - cover-created
-        self.assertEqual(1, len(resp.data))
+        # There should be two - cover-created and cover-comment-created
+        self.assertEqual(2, len(resp.data))
 
         resp = self.client.get(self.api_url(), {'cover': 999999})
         self.assertEqual(0, len(resp.data))
@@ -170,7 +177,7 @@ class TestEventAPI(APITestCase):
 
         # The final two events (patch-delegated, patch-state-changed)
         # have an actor set
-        actor = events[0].actor
+        actor = events.get(category='patch-delegated').actor
         resp = self.client.get(self.api_url(), {'actor': actor.pk})
         self.assertEqual(2, len(resp.data))
 
@@ -185,14 +192,15 @@ class TestEventAPI(APITestCase):
         resp = self.client.get(
             self.api_url(version='1.1'), {'actor': 'foo-bar'}
         )
-        self.assertEqual(len(events), len(resp.data))
+        # we don't see the two comment-related fields
+        self.assertEqual(len(events) - 2, len(resp.data))
 
     def test_list_bug_335(self):
         """Ensure we retrieve the embedded series project once."""
         for _ in range(3):
             self._create_events()
 
-        with self.assertNumQueries(27):
+        with self.assertNumQueries(33):
             self.client.get(self.api_url())
 
     def test_order_by_date_default(self):