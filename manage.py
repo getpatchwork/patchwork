b5769e6aabf6e42ffeb24f4a8edd88a54b11dd72
Stephen Finucane <stephen@that.guru>
2022-09-30 14:04:19 +0100

    manage: Check Django version on startup
    This was recently reported as an issue. Add a simple check to ensure
    people update their dependencies as expected.
    
    Signed-off-by: Stephen Finucane <stephen@that.guru>
    Cc: Siddhesh Poyarekar <sipoyare@redhat.com>
    Cc: DJ Delorie <dj@redhat.com>
    Cc: Carlos O'Donell <carlos@redhat.com>

--- manage.py
+++ manage.py
@@ -7,6 +7,11 @@ if __name__ == "__main__":
         "DJANGO_SETTINGS_MODULE", "patchwork.settings.production"
     )
 
+    import django
+
+    if django.VERSION < (3, 2):
+        raise Exception('Patchwork requires Django 3.2 or greater')
+
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
