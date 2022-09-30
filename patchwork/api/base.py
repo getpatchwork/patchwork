getpatchwork/patchwork

commit 88f56051c41de5dc643a7ce1c0911974d8cb3c70

api: add comments detail endpoint
Add new endpoint for patch and cover comments at api/.../comments/<comment_id>.
This involves updating the API version to v1.3 to reflect the new
endpoints as a minor change, following the usual semantic versioning
convention.

The endpoint will make it possible to use the REST API to update the new
`addressed` field for individual patch and cover comments with JavaScript
on the client side. In the process of these changes, clean up the use of
the CurrentPatchDefault context so that it exists in base.py and can be
used throughout the API (e.g. Check and Comment REST endpoints).

The tests cover retrieval and update requests and also handle calls from
the various API versions. Also, they cover permissions for update
requests and handle invalid update values for the new `addressed` field.

Signed-off-by: Raxel Gutierrez <raxel@google.com>
[dja: changes to not conflict with, and to adopt the changes in, fecf7c86c2c5
      various other minor changes as described on list]
Signed-off-by: Daniel Axtens <dja@axtens.net>

docs/api/schemas/patchwork.j2
@@ -324,6 +324,74 @@ paths:
                 $ref: '#/components/schemas/Error'
       tags:
         - comments
+{% if version >= (1, 3) %}
+  /api/{{ version_url }}covers/{cover_id}/comments/{comment_id}/:
+    parameters:
+      - in: path
+        name: cover_id
+        description: A unique integer value identifying the parent cover.
+        required: true
+        schema:
+          title: Cover ID
+          type: integer
+      - in: path
+        name: comment_id
+        description: A unique integer value identifying this comment.
+        required: true
+        schema:
+          title: Comment ID
+          type: integer
+    get:
+      description: Show a cover comment.
+      operationId: cover_comments_read
+      responses:
+        '200':
+          description: ''
+          content:
+            application/json:
+              schema:
+                $ref: '#/components/schemas/Comment'
+        '404':
+          description: Not found
+          content:
+            application/json:
+              schema:
+                $ref: '#/components/schemas/Error'
+      tags:
+        - comments
+    patch:
+      description: Update a cover comment (partial).
+      operationId: cover_comments_partial_update
+      requestBody:
+        $ref: '#/components/requestBodies/Comment'
+      responses:
+        '200':
+          description: ''
+          content:
+            application/json:
+              schema:
+                $ref: '#/components/schemas/Comment'
+        '400':
+          description: Invalid Request
+          content:
+            application/json:
+              schema:
+                $ref: '#/components/schemas/ErrorCommentUpdate'
+        '403':
+          description: Forbidden
+          content:
+            application/json:
+              schema:
+                $ref: '#/components/schemas/Error'
+        '404':
+          description: Not found
+          content:
+            application/json:
+              schema:
+                $ref: '#/components/schemas/Error'
+      tags:
+        - comments
+{% endif %}
   /api/{{ version_url }}events/:
     get:
       description: List events.
@@ -656,6 +724,74 @@ paths:
                 $ref: '#/components/schemas/Error'
       tags:
         - comments
+{% if version >= (1, 3) %}
+  /api/{{ version_url }}patches/{patch_id}/comments/{comment_id}/:
+    parameters:
+      - in: path
+        name: patch_id
+        description: A unique integer value identifying the parent patch.
+        required: true
+        schema:
+          title: Patch ID
+          type: integer
+      - in: path
+        name: comment_id
+        description: A unique integer value identifying this comment.
+        required: true
+        schema:
+          title: Comment ID
+          type: integer
+    get:
+      description: Show a patch comment.
+      operationId: patch_comments_read
+      responses:
+        '200':
+          description: ''
+          content:
+            application/json:
+              schema:
+                $ref: '#/components/schemas/Comment'
+        '404':
+          description: Not found
+          content:
+            application/json:
+              schema:
+                $ref: '#/components/schemas/Error'
+      tags:
+        - comments
+    patch:
+      description: Update a patch comment (partial).
+      operationId: patch_comments_partial_update
+      requestBody:
+        $ref: '#/components/requestBodies/Comment'
+      responses:
+        '200':
+          description: ''
+          content:
+            application/json:
+              schema:
+                $ref: '#/components/schemas/Comment'
+        '400':
+          description: Invalid Request
+          content:
+            application/json:
+              schema:
+                $ref: '#/components/schemas/ErrorCommentUpdate'
+        '403':
+          description: Forbidden
+          content:
+            application/json:
+              schema:
+                $ref: '#/components/schemas/Error'
+        '404':
+          description: Not found
+          content:
+            application/json:
+              schema:
+                $ref: '#/components/schemas/Error'
+      tags:
+        - comments
+{% endif %}
   /api/{{ version_url }}patches/{patch_id}/checks/:
     parameters:
       - in: path
@@ -1277,6 +1413,14 @@ components:
         application/x-www-form-urlencoded:
           schema:
             $ref: '#/components/schemas/CheckCreate'
+{% if version >= (1, 3) %}
+    Comment:
+      required: true
+      content:
+        application/json:
+          schema:
+            $ref: '#/components/schemas/CommentUpdate'
+{% endif %}
     Patch:
       required: true
       content:
@@ -1586,6 +1730,17 @@ components:
               additionalProperties:
                 type: string
           readOnly: true
+{% if version >= (1, 3) %}
+        addressed:
+          title: Addressed
+          type: boolean
+    CommentUpdate:
+      type: object
+      properties:
+        addressed:
+          title: Addressed
+          type: boolean
+{% endif %}
     CoverList:
       type: object
       properties:
@@ -2659,6 +2814,16 @@ components:
           items:
             type: string
           readOnly: true
+{% if version >= (1, 3) %}
+    ErrorCommentUpdate:
+      type: object
+      properties:
+        addressed:
+          title: Addressed
+          type: array
+          items:
+            type: string
+{% endif %}
     ErrorPatchUpdate:
       type: object
       properties:

patchwork/api/base.py
@@ -3,6 +3,7 @@
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
 
+import rest_framework
 
 from django.conf import settings
 from django.shortcuts import get_object_or_404
@@ -13,6 +14,37 @@ from rest_framework.serializers import HyperlinkedIdentityField
 from rest_framework.serializers import HyperlinkedModelSerializer
 
 from patchwork.api import utils
+
+
+DRF_VERSION = tuple(int(x) for x in rest_framework.__version__.split('.'))
+
+
+if DRF_VERSION > (3, 11):
+    class CurrentPatchDefault(object):
+        requires_context = True
+
+        def __call__(self, serializer_field):
+            return serializer_field.context['request'].patch
+
+    class CurrentCoverDefault(object):
+        requires_context = True
+
+        def __call__(self, serializer_field):
+            return serializer_field.context['request'].cover
+else:
+    class CurrentPatchDefault(object):
+        def set_context(self, serializer_field):
+            self.patch = serializer_field.context['request'].patch
+
+        def __call__(self):
+            return self.patch
+
+    class CurrentCoverDefault(object):
+        def set_context(self, serializer_field):
+            self.patch = serializer_field.context['request'].cover
+
+        def __call__(self):
+            return self.cover
 
 
 class LinkHeaderPagination(PageNumberPagination):
@@ -44,7 +76,10 @@ class LinkHeaderPagination(PageNumberPagination):
 
 
 class PatchworkPermission(permissions.BasePermission):
-    """This permission works for Project and Patch model objects"""
+    """
+    This permission works for Project, Patch, PatchComment
+    and CoverComment model objects
+    """
     def has_object_permission(self, request, view, obj):
         # read only for everyone
         if request.method in permissions.SAFE_METHODS:

patchwork/api/comment.py
@@ -7,10 +7,15 @@ import email.parser
 
 from rest_framework.generics import get_object_or_404
 from rest_framework.generics import ListAPIView
+from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.serializers import HiddenField
 from rest_framework.serializers import SerializerMethodField
 
 from patchwork.api.base import BaseHyperlinkedModelSerializer
+from patchwork.api.base import MultipleFieldLookupMixin
 from patchwork.api.base import PatchworkPermission
+from patchwork.api.base import CurrentCoverDefault
+from patchwork.api.base import CurrentPatchDefault
 from patchwork.api.embedded import PersonSerializer
 from patchwork.models import Cover
 from patchwork.models import CoverComment
@@ -49,63 +54,135 @@ class BaseCommentListSerializer(BaseHyperlinkedModelSerializer):
 
     class Meta:
         fields = ('id', 'web_url', 'msgid', 'list_archive_url', 'date',
-                  'subject', 'submitter', 'content', 'headers')
-        read_only_fields = fields
+                  'subject', 'submitter', 'content', 'headers', 'addressed')
+        read_only_fields = ('id', 'web_url', 'msgid', 'list_archive_url',
+                            'date', 'subject', 'submitter', 'content',
+                            'headers')
         versioned_fields = {
             '1.1': ('web_url', ),
             '1.2': ('list_archive_url',),
+            '1.3': ('addressed',),
         }
 
 
-class CoverCommentListSerializer(BaseCommentListSerializer):
+class CoverCommentSerializer(BaseCommentListSerializer):
+
+    cover = HiddenField(default=CurrentCoverDefault())
 
     class Meta:
         model = CoverComment
-        fields = BaseCommentListSerializer.Meta.fields
-        read_only_fields = fields
+        fields = BaseCommentListSerializer.Meta.fields + (
+            'cover', 'addressed')
+        read_only_fields = BaseCommentListSerializer.Meta.read_only_fields + (
+            'cover', )
         versioned_fields = BaseCommentListSerializer.Meta.versioned_fields
+        extra_kwargs = {
+            'url': {'view_name': 'api-cover-comment-detail'}
+        }
 
 
-class PatchCommentListSerializer(BaseCommentListSerializer):
+class CoverCommentMixin(object):
+
+    permission_classes = (PatchworkPermission,)
+    serializer_class = CoverCommentSerializer
+
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+        comment_id = self.kwargs['comment_id']
+        obj = get_object_or_404(queryset, id=comment_id)
+        self.check_object_permissions(self.request, obj)
+        return obj
+
+    def get_queryset(self):
+        cover_id = self.kwargs['cover_id']
+        get_object_or_404(Cover, pk=cover_id)
+
+        return CoverComment.objects.filter(
+            cover=cover_id
+        ).select_related('submitter')
+
+
+class PatchCommentSerializer(BaseCommentListSerializer):
+
+    patch = HiddenField(default=CurrentPatchDefault())
 
     class Meta:
         model = PatchComment
-        fields = BaseCommentListSerializer.Meta.fields
-        read_only_fields = fields
+        fields = BaseCommentListSerializer.Meta.fields + ('patch', )
+        read_only_fields = BaseCommentListSerializer.Meta.read_only_fields + (
+            'patch', )
         versioned_fields = BaseCommentListSerializer.Meta.versioned_fields
+        extra_kwargs = {
+            'url': {'view_name': 'api-patch-comment-detail'}
+        }
 
 
-class CoverCommentList(ListAPIView):
-    """List cover comments"""
+class PatchCommentMixin(object):
 
     permission_classes = (PatchworkPermission,)
-    serializer_class = CoverCommentListSerializer
+    serializer_class = PatchCommentSerializer
+
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+        comment_id = self.kwargs['comment_id']
+        obj = get_object_or_404(queryset, id=comment_id)
+        self.check_object_permissions(self.request, obj)
+        return obj
+
+    def get_queryset(self):
+        patch_id = self.kwargs['patch_id']
+        get_object_or_404(Patch, id=patch_id)
+
+        return PatchComment.objects.filter(
+            patch=patch_id
+        ).select_related('submitter')
+
+
+class CoverCommentList(CoverCommentMixin, ListAPIView):
+    """List cover comments"""
+
     search_fields = ('subject',)
     ordering_fields = ('id', 'subject', 'date', 'submitter')
     ordering = 'id'
     lookup_url_kwarg = 'cover_id'
 
-    def get_queryset(self):
-        get_object_or_404(Cover, pk=self.kwargs['cover_id'])
 
-        return CoverComment.objects.filter(
-            cover=self.kwargs['cover_id']
-        ).select_related('submitter')
+class CoverCommentDetail(CoverCommentMixin, MultipleFieldLookupMixin,
+                         RetrieveUpdateAPIView):
+    """
+    get:
+    Show a cover comment.
+
+    patch:
+    Update a cover comment.
+
+    put:
+    Update a cover comment.
+    """
+    lookup_url_kwargs = ('cover_id', 'comment_id')
+    lookup_fields = ('cover_id', 'id')
 
 
-class PatchCommentList(ListAPIView):
-    """List comments"""
+class PatchCommentList(PatchCommentMixin, ListAPIView):
+    """List patch comments"""
 
-    permission_classes = (PatchworkPermission,)
-    serializer_class = PatchCommentListSerializer
     search_fields = ('subject',)
     ordering_fields = ('id', 'subject', 'date', 'submitter')
     ordering = 'id'
     lookup_url_kwarg = 'patch_id'
 
-    def get_queryset(self):
-        get_object_or_404(Patch, id=self.kwargs['patch_id'])
 
-        return PatchComment.objects.filter(
-            patch=self.kwargs['patch_id']
-        ).select_related('submitter')
+class PatchCommentDetail(PatchCommentMixin, MultipleFieldLookupMixin,
+                         RetrieveUpdateAPIView):
+    """
+    get:
+    Show a patch comment.
+
+    patch:
+    Update a patch comment.
+
+    put:
+    Update a patch comment.
+    """
+    lookup_url_kwargs = ('patch_id', 'comment_id')
+    lookup_fields = ('patch_id', 'id')

docs/api/schemas/generate-schemas.py
@@ -14,8 +14,8 @@ except ImportError:
     yaml = None
 
 ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
-VERSIONS = [(1, 0), (1, 1), (1, 2), None]
-LATEST_VERSION = (1, 2)
+VERSIONS = [(1, 0), (1, 1), (1, 2), (1, 3), None]
+LATEST_VERSION = (1, 3)
 
 
 def generate_schemas():

patchwork/api/check.py
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 from django.http.request import QueryDict
-import rest_framework
+
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.generics import get_object_or_404
 from rest_framework.generics import ListCreateAPIView
@@ -16,28 +16,11 @@ from rest_framework.serializers import ValidationError
 
 from patchwork.api.base import CheckHyperlinkedIdentityField
 from patchwork.api.base import MultipleFieldLookupMixin
+from patchwork.api.base import CurrentPatchDefault
 from patchwork.api.embedded import UserSerializer
 from patchwork.api.filters import CheckFilterSet
 from patchwork.models import Check
 from patchwork.models import Patch
-
-
-DRF_VERSION = tuple(int(x) for x in rest_framework.__version__.split('.'))
-
-
-if DRF_VERSION > (3, 11):
-    class CurrentPatchDefault(object):
-        requires_context = True
-
-        def __call__(self, serializer_field):
-            return serializer_field.context['request'].patch
-else:
-    class CurrentPatchDefault(object):
-        def set_context(self, serializer_field):
-            self.patch = serializer_field.context['request'].patch
-
-        def __call__(self):
-            return self.patch
 
 
 class CheckSerializer(HyperlinkedModelSerializer):

patchwork/urls.py
@@ -343,12 +343,28 @@ if settings.ENABLE_REST_API:
         ),
     ]
 
+    api_1_3_patterns = [
+        path(
+            'patches/<int:patch_id>/comments/<int:comment_id>/',
+            api_comment_views.PatchCommentDetail.as_view(),
+            name='api-patch-comment-detail',
+        ),
+        path(
+            'covers/<int:cover_id>/comments/<int:comment_id>/',
+            api_comment_views.CoverCommentDetail.as_view(),
+            name='api-cover-comment-detail',
+        ),
+    ]
+
     urlpatterns += [
         re_path(
-            r'^api/(?:(?P<version>(1.0|1.1|1.2))/)?', include(api_patterns)
+            r'^api/(?:(?P<version>(1.0|1.1|1.2|1.3))/)?', include(api_patterns)
         ),
         re_path(
-            r'^api/(?:(?P<version>(1.1|1.2))/)?', include(api_1_1_patterns)
+            r'^api/(?:(?P<version>(1.1|1.2|1.3))/)?', include(api_1_1_patterns)
+        ),
+        re_path(
+            r'^api/(?:(?P<version>(1.3))/)?', include(api_1_3_patterns)
         ),
         # token change
         path(

commit 73b0b12df256c4e2e9c5e724828c778e69f0cf13

REST: Add missing 'url' parameter for comments
This should be present on all resources.

Signed-off-by: Stephen Finucane <stephen@that.guru>
Fixes: 88f56051 ("api: add comments detail endpoint")

docs/api/schemas/latest/patchwork.yaml
@@ -1627,6 +1627,11 @@ components:
           title: ID
           type: integer
           readOnly: true
+        url:
+          title: URL
+          type: string
+          format: uri
+          readOnly: true
         web_url:
           title: Web URL
           type: string

docs/api/schemas/patchwork.j2
@@ -1683,6 +1683,13 @@ components:
           title: ID
           type: integer
           readOnly: true
+{% if version >= (1, 3) %}
+        url:
+          title: URL
+          type: string
+          format: uri
+          readOnly: true
+{% endif %}
 {% if version >= (1, 1) %}
         web_url:
           title: Web URL
@@ -2528,11 +2535,13 @@ components:
           title: ID
           type: integer
           readOnly: true
+{% if version >= (1, 3) %}
         url:
           title: URL
           type: string
           format: uri
           readOnly: true
+{% endif %}
 {% if version >= (1, 1) %}
         web_url:
           title: Web URL

docs/api/schemas/v1.0/patchwork.yaml
@@ -1993,11 +1993,6 @@ components:
           title: ID
           type: integer
           readOnly: true
-        url:
-          title: URL
-          type: string
-          format: uri
-          readOnly: true
         msgid:
           title: Message ID
           type: string

docs/api/schemas/v1.1/patchwork.yaml
@@ -2044,11 +2044,6 @@ components:
           title: ID
           type: integer
           readOnly: true
-        url:
-          title: URL
-          type: string
-          format: uri
-          readOnly: true
         web_url:
           title: Web URL
           type: string

docs/api/schemas/v1.2/patchwork.yaml
@@ -2287,11 +2287,6 @@ components:
           title: ID
           type: integer
           readOnly: true
-        url:
-          title: URL
-          type: string
-          format: uri
-          readOnly: true
         web_url:
           title: Web URL
           type: string

docs/api/schemas/v1.3/patchwork.yaml
@@ -1627,6 +1627,11 @@ components:
           title: ID
           type: integer
           readOnly: true
+        url:
+          title: URL
+          type: string
+          format: uri
+          readOnly: true
         web_url:
           title: Web URL
           type: string

patchwork/api/base.py
@@ -151,19 +151,17 @@ class NestedHyperlinkedIdentityField(HyperlinkedIdentityField):
 
 class BaseHyperlinkedModelSerializer(HyperlinkedModelSerializer):
     def to_representation(self, instance):
-        data = super(BaseHyperlinkedModelSerializer, self).to_representation(
-            instance
-        )
-
         request = self.context.get('request')
         for version in getattr(self.Meta, 'versioned_fields', {}):
             # if the user has requested a version lower that than in which the
             # field was added, we drop it
             if not utils.has_version(request, version):
                 for field in self.Meta.versioned_fields[version]:
-                    # After a PATCH with an older API version, we may not see
-                    # these fields. If they don't exist, don't panic, return
-                    # (and then discard) None.
-                    data.pop(field, None)
+                    if field in self.fields:
+                        del self.fields[field]
+
+        data = super(BaseHyperlinkedModelSerializer, self).to_representation(
+            instance
+        )
 
         return data

patchwork/api/bundle.py
@@ -99,7 +99,7 @@ class BundleSerializer(BaseHyperlinkedModelSerializer):
 
         if len(set([p.project.id for p in value])) > 1:
             raise ValidationError(
-                'Bundle patches must belong to the same ' 'project'
+                'Bundle patches must belong to the same project'
             )
 
         return value

patchwork/api/comment.py
@@ -12,6 +12,7 @@ from rest_framework.serializers import HiddenField
 from rest_framework.serializers import SerializerMethodField
 
 from patchwork.api.base import BaseHyperlinkedModelSerializer
+from patchwork.api.base import NestedHyperlinkedIdentityField
 from patchwork.api.base import MultipleFieldLookupMixin
 from patchwork.api.base import PatchworkPermission
 from patchwork.api.base import CurrentCoverDefault
@@ -58,6 +59,7 @@ class BaseCommentListSerializer(BaseHyperlinkedModelSerializer):
     class Meta:
         fields = (
             'id',
+            'url',
             'web_url',
             'msgid',
             'list_archive_url',
@@ -70,6 +72,7 @@ class BaseCommentListSerializer(BaseHyperlinkedModelSerializer):
         )
         read_only_fields = (
             'id',
+            'url',
             'web_url',
             'msgid',
             'list_archive_url',
@@ -82,17 +85,27 @@ class BaseCommentListSerializer(BaseHyperlinkedModelSerializer):
         versioned_fields = {
             '1.1': ('web_url',),
             '1.2': ('list_archive_url',),
-            '1.3': ('addressed',),
+            '1.3': (
+                'addressed',
+                'url',
+            ),
         }
 
 
 class CoverCommentSerializer(BaseCommentListSerializer):
 
+    url = NestedHyperlinkedIdentityField(
+        'api-cover-comment-detail',
+        lookup_field_mapping={
+            'cover_id': 'cover_id',
+            'comment_id': 'id',
+        },
+    )
     cover = HiddenField(default=CurrentCoverDefault())
 
     class Meta:
         model = CoverComment
-        fields = BaseCommentListSerializer.Meta.fields + ('cover', 'addressed')
+        fields = BaseCommentListSerializer.Meta.fields + ('cover',)
         read_only_fields = BaseCommentListSerializer.Meta.read_only_fields + (
             'cover',
         )
@@ -123,6 +136,13 @@ class CoverCommentMixin(object):
 
 class PatchCommentSerializer(BaseCommentListSerializer):
 
+    url = NestedHyperlinkedIdentityField(
+        'api-patch-comment-detail',
+        lookup_field_mapping={
+            'patch_id': 'patch_id',
+            'comment_id': 'id',
+        },
+    )
     patch = HiddenField(default=CurrentPatchDefault())
 
     class Meta:

patchwork/api/embedded.py
@@ -163,6 +163,7 @@ class CoverCommentSerializer(SerializedRelatedField):
                     'mbox',
                 ),
                 '1.2': ('list_archive_url',),
+                '1.3': ('url',),
             }
             extra_kwargs = {
                 'url': {'view_name': 'api-cover-comment-detail'},
@@ -225,6 +226,7 @@ class PatchCommentSerializer(SerializedRelatedField):
                     'mbox',
                 ),
                 '1.2': ('list_archive_url',),
+                '1.3': ('url',),
             }
             extra_kwargs = {
                 'url': {'view_name': 'api-patch-comment-detail'},

patchwork/api/patch.py
@@ -180,6 +180,7 @@ class PatchListSerializer(BaseHyperlinkedModelSerializer):
             'related',
         )
         read_only_fields = (
+            'url',
             'web_url',
             'project',
             'msgid',