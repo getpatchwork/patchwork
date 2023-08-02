002f86e3d71037a4b5aa8afbe5cd3e1f526c99f1
Stephen Finucane <stephen@that.guru>
2023-08-02 11:19:36 +0100

    Remove filter for openapi-core
    Effectively revert commit 4ddcdea4: the PR referenced merged a long time
    ago.
    
    Signed-off-by: Stephen Finucane <stephen@that.guru>

--- patchwork/tests/__init__.py
+++ patchwork/tests/__init__.py
@@ -13,15 +13,3 @@ TEST_FUZZ_DIR = os.path.join(os.path.dirname(__file__), 'fuzztests')
 # configure warnings
 
 warnings.simplefilter('once', DeprecationWarning)
-
-# TODO: Remove this once [1] merges and is released
-# [1] https://github.com/p1c2u/openapi-core/pull/395
-warnings.filterwarnings(
-    'ignore',
-    message=(
-        'The distutils package is deprecated and slated for removal in Python '
-        '3.12. Use setuptools or check PEP 632 for potential alternatives'
-    ),
-    module='openapi_core',
-    category=DeprecationWarning,
-)
