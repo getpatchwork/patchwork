b6b4989fc304a7fb3ebb5d8805844aa585236bc0
andrepapoti <andrepapoti@gmail.com>
2024-04-01 10:41:12 -0300

    api: Update Patch related serializers to accomodate for the new 'planning_to_review' field
    Signed-off-by: andrepapoti <andrepapoti@gmail.com>

--- patchwork/api/patch.py
+++ patchwork/api/patch.py
@@ -8,6 +8,7 @@
 import email.parser
 
 from django.core.exceptions import ValidationError
+from django.utils import timezone
 from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
 from rest_framework.exceptions import APIException
@@ -29,6 +30,7 @@ from patchwork.api.embedded import SeriesSerializer
 from patchwork.api.embedded import UserSerializer
 from patchwork.api.filters import PatchFilterSet
 from patchwork.models import Patch
+from patchwork.models import PatchReviewIntention
 from patchwork.models import PatchRelation
 from patchwork.models import State
 from patchwork.models import User
@@ -78,12 +80,41 @@ class PatchConflict(APIException):
     )
 
 
+class PatchReviewIntentionSerializer(BaseHyperlinkedModelSerializer):
+    user = UserSerializer()
+    patch = PatchSerializer()
+    is_stale = SerializerMethodField()
+
+    def get_is_stale(self, review_intention):
+        expiration_time = (
+            review_intention.patch.state.review_intention_expiration_time
+        )
+        valid_until = (
+            review_intention.last_time_marked_for_review + expiration_time
+        )
+
+        return timezone.now() > valid_until
+
+    class Meta:
+        model = PatchReviewIntention
+        fields = [
+            'id',
+            'user',
+            'patch',
+            'last_time_marked_for_review',
+            'is_stale',
+        ]
+
+
 class PatchListSerializer(BaseHyperlinkedModelSerializer):
     web_url = SerializerMethodField()
     project = ProjectSerializer(read_only=True)
     state = StateField()
     submitter = PersonSerializer(read_only=True)
     delegate = UserSerializer(allow_null=True)
+    planning_to_review = PatchReviewIntentionSerializer(
+        source='patchreviewintention_set', many=True
+    )
     mbox = SerializerMethodField()
     series = SeriesSerializer(read_only=True)
     comments = SerializerMethodField()
@@ -172,6 +203,8 @@ class PatchListSerializer(BaseHyperlinkedModelSerializer):
             'hash',
             'submitter',
             'delegate',
+            'planning_to_review',
+            'has_planned_review',
             'mbox',
             'series',
             'comments',
@@ -190,6 +223,7 @@ class PatchListSerializer(BaseHyperlinkedModelSerializer):
             'name',
             'hash',
             'submitter',
+            'has_planned_review',
             'mbox',
             'series',
             'comments',
@@ -230,16 +264,11 @@ class PatchDetailSerializer(PatchListSerializer):
     def get_prefixes(self, instance):
         return clean_subject(instance.name)[1]
 
-    def update(self, instance, validated_data):
-        # d-r-f cannot handle writable nested models, so we handle that
-        # specifically ourselves and let d-r-f handle the rest
-        if 'related' not in validated_data:
-            return super(PatchDetailSerializer, self).update(
-                instance, validated_data
-            )
+    def update_planning_to_review(self, instance, patchreviewintention_set):
+        intereted_user = patchreviewintention_set.pop()['user']
+        instance.planning_to_review.add(intereted_user.id)
 
-        related = validated_data.pop('related')
-
+    def update_related(self, instance, related):
         # Validation rules
         # ----------------
         #
@@ -280,9 +309,7 @@ class PatchDetailSerializer(PatchListSerializer):
             if instance.related and instance.related.patches.count() == 2:
                 instance.related.delete()
             instance.related = None
-            return super(PatchDetailSerializer, self).update(
-                instance, validated_data
-            )
+            return
 
         # break before make
         relations = {patch.related for patch in patches if patch.related}
@@ -305,6 +332,20 @@ class PatchDetailSerializer(PatchListSerializer):
             patch.save()
         instance.related = relation
         instance.save()
+
+    def update(self, instance, validated_data):
+        # d-r-f cannot handle writable nested models, so we handle that
+        # specifically ourselves and let d-r-f handle the rest
+
+        if 'related' in validated_data:
+            related = validated_data.pop('related')
+            self.update_related(instance, related)
+
+        if 'patchreviewintention_set' in validated_data:
+            patchreviewintention_set = validated_data.pop(
+                'patchreviewintention_set'
+            )
+            self.update_planning_to_review(instance, patchreviewintention_set)
 
         return super(PatchDetailSerializer, self).update(
             instance, validated_data
