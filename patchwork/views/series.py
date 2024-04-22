fe7c8685cef2abc4937c2718723b5d8653b17764
andrepapoti <andrepapoti@gmail.com>
2024-04-22 15:25:34 -0300

    views: add series-list view
    This view is meant to give a quickoverview on a project since some of
    them can have hundreds of patches actives.
    This view also allows the user to apply changes on all of the series
    patches at once
    
    Signed-off-by: andrepapoti <andrepapoti@gmail.com>

--- patchwork/views/series.py
+++ patchwork/views/series.py
@@ -2,12 +2,16 @@
 # Copyright (C) 2017 Stephen Finucane <stephen@that.guru>
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
+import collections
 
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
+from django.shortcuts import render
 
 from patchwork.models import Series
+from patchwork.models import Project
 from patchwork.views.utils import series_to_mbox
+from patchwork.paginator import Paginator
 
 
 def series_mbox(request, series_id):
@@ -20,3 +24,43 @@ def series_mbox(request, series_id):
     )
 
     return response
+
+
+def series_list(request, project_id):
+    project = get_object_or_404(Project, linkname=project_id)
+    sort = request.GET.get('order', 'date.desc')
+    sort_field, sort_dir = sort.split('.')
+    sort_order = f"{'-' if sort_dir == 'desc' else ''}{sort_field}"
+    context = {}
+    series_list = (
+        Series.objects.filter(project=project)
+        .only(
+            'submitter',
+            'project',
+            'version',
+            'name',
+            'date',
+            'id',
+            'cover_letter',
+        )
+        .select_related('project', 'submitter', 'cover_letter')
+        .order_by(sort_order)
+    )
+
+    paginator = Paginator(request, series_list)
+    context.update(
+        {
+            'project': project,
+            'projects': Project.objects.all(),
+            'series_list': series_list,
+            'page': paginator.current_page,
+            'order': sort,
+            'list_view': {
+                'view': 'series-list',
+                'view_params': {'project_id': project.linkname},
+                'params': collections.OrderedDict(),
+            },
+        }
+    )
+
+    return render(request, 'patchwork/series-list.html', context)
